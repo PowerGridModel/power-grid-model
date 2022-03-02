612932713369071bce4c64a83222309821b3a01d
Bram Stoeller <bram.stoeller@brainbuilders.nl>
2022-03-02 11:46:12 +0100

    Update unittest

--- tests/unit/conversion/test_filters.py
+++ tests/unit/conversion/test_filters.py
@@ -10,30 +10,38 @@ def test_inverse():
 
 
 def test_get_winding_from():
-    assert get_winding_from("YNd11") == WindingType.wye_n
-    assert get_winding_from("YNyn2") == WindingType.wye_n
-    assert get_winding_from("Yyn1") == WindingType.wye
-    assert get_winding_from("Dyn5") == WindingType.delta
+    assert get_winding_from("Yy1") == WindingType.wye
+    assert get_winding_from("Yyn2") == WindingType.wye
+    assert get_winding_from("Yd3") == WindingType.wye
+    assert get_winding_from("YNy4") == WindingType.wye_n
+    assert get_winding_from("YNyn5") == WindingType.wye_n
+    assert get_winding_from("YNd6") == WindingType.wye_n
+    assert get_winding_from("Dy7") == WindingType.delta
+    assert get_winding_from("Dyn8") == WindingType.delta
+    assert get_winding_from("Dd9") == WindingType.delta
     with pytest.raises(ValueError):
         get_winding_from("XNd11")
 
 
 def test_get_winding_to():
-    assert get_winding_to("YNd11") == WindingType.delta
-    assert get_winding_to("YNyn2") == WindingType.wye_n
-    assert get_winding_to("YNy1") == WindingType.wye
-    assert get_winding_to("Dyn5") == WindingType.wye_n
+    assert get_winding_to("Yy1") == WindingType.wye
+    assert get_winding_to("Yyn2") == WindingType.wye_n
+    assert get_winding_to("Yd3") == WindingType.delta
+    assert get_winding_to("YNy4") == WindingType.wye
+    assert get_winding_to("YNyn5") == WindingType.wye_n
+    assert get_winding_to("YNd6") == WindingType.delta
+    assert get_winding_to("Dy7") == WindingType.wye
+    assert get_winding_to("Dyn8") == WindingType.wye_n
+    assert get_winding_to("Dd9") == WindingType.delta
     with pytest.raises(ValueError):
         get_winding_to("XNd11")
 
 
 def test_get_clock():
-    assert get_clock("YNd11") == 11
-    assert get_clock("YNyn2") == 2
-    assert get_clock("YNd1") == 1
-    assert get_clock("Dyn5") == 5
     assert get_clock("YNd0") == 0
+    assert get_clock("YNyn5") == 5
     assert get_clock("YNd12") == 12
     with pytest.raises(ValueError):
         get_clock("YNd-1")
+    with pytest.raises(ValueError):
         get_clock("YNd13")
