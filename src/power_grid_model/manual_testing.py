11d4e6fe7be0f2af2dcb8a7c3ff6c3d6ff17a2f6
Bram Stoeller <bram.stoeller@alliander.com>
2022-03-15 08:30:00 +0100

    json writer, don't write a non-list non-dict value on a new line.
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model/manual_testing.py
+++ src/power_grid_model/manual_testing.py
@@ -34,7 +34,7 @@ def is_nan(data) -> bool:
 
 
 def convert_list_to_batch_data(
-    list_data: List[Dict[str, np.ndarray]]
+        list_data: List[Dict[str, np.ndarray]]
 ) -> Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]:
     """
     Convert list of dataset to one single batch dataset
@@ -71,7 +71,7 @@ def convert_list_to_batch_data(
 
 
 def convert_python_to_numpy(
-    data: Union[Dict, List], data_type: str
+        data: Union[Dict, List], data_type: str
 ) -> Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]:
     """
     Convert native python data to internal numpy
@@ -109,7 +109,7 @@ def convert_python_to_numpy(
 
 
 def convert_batch_to_list_data(
-    batch_data: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]
+        batch_data: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]
 ) -> List[Dict[str, np.ndarray]]:
     """
     Convert list of dataset to one single batch dataset
@@ -134,7 +134,7 @@ def convert_batch_to_list_data(
         single_dataset = {}
         for key, batch in batch_data.items():
             if isinstance(batch, dict):
-                single_dataset[key] = batch["data"][batch["indptr"][i] : batch["indptr"][i + 1]]
+                single_dataset[key] = batch["data"][batch["indptr"][i]: batch["indptr"][i + 1]]
             else:
                 single_dataset[key] = batch[i, ...]
         list_data.append(single_dataset)
@@ -142,7 +142,7 @@ def convert_batch_to_list_data(
 
 
 def convert_numpy_to_python(
-    data: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]
+        data: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]]
 ) -> Union[Dict[str, List[Dict[str, Union[int, float]]]], List[Dict[str, List[Dict[str, Union[int, float]]]]]]:
     """
     Convert internal numpy arrays to native python data
@@ -186,11 +186,11 @@ def import_json_data(json_file: Path, data_type: str) -> Union[Dict[str, np.ndar
 
 
 def export_json_data(
-    json_file: Path,
-    data: Union[Dict[str, np.ndarray], List[Dict[str, np.ndarray]]],
-    indent: Optional[int] = 2,
-    compact: bool = False,
-    meta_data: Optional[Dict[int, Any]] = None,
+        json_file: Path,
+        data: Union[Dict[str, np.ndarray], List[Dict[str, np.ndarray]]],
+        indent: Optional[int] = 2,
+        compact: bool = False,
+        meta_data: Optional[Dict[int, Any]] = None,
 ):
     """
     export json data
@@ -238,7 +238,7 @@ def compact_json_dump(data: Any, file_pointer: IO[str], indent: int, max_level: 
         file_pointer.write(tab + "{\n")
         n_obj = len(data)
         for i, (key, obj) in enumerate(data.items(), start=1):
-            if level == max_level - 1:
+            if level == max_level - 1 or not isinstance(obj, (list, dict)):
                 file_pointer.write(tab + " " * indent + f'"{key}": ')
                 json.dump(obj, file_pointer, indent=None)
             else:
@@ -247,4 +247,5 @@ def compact_json_dump(data: Any, file_pointer: IO[str], indent: int, max_level: 
             file_pointer.write(",\n" if i < n_obj else "\n")
         file_pointer.write(tab + "}")
     else:
-        compact_json_dump(data, file_pointer, indent, level, level)
+        file_pointer.write(tab)
+        json.dump(data, file_pointer, indent=None)
