95012439fe39babc67369345eadfcd2e43fda028
Tony Xiang <tony.xiang@alliander.com>
2022-04-14 22:29:44 +0200

    output not working yet
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- include/power_grid_model/auxiliary/output.hpp
+++ include/power_grid_model/auxiliary/output.hpp
@@ -2,6 +2,9 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+// clang-format off
+
+
 #pragma once
 #ifndef POWER_GRID_MODEL_AUXILIARY_OUTPUT_HPP
 #define POWER_GRID_MODEL_AUXILIARY_OUTPUT_HPP
@@ -11,39 +14,33 @@
 
 namespace power_grid_model {
 
-struct BaseOutput {
-    ID id;
-    IntS energized;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(BaseOutput, 0, _,
+    ID, id,
+    IntS, energized);
 
 template <bool sym>
-struct NodeOutput : BaseOutput {
-    RealValue<sym> u_pu, u, u_angle;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(NodeOutput, 1, BaseOutput,
+    RealValue<sym>, u_pu, RealValue<sym>, u, RealValue<sym>, u_angle);
 
 template <bool sym>
-struct BranchOutput : BaseOutput {
-    double loading;
-    RealValue<sym> p_from, q_from, i_from, s_from;
-    RealValue<sym> p_to, q_to, i_to, s_to;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(BranchOutput, 1, BaseOutput,
+    double, loading,
+    RealValue<sym>, p_from, RealValue<sym>, q_from, RealValue<sym>, i_from, RealValue<sym>, s_from,
+    RealValue<sym>, p_to, RealValue<sym>, q_to, RealValue<sym>, i_to, RealValue<sym>, s_to);
 
 template <bool sym>
-struct ApplianceOutput : BaseOutput {
-    RealValue<sym> p, q, i, s, pf;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(ApplianceOutput, 1, BaseOutput,
+    RealValue<sym>, p, RealValue<sym>, q, RealValue<sym>, i, RealValue<sym>, s, RealValue<sym>, pf);
 
 template <bool sym>
-struct VoltageSensorOutput : BaseOutput {
-    RealValue<sym> u_residual;
-    RealValue<sym> u_angle_residual;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(VoltageSensorOutput, 1, BaseOutput,
+    RealValue<sym>, u_residual,
+    RealValue<sym>, u_angle_residual);
 
 template <bool sym>
-struct PowerSensorOutput : BaseOutput {
-    RealValue<sym> p_residual;
-    RealValue<sym> q_residual;
-};
+POWER_GRID_MODEL_DATA_STRUCT_DEF(PowerSensorOutput, 1, BaseOutput,
+    RealValue<sym>, p_residual,
+    RealValue<sym>, q_residual);
 
 using SymNodeOutput = NodeOutput<true>;
 using AsymNodeOutput = NodeOutput<false>;
@@ -55,3 +52,5 @@ using AsymApplianceOutput = ApplianceOutput<false>;
 }  // namespace power_grid_model
 
 #endif
+
+// clang-format off
