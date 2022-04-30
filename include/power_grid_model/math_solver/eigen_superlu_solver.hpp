0985141269d966023770cef9e39268bda3060f83
Tony Xiang <tony.xiang@alliander.com>
2022-04-30 21:26:20 +0200

    refactor to have stateful solver
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- include/power_grid_model/math_solver/eigen_superlu_solver.hpp
+++ include/power_grid_model/math_solver/eigen_superlu_solver.hpp
@@ -30,36 +30,62 @@ class EigenSuperLUSolver final {
     using BufferVector = Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, 1>, Eigen::Aligned8>;
     using SparseIdxEntry = Eigen::Triplet<Idx, Idx>;
 
+    struct BSRHandle {
+        Idx matrix_size_in_block;
+        Idx block_size;
+        Idx nnz_block;
+        Idx block_capacity;
+        Idx nnz;
+        Idx matrix_size;
+        std::shared_ptr<IdxVector const> data_mapping;
+    };
+
    public:
     EigenSuperLUSolver(Idx matrix_size_in_block,                    // size of matrix in block form
                        Idx block_size,                              // size of block
                        std::shared_ptr<IdxVector const> const& ia,  // indptr
                        std::shared_ptr<IdxVector const> const& ja   // column indices
-                       )
-        : matrix_size_in_block_{matrix_size_in_block},
-          block_size_{block_size},
-          nnz_block_{ia->back()},
-          block_capacity_{block_size_ * block_size_},
-          nnz_{nnz_block_ * block_capacity_},
-          matrix_size_{matrix_size_in_block_ * block_size_} {
-        initialize_solver(*ia, *ja);
+    ) {
+        bsr_handle_.matrix_size_in_block = matrix_size_in_block;
+        bsr_handle_.block_size = block_size;
+        bsr_handle_.nnz_block = ia->back();
+        bsr_handle_.block_capacity = block_size * block_size;
+        bsr_handle_.nnz = bsr_handle_.nnz_block * bsr_handle_.block_capacity;
+        bsr_handle_.matrix_size = matrix_size_in_block * block_size;
+        initialize_matrix(*ia, *ja);
+        initialize_solver();
+    }
+
+    // copy construction
+    EigenSuperLUSolver(EigenSuperLUSolver const& other)
+        : bsr_handle_{other.bsr_handle_}, sparse_matrix_{other.sparse_matrix_} {
+        initialize_solver();
+    }
+
+    // copy assignment
+    EigenSuperLUSolver& operator=(EigenSuperLUSolver const& other) {
+        // copy new handle
+        bsr_handle_ = other.bsr_handle_;
+        // copy new matrix
+        sparse_matrix_ = other.sparse_matrix_;
+        // re initialize
+        initialize_solver();
+        return *this;
     }
 
     void solve(void const* data, void* b, void* x, bool use_prefactorization = false) {
         (void)use_prefactorization;  // suppress unused variable warning
         // copy data
         copy_matrix_data(data);
-        BufferVector bm{std::launder(reinterpret_cast<T*>(b)), matrix_size_};
-        BufferVector xm{std::launder(reinterpret_cast<T*>(x)), matrix_size_};
-        SparseSolver sparse_solver;
-        sparse_solver.analyzePattern(sparse_matrix_);
-        sparse_solver.factorize(sparse_matrix_);
-        if (sparse_solver.info() != Eigen::Success) {
-            throw SparseMatrixError{sparse_solver.info(), sparse_solver.lastErrorMessage()};
+        BufferVector bm{std::launder(reinterpret_cast<T*>(b)), bsr_handle_.matrix_size};
+        BufferVector xm{std::launder(reinterpret_cast<T*>(x)), bsr_handle_.matrix_size};
+        sparse_solver_.factorize(sparse_matrix_);
+        if (sparse_solver_.info() != Eigen::Success) {
+            throw SparseMatrixError{sparse_solver_.info(), sparse_solver_.lastErrorMessage()};
         }
-        xm = sparse_solver.solve(bm);
-        if (sparse_solver.info() != Eigen::Success) {
-            throw SparseMatrixError{sparse_solver.info(), sparse_solver.lastErrorMessage()};
+        xm = sparse_solver_.solve(bm);
+        if (sparse_solver_.info() != Eigen::Success) {
+            throw SparseMatrixError{sparse_solver_.info(), sparse_solver_.lastErrorMessage()};
         }
     }
 
@@ -72,54 +98,54 @@ class EigenSuperLUSolver final {
     }
 
    private:
-    Idx matrix_size_in_block_;
-    Idx block_size_;
-    Idx nnz_block_;
-    Idx block_capacity_;
-    Idx nnz_;
-    Idx matrix_size_;
+    BSRHandle bsr_handle_;
     SparseMatrix sparse_matrix_;
-    // SparseSolver sparse_solver_;  // not copyable or movable
-    std::shared_ptr<IdxVector const> data_mapping_;
+    SparseSolver sparse_solver_;  // not copyable or movable
 
-    void initialize_solver(IdxVector const& ia,  // indptr
+    void initialize_matrix(IdxVector const& ia,  // indptr
                            IdxVector const& ja   // column indices
     ) {
         // loop to add triplet
         std::vector<SparseIdxEntry> idx_list;
-        idx_list.reserve(nnz_);
+        idx_list.reserve(bsr_handle_.nnz);
         // two loops for blocks in csr
-        for (Idx bi = 0; bi != matrix_size_in_block_; ++bi) {
+        for (Idx bi = 0; bi != bsr_handle_.matrix_size_in_block; ++bi) {
             for (Idx block_ind = ia[bi]; block_ind != ia[bi + 1]; ++block_ind) {
                 Idx const bj = ja[block_ind];
                 // two loops for rows/columns inside block
-                for (Idx ci = 0; ci != block_size_; ++ci) {
-                    for (Idx cj = 0; cj != block_size_; ++cj) {
-                        Idx const i = bi * block_size_ + ci;
-                        Idx const j = bj * block_size_ + cj;
-                        Idx const data_ind = block_ind * block_capacity_ + ci * block_size_ + cj;
+                for (Idx ci = 0; ci != bsr_handle_.block_size; ++ci) {
+                    for (Idx cj = 0; cj != bsr_handle_.block_size; ++cj) {
+                        Idx const i = bi * bsr_handle_.block_size + ci;
+                        Idx const j = bj * bsr_handle_.block_size + cj;
+                        Idx const data_ind = block_ind * bsr_handle_.block_capacity + ci * bsr_handle_.block_size + cj;
                         idx_list.emplace_back(i, j, data_ind);
                     }
                 }
             }
         }
         // idx matrix
-        SparseIndexMatrix idx_matrix{matrix_size_, matrix_size_};
+        SparseIndexMatrix idx_matrix{bsr_handle_.matrix_size, bsr_handle_.matrix_size};
         idx_matrix.setFromTriplets(idx_list.begin(), idx_list.end());
         assert(idx_matrix.isCompressed());
         // copy idx data
-        data_mapping_ = std::make_shared<IdxVector const>(idx_matrix.valuePtr(), idx_matrix.valuePtr() + nnz_);
+        bsr_handle_.data_mapping =
+            std::make_shared<IdxVector const>(idx_matrix.valuePtr(), idx_matrix.valuePtr() + bsr_handle_.nnz);
         // initialize sparse matrix
         sparse_matrix_ = idx_matrix.cast<T>();
         assert(sparse_matrix_.isCompressed());
     }
 
+    void initialize_solver() {
+        sparse_solver_.analyzePattern(sparse_matrix_);
+    }
+
     void copy_matrix_data(void const* data) {
         T const* const input_ptr = std::launder(reinterpret_cast<T const*>(data));
         T* const data_ptr = sparse_matrix_.valuePtr();
-        std::transform(data_mapping_->begin(), data_mapping_->end(), data_ptr, [&input_ptr](Idx j) {
-            return input_ptr[j];
-        });
+        std::transform(bsr_handle_.data_mapping->begin(), bsr_handle_.data_mapping->end(), data_ptr,
+                       [&input_ptr](Idx j) {
+                           return input_ptr[j];
+                       });
     }
 };
 
