e637179fb551aeef75e17fd3e7f73e6c8f62372b
Tony Xiang <tony.xiang@alliander.com>
2022-05-20 13:37:56 +0200

    add block test, fails
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/cpp_unit_tests/test_sparse_lu_solver.cpp
+++ tests/cpp_unit_tests/test_sparse_lu_solver.cpp
@@ -33,8 +33,8 @@ void check_result(std::vector<T> const& x, std::vector<T> const& x_solver) {
 }
 
 // test block calculation with 2*2
-using Tensor = Eigen::Array22d;
-using Array = Eigen::Array2d;
+using Tensor = Eigen::Array<double, 2, 2, Eigen::RowMajor>;
+using Array = Eigen::Array<double, 2, 1, Eigen::ColMajor>;
 
 TEST_CASE("Test Sparse LU solver") {
     // 3 * 3 matrix, with diagonal, two fill-ins
@@ -131,91 +131,35 @@ TEST_CASE("Test Sparse LU solver") {
     }
 
     SECTION("Test block(double 2*2) calculation") {
-        // [  0 1   1   2   3   4           3             40        
+        // [  0 1   1   2   3   4           3             40
         //  100 0   7  -1   5   6           4            355
         //    1 2   0 200   f   f       * [ -1 ]   =  [ -189 ]
         //   -3 4   3   1   f   f       *   -2             3
-        //    5 6   f   f   1   0           5             44 
+        //    5 6   f   f   1   0           5             44
         //   -7 8   f   f   0 100           6            611
 
-
         //  2 f 6]        2          18
-        std::vector<double> data = {
-            4, 1, 5,  // row 0
-            3, 7,     // row 1
-            2, 6      // row 2
+        std::vector<Tensor> data = {
+            {{0, 1}, {100, 0}},  // 0, 0
+            {{1, 2}, {7, -1}},   // 0, 1
+            {{3, 4}, {5, 6}},    // 0, 2
+            {{1, 2}, {-3, 4}},   // 1, 0
+            {{0, 200}, {3, 1}},  // 1, 1
+            {{5, 6}, {-7, 8}},   // 2, 0
+            {{1, 0}, {0, 100}},  // 2, 2
         };
-        std::vector<double> rhs = {21, 2, 18};
-        std::vector<double> x_ref = {3, -1, 2};
-        std::vector<double> x(3, 0.0);
-        SparseLUSolver<double, double, double> solver{row_indptr, col_indices, diag_lu, data_mapping};
+        std::vector<Array> rhs = {{40, 355}, {-189, 3}, {44, 611}};
+        std::vector<Array> x_ref = {{3, 4}, {-1, -2}, {5, 6}};
+        std::vector<Array> x(3, Array::Zero());
+        SparseLUSolver<Tensor, Array, Array> solver{row_indptr, col_indices, diag_lu, data_mapping};
 
         SECTION("Test calculation") {
             solver.solve(data.data(), rhs.data(), x.data());
             check_result(x, x_ref);
         }
+    }
 
-        SECTION("Test copy and move") {
-            SparseLUSolver<double, double, double> s1{solver};
-            // copy construction
-            s1.solve(data.data(), rhs.data(), x.data());
-            check_result(x, x_ref);
-            // copy assignment
-            s1 = solver;
-            s1.solve(data.data(), rhs.data(), x.data());
-            check_result(x, x_ref);
-            // self assignment
-            auto& s2 = s1;
-            s1 = s2;
-            s1.solve(data.data(), rhs.data(), x.data());
-            check_result(x, x_ref);
-            // move construction
-            SparseLUSolver<double, double, double> s3{std::move(solver)};
-            s3.solve(data.data(), rhs.data(), x.data());
-            check_result(x, x_ref);
-        }
+}
 
-        SECTION("Test (pseudo) singular") {
-            data[0] = 0.0;
-            CHECK_THROWS_AS(solver.solve(data.data(), rhs.data(), x.data()), SparseMatrixError);
-        }
-
-        SECTION("Test prefactorize") {
-            solver.prefactorize(data.data());
-            solver.solve(data.data(), rhs.data(), x.data(), true);
-            check_result(x, x_ref);
-
-            // basically data / 2
-            std::vector<double> other_data = data;
-            for (double& d : other_data) {
-                d /= 2.0;
-            }
-            // basically x * 2
-            std::vector<double> other_x_ref = x_ref;
-            for (double& p : other_x_ref) {
-                p *= 2.0;
-            }
-
-            // because all data should be prefactorized, changing the data should not
-            // change the result when use_prefactorization = true
-            solver.solve(other_data.data(), rhs.data(), x.data(), true);
-            check_result(x, x_ref);
-
-            // prefactorize other_data, then solve and compare with other_x_ref
-            solver.prefactorize(other_data.data());
-            solver.solve(other_data.data(), rhs.data(), x.data(), true);
-            check_result(x, other_x_ref);
-
-            // solve and compare with other_x without using prefactorization
-            solver.solve(other_data.data(), rhs.data(), x.data(), false);
-            check_result(x, other_x_ref);
-
-            // invalidate pre-factorization
-            // and re-run with original data with pre-factorization enabled
-            // it should still re-do the factorization
-            solver.invalidate_prefactorization();
-            solver.solve(data.data(), rhs.data(), x.data(), true);
-            check_result(x, x_ref);
-        }
 
 }  // namespace power_grid_model
