5d174a5f4cc5895f49c8c07ed5afc6b55641e608
Nitish Bharambe <nitish.bharambe@alliander.com>
2022-06-15 13:08:42 +0200

    Remove commented code
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- include/power_grid_model/math_solver/iterative_current_pf_solver.hpp
+++ include/power_grid_model/math_solver/iterative_current_pf_solver.hpp
@@ -55,13 +55,6 @@ class IterativecurrentPFSolver : public IterativePFSolver<sym> {
    public:
     IterativecurrentPFSolver(YBus<sym> const& y_bus, std::shared_ptr<MathModelTopology const> const& topo_ptr)
         : IterativePFSolver<sym>{y_bus, topo_ptr},
-          /*
-          n_bus_{y_bus.size()},
-          phase_shift_{topo_ptr, &topo_ptr->phase_shift},
-          load_gen_bus_indptr_{topo_ptr, &topo_ptr->load_gen_bus_indptr},
-          source_bus_indptr_{topo_ptr, &topo_ptr->source_bus_indptr},
-          load_gen_type_{topo_ptr, &topo_ptr->load_gen_type},
-          */
           updated_u_(y_bus.size()),
           rhs_(n_bus_),
           mat_data_(y_bus.nnz()),
@@ -163,12 +156,6 @@ class IterativecurrentPFSolver : public IterativePFSolver<sym> {
     }
 
    private:
-    // Idx n_bus_;
-    // shared topo data
-    // std::shared_ptr<DoubleVector const> phase_shift_;
-    // std::shared_ptr<IdxVector const> load_gen_bus_indptr_;
-    // std::shared_ptr<IdxVector const> source_bus_indptr_;
-    // std::shared_ptr<std::vector<LoadGenType> const> load_gen_type_;
     ComplexValueVector<sym> updated_u_;
     ComplexValueVector<sym> rhs_;
     ComplexTensorVector<sym> mat_data_;
@@ -229,53 +216,6 @@ class IterativecurrentPFSolver : public IterativePFSolver<sym> {
         }
         return max_dev;
     }
-
-    /*
-    void calculate_result(YBus<sym> const& y_bus, PowerFlowInput<sym> const& input, MathOutput<sym>& output) {
-        // pending to correct
-        // call y bus
-        output.branch = y_bus.calculate_branch_flow(output.u);
-        output.shunt = y_bus.calculate_shunt_flow(output.u);
-
-        // prepare source and load gen
-        output.source.resize(source_bus_indptr_->back());
-        output.load_gen.resize(load_gen_bus_indptr_->back());
-        // loop all bus
-        for (Idx bus = 0; bus != n_bus_; ++bus) {
-            // source
-            for (Idx source = (*source_bus_indptr_)[bus]; source != (*source_bus_indptr_)[bus + 1]; ++source) {
-                ComplexValue<sym> const u_ref{input.source[source]};
-                ComplexTensor<sym> const y_ref = y_bus.math_model_param().source_param[source];
-                output.source[source].i = dot(y_ref, u_ref - output.u[bus]);
-                output.source[source].s = output.u[bus] * conj(output.source[source].i);
-            }
-
-            // load_gen
-            for (Idx load_gen = (*load_gen_bus_indptr_)[bus]; load_gen != (*load_gen_bus_indptr_)[bus + 1];
-                 ++load_gen) {
-                LoadGenType const type = (*load_gen_type_)[load_gen];
-                switch (type) {
-                    case LoadGenType::const_pq:
-                        // always same power
-                        output.load_gen[load_gen].s = input.s_injection[load_gen];
-                        break;
-                    case LoadGenType::const_y:
-                        // power is quadratic relation to voltage
-                        output.load_gen[load_gen].s =
-                            input.s_injection[load_gen] * cabs(output.u[bus]) * cabs(output.u[bus]);
-                        break;
-                    case LoadGenType::const_i:
-                        // power is linear relation to voltage
-                        output.load_gen[load_gen].s = input.s_injection[load_gen] * cabs(output.u[bus]);
-                        break;
-                    default:
-                        throw MissingCaseForEnumError("Power injection", type);
-                }
-                output.load_gen[load_gen].i = conj(output.load_gen[load_gen].s / output.u[bus]);
-            }
-        }
-    }
-    */
 };
 
 template class IterativecurrentPFSolver<true>;
