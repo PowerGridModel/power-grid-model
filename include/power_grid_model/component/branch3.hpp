170d4163bd7fb26512cc6f2a93168293e279a567
Peter Salemink <peter.salemink@alliander.com>
2022-07-15 13:51:07 +0200

    Branch3 constructor
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- include/power_grid_model/component/branch3.hpp
+++ include/power_grid_model/component/branch3.hpp
@@ -22,6 +22,27 @@ class Branch3 : public Base {
     ComponentType math_model_type() const final {
         return ComponentType::branch3;
     }
+
+    Branch3(Branch3Input const& branch3_input)
+        : Base{branch3_input},
+          node_1_{branch3_input.node_1},
+          node_2_{branch3_input.node_2},
+          node_3_{branch3_input.node_3},
+          status_1_{branch3_input.status_1},
+          status_2_{branch3_input.status_2},
+          status_3_{branch3_input.status_3} {
+        if (node_1_ == node_2_ || node_1_ == node_3_ || node_2_ == node_3_) {
+            throw InvalidBranch3{id(), node_1_, node_2_, node_3_};
+        }
+    }
+
+   private:
+    ID node_1_;
+    ID node_2_;
+    ID node_3_;
+    bool status_1_;
+    bool status_2_;
+    bool status_3_;
 };
 
 }  // namespace power_grid_model
