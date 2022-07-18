78e201ee96c3210f38a2628ddba0bc1ca9ad57f9
Peter Salemink <peter.salemink@alliander.com>
2022-07-18 14:01:28 +0200

    3wt base constructor
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- include/power_grid_model/component/three_winding_transformer.hpp
+++ include/power_grid_model/component/three_winding_transformer.hpp
@@ -15,6 +15,79 @@ class ThreeWindingTransformer : public Branch3 {
     using InputType = ThreeWindingTransformerInput;
     using UpdateType = ThreeWindingTransformerUpdate;
     static constexpr char const* name = "three_winding_transformer";
+
+    ThreeWindingTransformer(ThreeWindingTransformerInput const& transformer_input, double u1_rated, double u2_rated,
+                            double u3_rated)
+        : Branch3{three_winding_transformer_input},
+          u1_{three_winding_transformer_input.u1},
+          u2_{three_winding_transformer_input.u2},
+          u3_{three_winding_transformer_input.u3},
+          sn_1_{three_winding_transformer_input.sn_1},
+          sn_2_{three_winding_transformer_input.sn_2},
+          sn_3_{three_winding_transformer_input.sn_3},
+          uk_12_{three_winding_transformer_input.uk_12},
+          uk_13_{three_winding_transformer_input.uk_13},
+          uk_23_{three_winding_transformer_input.uk_23},
+          pk_12_{three_winding_transformer_input.pk_12},
+          pk_13_{three_winding_transformer_input.pk_13},
+          pk_23_{three_winding_transformer_input.pk_23},
+          i0_{three_winding_transformer_input.i0},
+          p0_{three_winding_transformer_input.p0},
+          winding_1_{three_winding_transformer_input.winding_1},
+          winding_2_{three_winding_transformer_input.winding_2},
+          winding_3_{three_winding_transformer_input.winding_3},
+          clock_12_{three_winding_transformer_input.clock_12},
+          clock_13_{three_winding_transformer_input.clock_13},
+          clock_23_{three_winding_transformer_input.clock_23},
+          tap_side_{three_winding_transformer_input.tap_side},
+          tap_pos_{three_winding_transformer_input.tap_pos},
+          tap_min_{three_winding_transformer_input.tap_min},
+          tap_max_{three_winding_transformer_input.tap_max},
+          tap_nom_{three_winding_transformer_input.tap_nom == na_IntS ? (IntS)0
+                                                                      : three_winding_transformer_input.tap_nom},
+          tap_direction_{tap_max_ > tap_min_ ? (IntS)1 : (IntS)-1},
+          tap_size_{three_winding_transformer_input.tap_size},
+          uk_12_min_{is_nan(three_winding_transformer_input.uk_12_min) ? uk_12_
+                                                                       : three_winding_transformer_input.uk_12_min},
+          uk_12_max_{is_nan(three_winding_transformer_input.uk_12_max) ? uk_12_
+                                                                       : three_winding_transformer_input.uk_12_max},
+          uk_13_min_{is_nan(three_winding_transformer_input.uk_13_min) ? uk_13_
+                                                                       : three_winding_transformer_input.uk_13_min},
+          uk_13_max_{is_nan(three_winding_transformer_input.uk_13_max) ? uk_13_
+                                                                       : three_winding_transformer_input.uk_13_max},
+          uk_23_min_{is_nan(three_winding_transformer_input.uk_23_min) ? uk_23_
+                                                                       : three_winding_transformer_input.uk_23_min},
+          uk_23_max_{is_nan(three_winding_transformer_input.uk_23_max) ? uk_23_
+                                                                       : three_winding_transformer_input.uk_23_max},
+          pk_12_min_{is_nan(three_winding_transformer_input.pk_12_min) ? pk_12_
+                                                                       : three_winding_transformer_input.pk_12_min},
+          pk_12_max_{is_nan(three_winding_transformer_input.pk_12_max) ? pk_12_
+                                                                       : three_winding_transformer_input.pk_12_max},
+          pk_13_min_{is_nan(three_winding_transformer_input.pk_13_min) ? pk_13_
+                                                                       : three_winding_transformer_input.pk_13_min},
+          pk_13_max_{is_nan(three_winding_transformer_input.pk_13_max) ? pk_13_
+                                                                       : three_winding_transformer_input.pk_13_max},
+          pk_23_min_{is_nan(three_winding_transformer_input.pk_23_min) ? pk_23_
+                                                                       : three_winding_transformer_input.pk_23_min},
+          pk_23_max_{is_nan(three_winding_transformer_input.pk_23_max) ? pk_23_
+                                                                       : three_winding_transformer_input.pk_23_max}
+    // base_i_1/2/3
+    // z_grounding_1/2/3
+    {
+        // TODO
+    }
+
+   private:
+    double u1_, u2_, u3_;
+    double sn_1_, sn_2_, sn_3_;
+    double uk_12_, uk_13_, uk_23_, pk_12_, pk_13_, pk_23_, i0_, p0_;
+    WindingType winding_1_, winding_2_, winding_3_;
+    IntS clock_12_, clock_13_, clock_23_;
+    Branch3Side tap_side_;
+    IntS tap_pos_, tap_min_, tap_max_, tap_nom_, tap_direction_;
+    double tap_size_;
+    double uk_12_min_, uk_12_max_, uk_13_min_, uk_13_max_, uk_23_min_, uk_23_max_;
+    double pk_12_min_, pk_12_max_, pk_13_min_, pk_13_max_, pk_23_min_, pk_23_max_;
 };
 
 }  // namespace power_grid_model
