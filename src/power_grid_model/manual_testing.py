e72e5fd3d51f72184fa62341a191d45aaf0ff7d5
Bram Stoeller <bram.stoeller@alliander.com>
2022-07-26 12:23:47 +0200

    Comments and restructure _compact_json_dump
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model/manual_testing.py
+++ src/power_grid_model/manual_testing.py
@@ -282,36 +282,72 @@ def export_json_data(
 
     with open(json_file, mode="w", encoding="utf-8") as file_pointer:
         if compact and indent:
-            max_level = 4 if isinstance(json_data, list) else 3
-            compact_json_dump(json_data, file_pointer, indent=indent, max_level=max_level)
+            is_batch_data = isinstance(json_data, list)
+            max_level = 4 if is_batch_data else 3
+            _compact_json_dump(json_data, file_pointer, indent=indent, max_level=max_level)
         else:
             json.dump(json_data, file_pointer, indent=indent)
 
 
-def compact_json_dump(data: Any, io_stream: IO[str], indent: int, max_level: int, level: int = 0):
+def _compact_json_dump(data: Any, io_stream: IO[str], indent: int, max_level: int, level: int = 0):
+    """Custom compact JSON writer that is intended to put data belonging to a single object on a single line.
+
+    For example:
+    {
+        "node": [
+            {"id": 0, "u_rated": 10500.0, "extra": {"original_id": 123}},
+            {"id": 1, "u_rated": 10500.0, "extra": {"original_id": 456}},
+        ],
+        "line": [
+            {"id": 0, "node_from": 0, "node_to": 1, ...}
+        ]
+    }
+
+    The function is being called recursively, starting at level 0 and recursing until max_level is reached. It is
+    basically a full json writer, but for efficiency reasons, on the last levels the native json.dump method is used.
+    """
+
+    # Let's define a 'tab' indent, depending on the level
     tab = " " * level * indent
-    if level >= max_level:
+
+    # If we are at the max_level, or the data simply doesn't contain any more levels, write the indent and serialize
+    # the data on a single line.
+    if level >= max_level or not isinstance(data, (list, dict)):
         io_stream.write(tab)
         json.dump(data, io_stream, indent=None)
-    elif isinstance(data, list):
+        return
+
+    # We'll need the number of objects later on
+    n_obj = len(data)
+
+    # If the data is a list:
+    # 1. start with an opening bracket
+    # 2. dump each element in the list
+    # 3. add a comma and a new line after each element, except for the last element, there we don't need a comma.
+    # 4. finish with a closing bracket
+    if isinstance(data, list):
         io_stream.write(tab + "[\n")
-        n_obj = len(data)
         for i, obj in enumerate(data, start=1):
-            compact_json_dump(obj, io_stream, indent, max_level, level + 1)
+            _compact_json_dump(obj, io_stream, indent, max_level, level + 1)
             io_stream.write(",\n" if i < n_obj else "\n")
         io_stream.write(tab + "]")
-    elif isinstance(data, dict):
-        io_stream.write(tab + "{\n")
-        n_obj = len(data)
-        for i, (key, obj) in enumerate(data.items(), start=1):
-            if level == max_level - 1 or not isinstance(obj, (list, dict)):
-                io_stream.write(tab + " " * indent + f'"{key}": ')
-                json.dump(obj, io_stream, indent=None)
-            else:
-                io_stream.write(tab + " " * indent + f'"{key}":\n')
-                compact_json_dump(obj, io_stream, indent, max_level, level + 2)
-            io_stream.write(",\n" if i < n_obj else "\n")
-        io_stream.write(tab + "}")
-    else:
-        io_stream.write(tab)
-        json.dump(data, io_stream, indent=None)
+        return
+
+    # If the data is a dictionary:
+    # 1. start with an opening curly bracket
+    # 2. for each element: write it's key, plus a colon ':'
+    # 3. if the next level would be the max_level, add a space and dump the element on a single,
+    #    else add a new line before dumping the element recursively.
+    # 4. add a comma and a new line after each element, except for the last element, there we don't need a comma.
+    # 5. finish with a closing curly bracket
+    io_stream.write(tab + "{\n")
+    for i, (key, obj) in enumerate(data.items(), start=1):
+        io_stream.write(tab + " " * indent + f'"{key}":')
+        if level == max_level - 1 or not isinstance(obj, (list, dict)):
+            io_stream.write(" ")
+            json.dump(obj, io_stream, indent=None)
+        else:
+            io_stream.write("\n")
+            _compact_json_dump(obj, io_stream, indent, max_level, level + 2)
+        io_stream.write(",\n" if i < n_obj else "\n")
+    io_stream.write(tab + "}")
