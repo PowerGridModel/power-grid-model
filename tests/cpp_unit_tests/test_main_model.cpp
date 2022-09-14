c1ca6e2a39eab4af4a01f90424b7bff7522767d1
Tony Xiang <tony.xiang@alliander.com>
2022-09-14 09:48:39 +0200

    formatting
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/cpp_unit_tests/test_main_model.cpp
+++ tests/cpp_unit_tests/test_main_model.cpp
@@ -104,7 +104,7 @@ TEST_CASE("Test main model") {
     main_model.add_component<AsymVoltageSensor>(asym_voltage_sensor_input);
     main_model.set_construction_complete();
 
-    SUBCASE("Test get indexer"){
+    SUBCASE("Test get indexer") {
         IdxVector const node_id{2, 1, 3, 2};
         IdxVector const expected_indexer{1, 0, 2, 1};
         IdxVector indexer(4);
