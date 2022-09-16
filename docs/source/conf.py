28318f1d5cb30044fc2bb848ec7a8dcc9f9e6cf0
Nitish Bharambe <nitish.bharambe@alliander.com>
2022-09-16 15:37:46 +0200

    add sphinx basic structure
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'power-grid-model'
+copyright = '2022, alliander-opensource'
+author = 'alliander-opensource'
+release = '1.4.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,25 @@
+% power-grid-model documentation master file, created by
+% sphinx-quickstart on Fri Sep 16 12:10:59 2022.
+% You can adapt this file completely to your liking, but it should at least
+% contain the root `toctree` directive.
+
+# Welcome to power-grid-model's documentation!
+
+```{warning}
+The documentation is under heavy development
+```
+
+```{toctree}
+:caption: 'Contents:'
+:maxdepth: 2
+
+graph-data-model
+native-data-interface
+python-api-reference
+```
+
+# Indices and tables
+
+- {ref}`genindex`
+- {ref}`modindex`
+- {ref}`search`
