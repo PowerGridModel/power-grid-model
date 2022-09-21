3d8e21b702541a450ce65fa131995cff29e2d13a
Nitish Bharambe <nitish.bharambe@alliander.com>
2022-09-21 11:06:20 +0200

    edit conf.py
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- docs/conf.py
+++ docs/conf.py
@@ -7,13 +7,23 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "power-grid-model"
 copyright = "2022, alliander-opensource"
 author = "alliander-opensource"
-release = "1.4.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,6 +33,7 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
 templates_path = ["_templates"]
@@ -37,3 +48,20 @@ html_static_path = ["_static"]
 # Internationalization
 # locale_dirs = ['locale/']   # path is example but recommended.
 # gettext_compact = False     # optional.
+
+
+# Napoleon settings (Extention for reading google/numpy docstrings. Add or remove as needed)
+# napoleon_google_docstring = True
+# napoleon_numpy_docstring = True
+# napoleon_include_init_with_doc = False
+# napoleon_include_private_with_doc = False
+# napoleon_include_special_with_doc = True
+# napoleon_use_admonition_for_examples = False
+# napoleon_use_admonition_for_notes = False
+# napoleon_use_admonition_for_references = False
+# napoleon_use_ivar = False
+# napoleon_use_param = True
+# napoleon_use_rtype = True
+# napoleon_preprocess_types = False
+# napoleon_type_aliases = None
+# napoleon_attr_annotations = True
