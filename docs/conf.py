b429677ef2c31f2142fd80401f492a8835356f8a
Nitish Bharambe <nitish.bharambe@alliander.com>
2022-09-23 15:18:17 +0200

    remove commented options from conf
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- docs/conf.py
+++ docs/conf.py
@@ -44,24 +44,3 @@ templates_path = ["_templates"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-
-# Internationalization
-# locale_dirs = ['locale/']   # path is example but recommended.
-# gettext_compact = False     # optional.
-
-
-# Napoleon settings (Extention for reading google/numpy docstrings. Add or remove as needed)
-# napoleon_google_docstring = True
-# napoleon_numpy_docstring = True
-# napoleon_include_init_with_doc = False
-# napoleon_include_private_with_doc = False
-# napoleon_include_special_with_doc = True
-# napoleon_use_admonition_for_examples = False
-# napoleon_use_admonition_for_notes = False
-# napoleon_use_admonition_for_references = False
-# napoleon_use_ivar = False
-# napoleon_use_param = True
-# napoleon_use_rtype = True
-# napoleon_preprocess_types = False
-# napoleon_type_aliases = None
-# napoleon_attr_annotations = True
