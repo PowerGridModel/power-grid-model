224692a4c495dc00ac7f51aa07bf807650d975eb
Nitish Bharambe <nitish.bharambe@alliander.com>
2022-10-14 10:33:19 +0200

    remove comments from conf DCO Remediation Commit for Nitish Bharambe <nitish.bharambe@alliander.com>
    I, Nitish Bharambe <nitish.bharambe@alliander.com>, hereby add my Signed-off-by to this commit: b1cd8b40abb0fed4b31cf5b66bc8d747187dd9de
    
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- docs/conf.py
+++ docs/conf.py
@@ -3,15 +3,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
