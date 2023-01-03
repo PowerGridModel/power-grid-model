b279da86994dfdab5f17e3a2c85ab105e206d930
Tony Xiang <tony.xiang@alliander.com>
2023-01-03 15:38:02 +0100

    reuse header
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- code-generation/run_code_gen.py
+++ code-generation/run_code_gen.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from generate_attribute_classes import code_gen
 from pathlib import Path
 
