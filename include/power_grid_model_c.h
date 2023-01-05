d054df688d4dc01d94c48e3ac4f645c58eb2bfcd
Tony Xiang <tony.xiang@alliander.com>
2023-01-05 14:02:46 +0100

    start c lib
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ include/power_grid_model_c.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_C_HPP
+#define POWER_GRID_MODEL_C_HPP
+
+#endif
