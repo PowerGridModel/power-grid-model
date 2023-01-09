3151725fe0fccb46ecf826ee50e4b8ad72af6962
Tony Xiang <tony.xiang@alliander.com>
2023-01-09 10:38:21 +0100

    test data class
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/cpp_unit_tests/test_c_api_meta_data.cpp
+++ tests/cpp_unit_tests/test_c_api_meta_data.cpp
@@ -2,35 +2,49 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-#include "doctest/doctest.h"
-#include "power_grid_model_c.h"
-#include "power_grid_model/auxiliary/meta_data_gen.hpp"
 #include <memory>
 
-struct HandleDestructor{
-    void operator() (PGM_Handle* handle){
+#include "doctest/doctest.h"
+#include "power_grid_model/auxiliary/meta_data_gen.hpp"
+#include "power_grid_model_c.h"
+
+struct HandleDestructor {
+    void operator()(PGM_Handle* handle) {
         PGM_destroy_handle(handle);
     }
 };
 
 using HandlePtr = std::unique_ptr<PGM_Handle, HandleDestructor>;
 
-
 namespace power_grid_model::meta_data {
 
 TEST_CASE("C API Meta Data") {
+    // get handle
+    HandlePtr const unique_handle{PGM_create_handle()};
+    PGM_Handle* hl = unique_handle.get();
+    auto const& meta = meta_data();
+
     SUBCASE("Datasets") {
-        HandlePtr const unique_handle{PGM_create_handle()};
-        PGM_Handle* hl = unique_handle.get();
-        auto const& meta = meta_data();
         CHECK(PGM_meta_n_datasets(hl) == (Idx)meta.size());
-        for (Idx i = 0; i != (Idx)meta.size(); ++i){
+        for (Idx i = 0; i != (Idx)meta.size(); ++i) {
             auto const found = meta.find(PGM_meta_dataset_name(hl, i));
             CHECK(found != meta.end());
-        } 
+        }
     }
 
+    SUBCASE("Data class") {
+        for (auto const& [dataset_name, data_classes] : meta) {
+            CHECK(PGM_meta_n_classes(hl, dataset_name.c_str()) == (Idx)data_classes.size());
+            for (Idx i = 0; i != (Idx)data_classes.size(); ++i) {
+                auto const found = data_classes.find(PGM_meta_class_name(hl, dataset_name.c_str(), i));
+                CHECK(found != data_classes.end());
+                auto const& class_name = found->first;
+                auto const& class_meta = found->second;
+                CHECK(PGM_meta_class_size(hl, dataset_name.c_str(), class_name.c_str()) == class_meta.size);
+                CHECK(PGM_meta_class_alignment(hl, dataset_name.c_str(), class_name.c_str()) == class_meta.alignment);
+            }
+        }
+    }
 }
 
-
-} // namespace power_grid_model::meta_data
+}  // namespace power_grid_model::meta_data
