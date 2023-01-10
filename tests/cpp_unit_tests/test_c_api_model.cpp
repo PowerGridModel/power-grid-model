5b260db08d344852c6fda94be51bb3817c640426
Tony Xiang <tony.xiang@alliander.com>
2023-01-10 11:26:43 +0100

    start testing model
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/cpp_unit_tests/test_c_api_model.cpp
+++ tests/cpp_unit_tests/test_c_api_model.cpp
@@ -23,6 +23,27 @@ TEST_CASE("C API Model") {
     NodeInput node_input{{0}, 100.0};
     SourceInput source_input{{{1}, 0, 1}, 1.0, 0.0, nan, 0.0, 1.0};
     SymLoadGenInput load_input{{{{2}, 0, 1}, LoadGenType::const_i}, 0.0, 0.0};
+    std::array input_type_names{"node", "source", "sym_load"};
+    std::array<Idx, 3> input_type_sizes{1, 1, 1};
+    // create one buffer
+    BufferPtr const unique_node_buffer{PGM_create_buffer(hl, "input", "node", 1)};
+    std::memcpy(unique_node_buffer.get(), &node_input, sizeof(NodeInput));
+    std::array<void const*, 3> input_data{unique_node_buffer.get(), &source_input, &load_input};
+
+    // output data
+    NodeOutput<true> sym_node_output{};
+    std::array output_type_names{"node"};
+    std::array<void*, 1> sym_output_data{&sym_node_output};
+
+    // create model
+    ModelPtr unique_model{
+        PGM_create_model(hl, 50.0, 3, input_type_names.data(), input_type_sizes.data(), input_data.data())};
+    PGM_PowerGridModel* model = unique_model.get();
+
+    SUBCASE("Simple power flow") {
+        PGM_calculate(hl, model, opt, 1, output_type_names.data(), sym_output_data.data(),  // basic parameters
+                      0, 0, nullptr, nullptr, nullptr, nullptr);                            // batch parameters
+    }
 }
 
 }  // namespace power_grid_model
