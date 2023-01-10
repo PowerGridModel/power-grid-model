a4ebcdd8cf49545c85d2f60dc4b5b0a62c10c883
Tony Xiang <tony.xiang@alliander.com>
2023-01-10 21:47:01 +0100

    tested batch error
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/cpp_unit_tests/test_c_api_model.cpp
+++ tests/cpp_unit_tests/test_c_api_model.cpp
@@ -17,10 +17,14 @@ source_1(1.0 p.u., 100.0 V) --internal_impedance(j10.0 ohm, sk=1000.0 VA, rx_rat
 
 u0 = 100.0 V - (j10.0 ohm * -j5.0 A) = 50.0 V
 
-update:
+update_0:
     u_ref = 0.5 p.u. (50.0 V)
     q_specified = 100 var (-j1.0A)
 u0 = 50.0 V - (j10.0 ohm * -j1.0 A) = 40.0 V
+
+update_1:
+    q_specified = 300 var (-j3.0A)
+u0 = 100.0 V - (j10.0 ohm * -j3.0 A) = 70.0 V
 */
 
 namespace power_grid_model {
@@ -45,19 +49,21 @@ TEST_CASE("C API Model") {
     std::array<void const*, 3> input_data{unique_node_buffer.get(), &source_input, &load_input};
 
     // output data
-    NodeOutput<true> sym_node_output{};
+    std::array<NodeOutput<true>, 2> sym_node_outputs{};
+    NodeOutput<true>& node_result_0 = sym_node_outputs[0];
+    NodeOutput<true>& node_result_1 = sym_node_outputs[1];
     std::array output_type_names{"node"};
-    std::array<void*, 1> sym_output_data{&sym_node_output};
+    std::array<void*, 1> sym_output_data{sym_node_outputs.data()};
 
     // update data
     SourceUpdate source_update{{{1}, na_IntS}, 0.5, nan};
-    SymLoadGenUpdate load_update{{{2}, na_IntS}, nan, 100.0};
+    std::array<SymLoadGenUpdate, 2> load_updates{{{{{2}, na_IntS}, nan, 100.0}, {{{2}, na_IntS}, nan, 300.0}}};
     std::array update_type_names{"source", "sym_load"};
     std::array<Idx, 2> update_type_sizes{1, 1};
-    std::array<void const*, 2> update_data{&source_update, &load_update};
-    std::array<Idx, 2> sizes_per_batch{1, -1};
-    std::array<Idx, 2> load_update_indptr{0, 1};
-    std::array<Idx const*, 2> indptrs_per_type{nullptr, load_update_indptr.data()};
+    std::array<void const*, 2> update_data{&source_update, load_updates.data()};
+    std::array<Idx, 2> sizes_per_batch{-1, 1};
+    std::array<Idx, 3> source_update_indptr{0, 1, 1};
+    std::array<Idx const*, 2> indptrs_per_type{source_update_indptr.data(), nullptr};
 
     // create model
     ModelPtr unique_model{
@@ -68,11 +74,11 @@ TEST_CASE("C API Model") {
         PGM_calculate(hl, model, opt, 1, output_type_names.data(), sym_output_data.data(),  // basic parameters
                       0, 0, nullptr, nullptr, nullptr, nullptr);                            // batch parameters
         CHECK(PGM_err_code(hl) == 0);
-        CHECK(sym_node_output.id == 0);
-        CHECK(sym_node_output.energized == 1);
-        CHECK(sym_node_output.u == doctest::Approx(50.0));
-        CHECK(sym_node_output.u_pu == doctest::Approx(0.5));
-        CHECK(sym_node_output.u_angle == doctest::Approx(0.0));
+        CHECK(node_result_0.id == 0);
+        CHECK(node_result_0.energized == 1);
+        CHECK(node_result_0.u == doctest::Approx(50.0));
+        CHECK(node_result_0.u_pu == doctest::Approx(0.5));
+        CHECK(node_result_0.u_angle == doctest::Approx(0.0));
     }
 
     SUBCASE("Simple update") {
@@ -81,23 +87,28 @@ TEST_CASE("C API Model") {
         PGM_calculate(hl, model, opt, 1, output_type_names.data(), sym_output_data.data(),  // basic parameters
                       0, 0, nullptr, nullptr, nullptr, nullptr);                            // batch parameters
         CHECK(PGM_err_code(hl) == 0);
-        CHECK(sym_node_output.id == 0);
-        CHECK(sym_node_output.energized == 1);
-        CHECK(sym_node_output.u == doctest::Approx(40.0));
-        CHECK(sym_node_output.u_pu == doctest::Approx(0.4));
-        CHECK(sym_node_output.u_angle == doctest::Approx(0.0));
+        CHECK(node_result_0.id == 0);
+        CHECK(node_result_0.energized == 1);
+        CHECK(node_result_0.u == doctest::Approx(40.0));
+        CHECK(node_result_0.u_pu == doctest::Approx(0.4));
+        CHECK(node_result_0.u_angle == doctest::Approx(0.0));
     }
 
     SUBCASE("Batch power flow") {
         PGM_calculate(hl, model, opt, 1, output_type_names.data(), sym_output_data.data(),  // basic parameters
-                      1, 2, update_type_names.data(), sizes_per_batch.data(), indptrs_per_type.data(),
+                      2, 2, update_type_names.data(), sizes_per_batch.data(), indptrs_per_type.data(),
                       update_data.data());  // batch parameters
         CHECK(PGM_err_code(hl) == 0);
-        CHECK(sym_node_output.id == 0);
-        CHECK(sym_node_output.energized == 1);
-        CHECK(sym_node_output.u == doctest::Approx(40.0));
-        CHECK(sym_node_output.u_pu == doctest::Approx(0.4));
-        CHECK(sym_node_output.u_angle == doctest::Approx(0.0));
+        CHECK(node_result_0.id == 0);
+        CHECK(node_result_0.energized == 1);
+        CHECK(node_result_0.u == doctest::Approx(40.0));
+        CHECK(node_result_0.u_pu == doctest::Approx(0.4));
+        CHECK(node_result_0.u_angle == doctest::Approx(0.0));
+        CHECK(node_result_1.id == 0);
+        CHECK(node_result_1.energized == 1);
+        CHECK(node_result_1.u == doctest::Approx(70.0));
+        CHECK(node_result_1.u_pu == doctest::Approx(0.7));
+        CHECK(node_result_1.u_angle == doctest::Approx(0.0));
     }
 
     SUBCASE("Construction error") {
@@ -140,18 +151,23 @@ TEST_CASE("C API Model") {
     }
 
     SUBCASE("Batch calculation error") {
-        // not converging
-        PGM_set_max_iter(hl, opt, 3);
-        PGM_set_calculation_method(hl, opt, PGM_iterative_current);
-        load_update.p_specified = 500.0;        
+        // wrong id
+        load_updates[1].id = 5;
         PGM_calculate(hl, model, opt, 1, output_type_names.data(), sym_output_data.data(),  // basic parameters
-                      1, 2, update_type_names.data(), sizes_per_batch.data(), indptrs_per_type.data(),
+                      2, 2, update_type_names.data(), sizes_per_batch.data(), indptrs_per_type.data(),
                       update_data.data());  // batch parameters
+        // failed in batch 1
         CHECK(PGM_err_code(hl) == 2);
         CHECK(PGM_n_failed_batches(hl) == 1);
         CHECK(PGM_failed_batches(hl)[0] == 1);
         std::string err_msg{PGM_batch_errs(hl)[0]};
-        CHECK(err_msg.find("Iteration failed to converge after") != std::string::npos);
+        CHECK(err_msg.find("The id cannot be found:") != std::string::npos);
+        // valid results for batch 0
+        CHECK(node_result_0.id == 0);
+        CHECK(node_result_0.energized == 1);
+        CHECK(node_result_0.u == doctest::Approx(40.0));
+        CHECK(node_result_0.u_pu == doctest::Approx(0.4));
+        CHECK(node_result_0.u_angle == doctest::Approx(0.0));
     }
 }
 
