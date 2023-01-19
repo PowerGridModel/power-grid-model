ff09022515798de30677409a70c2d8e0cc8a789c
Bram Stoeller <bram.stoeller@alliander.com>
2023-01-19 15:18:50 +0100

    Apparently kwargs is a tuple in older python/pytest versions
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- tests/unit/validation/test_validation_functions.py
+++ tests/unit/validation/test_validation_functions.py
@@ -404,16 +404,11 @@ def test_validate_generic_power_sensor__terminal_types(
 
     # Assert
     for call in all_valid_ids.call_args_list:
-        print(
-            ref_component,
-            measured_terminal_type,
-            call.kwargs,
-        )
         kwargs = dict(call.kwargs)
         if kwargs.get("measured_terminal_type") == measured_terminal_type:
-            if isinstance(call.kwargs["ref_components"], str) and ref_component == kwargs["ref_components"]:
+            if isinstance(kwargs["ref_components"], str) and ref_component == kwargs["ref_components"]:
                 return
-            if isinstance(call.kwargs["ref_components"], list) and ref_component in kwargs["ref_components"]:
+            if isinstance(kwargs["ref_components"], list) and ref_component in kwargs["ref_components"]:
                 return
 
     raise AssertionError(
