399757221359217b9fe1276335d09fd16f98393a
Tony Xiang <tony.xiang@alliander.com>
2023-01-27 11:06:18 +0100

    start example
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c_example/main.c
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+// example file for power grid model C API
+
+#include <stdio.h>
+#include "power_grid_model_c.h"
+
+
+int main(int argc, char** argv){
+    (void)(argc);
+    (void)(argv);
+    printf("%s", "This is an example to call the C-API of Power grid Model.\n");
+    return 0;
+}
