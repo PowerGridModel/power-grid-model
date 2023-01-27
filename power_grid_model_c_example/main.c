bebe85bd51f161d711c347de85bfab019a110627
Tony Xiang <tony.xiang@alliander.com>
2023-01-27 11:17:03 +0100

    add example
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c_example/main.c
+++ power_grid_model_c_example/main.c
@@ -5,12 +5,19 @@
 // example file for power grid model C API
 
 #include <stdio.h>
+
 #include "power_grid_model_c.h"
 
-
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     (void)(argc);
     (void)(argv);
     printf("%s", "This is an example to call the C-API of Power grid Model.\n");
+
+    // create handle
+    PGM_Handle* handle = PGM_create_handle();
+
+    // release all the resources
+    PGM_destroy_handle(handle);
+
     return 0;
 }
