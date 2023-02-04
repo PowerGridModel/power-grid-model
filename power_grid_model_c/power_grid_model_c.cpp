95540f49f931d557331000b8612fa2811e02742e
Tony Xiang <tony.xiang@alliander.com>
2023-02-04 22:18:34 +0100

    use decltype(auto)
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model_c.cpp
+++ power_grid_model_c/power_grid_model_c.cpp
@@ -68,13 +68,14 @@ std::map<std::string, std::vector<std::string>> list_of_classes() {
 }
 template <class Functor>
 auto call_with_bound(PGM_Handle* handle, Functor func) -> std::invoke_result_t<Functor> {
+    static std::remove_cv_t<std::invoke_result_t<Functor>> const empty{};
     try {
         return func();
     }
     catch (std::out_of_range& e) {
         handle->err_code = PGM_regular_error;
         handle->err_msg = std::string(e.what()) + "\n You supplied wrong name and/or index!\n";
-        return std::invoke_result_t<Functor>{};
+        return empty;
     }
 }
 
@@ -124,52 +125,52 @@ PGM_Idx PGM_meta_n_datasets(PGM_Handle*) {
 }
 char const* PGM_meta_dataset_name(PGM_Handle* handle, PGM_Idx idx) {
     static auto const dataset_list = list_of_datasets();
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return dataset_list.at(idx).c_str();
     });
 }
 // class
 PGM_Idx PGM_meta_n_components(PGM_Handle* handle, char const* dataset) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return (Idx)pgm_meta.at(dataset).size();
     });
 }
 char const* PGM_meta_component_name(PGM_Handle* handle, char const* dataset, PGM_Idx idx) {
     static auto const class_list = list_of_classes();
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return class_list.at(dataset).at(idx).c_str();
     });
 }
 size_t PGM_meta_component_size(PGM_Handle* handle, char const* dataset, char const* component) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component).size;
     });
 }
 size_t PGM_meta_component_alignment(PGM_Handle* handle, char const* dataset, char const* component) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component).alignment;
     });
 }
 // attributes
 PGM_Idx PGM_meta_n_attributes(PGM_Handle* handle, char const* dataset, char const* component) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return (Idx)pgm_meta.at(dataset).at(component).attributes.size();
     });
 }
 char const* PGM_meta_attribute_name(PGM_Handle* handle, char const* dataset, char const* component, PGM_Idx idx) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component).attributes.at(idx).name.c_str();
     });
 }
 char const* PGM_meta_attribute_ctype(PGM_Handle* handle, char const* dataset, char const* component,
                                      char const* attribute) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component).get_attr(attribute).ctype.c_str();
     });
 }
 size_t PGM_meta_attribute_offset(PGM_Handle* handle, char const* dataset, char const* component,
                                  char const* attribute) {
-    return call_with_bound(handle, [&]() {
+    return call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component).get_attr(attribute).offset;
     });
 }
@@ -179,7 +180,7 @@ int PGM_is_little_endian(PGM_Handle*) {
 
 // buffer control
 void* PGM_create_buffer(PGM_Handle* handle, char const* dataset, char const* component, PGM_Idx size) {
-    auto const& data_class = call_with_bound(handle, [&]() {
+    auto const& data_class = call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component);
     });
     if (data_class.name == "") {
@@ -199,7 +200,7 @@ void PGM_destroy_buffer(void* ptr) {
 #endif
 }
 void PGM_buffer_set_nan(PGM_Handle* handle, char const* dataset, char const* component, void* ptr, PGM_Idx size) {
-    auto const& data_class = call_with_bound(handle, [&]() {
+    auto const& data_class = call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component);
     });
     if (data_class.name == "") {
@@ -213,10 +214,10 @@ void PGM_buffer_set_nan(PGM_Handle* handle, char const* dataset, char const* com
 template <bool is_get, class BufferPtr, class ValuePtr>
 void buffer_get_set_value(PGM_Handle* handle, char const* dataset, char const* component, char const* attribute,
                           BufferPtr buffer_ptr, ValuePtr value_ptr, PGM_Idx size, PGM_Idx stride) {
-    auto const& data_class = call_with_bound(handle, [&]() {
+    auto const& data_class = call_with_bound(handle, [&]() -> decltype(auto) {
         return pgm_meta.at(dataset).at(component);
     });
-    auto const& attr = call_with_bound(handle, [&]() {
+    auto const& attr = call_with_bound(handle, [&]() -> decltype(auto) {
         return data_class.get_attr(attribute);
     });
     if (attr.name == "") {
