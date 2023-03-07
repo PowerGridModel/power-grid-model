1f112cb26c18616e2943b108757ec3e99e223068
Peter Salemink <peter.salemink@alliander.com>
2023-03-07 14:59:52 +0100

    add short_circuit.hpp
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

---
+++ include/power_grid_model/component/short_circuit.hpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_COMPONENT_SHORT_CIRCUIT_HPP
+#define POWER_GRID_MODEL_COMPONENT_SHORT_CIRCUIT_HPP
+
+namespace power_grid_model {}
+
+#endif
