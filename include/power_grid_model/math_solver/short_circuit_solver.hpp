e43a0a26600baba765e0c33a1d1df8f40155f43a
Peter Salemink <peter.salemink@alliander.com>
2023-03-16 11:35:35 +0100

    empty skeleton
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

---
+++ include/power_grid_model/math_solver/short_circuit_solver.hpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_MATH_SOLVER_SHORT_CIRCUIT_SOLVER_HPP
+#define POWER_GRID_MODEL_MATH_SOLVER_SHORT_CIRCUIT_SOLVER_HPP
+
+namespace power_grid_model {
+
+// hide implementation in inside namespace
+namespace math_model_impl {}
+
+}  // namespace power_grid_model
+
+#endif
