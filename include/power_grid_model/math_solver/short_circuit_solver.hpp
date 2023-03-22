5c82efe94a16618f593614bfc7470ac477127545
Peter Salemink <peter.salemink@alliander.com>
2023-03-22 11:41:07 +0100

    empty solver class
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- include/power_grid_model/math_solver/short_circuit_solver.hpp
+++ include/power_grid_model/math_solver/short_circuit_solver.hpp
@@ -6,10 +6,23 @@
 #ifndef POWER_GRID_MODEL_MATH_SOLVER_SHORT_CIRCUIT_SOLVER_HPP
 #define POWER_GRID_MODEL_MATH_SOLVER_SHORT_CIRCUIT_SOLVER_HPP
 
+#include "../calculation_parameters.hpp"
+#include "y_bus.hpp"
+
 namespace power_grid_model {
 
 // hide implementation in inside namespace
-namespace math_model_impl {}
+namespace math_model_impl {
+
+// solver
+template <bool sym>
+class ShortCircuitSolver {
+   public:
+    ShortCircuitSolver(YBus<sym> const& y_bus, std::shared_ptr<MathModelTopology const> const& topo_ptr) {
+    }
+};
+
+}  // namespace math_model_impl
 
 }  // namespace power_grid_model
 
