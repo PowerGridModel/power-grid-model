c75935bd85171d9c86e09d6b0213ff41026925b9
Martijn Govers <Martijn.Govers@Alliander.com>
2023-04-12 14:26:29 +0200

    export PGM C-api

---
+++ cmake/pgm_version.cmake
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# CMakeList.txt : CMake project for power_grid_model, include source and define
+# project specific logic here.
+#
+cmake_minimum_required (VERSION 3.9)
+
+file(READ "${CMAKE_SOURCE_DIR}/VERSION" PGM_VERSION)
