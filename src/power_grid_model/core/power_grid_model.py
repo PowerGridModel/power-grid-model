be08c63bcadb10e5b31e21da518cb1e7958f4f8e
ChenguangWang-Sam <samwangchenguang@gmail.com>
2023-06-28 13:37:33 +0200

    change indentation as two taps

--- src/power_grid_model/core/power_grid_model.py
+++ src/power_grid_model/core/power_grid_model.py
@@ -286,13 +286,14 @@ class PowerGridModel:
 
         Args:
             symmetric (bool): Whether to perform a three-phase symmetric calculation.      
-                    True: Three-phase symmetric calculation, even for asymmetric loads/generations.
-                    False: Three-phase asymmetric calculation.        
+                    Value:
+                            True: Three-phase symmetric calculation, even for asymmetric loads/generations.
+                            False: Three-phase asymmetric calculation.        
             error_tolerance: Error tolerance for voltage in p.u., applicable only when iterative=True.
             max_iterations: Maximum number of iterations, applicable only when iterative=True.
             calculation_method: The calculation method to use.
-                - Newton_raphson: Use Newton-Raphson iterative method (default).
-                - Linear: Use linear method.
+                    - Newton_raphson: Use Newton-Raphson iterative method (default).
+                    - Linear: Use linear method.
             update_data: Data for batch calculation with batch update.
               None: Calculate power flow once with the current model attributes. A dictionary for batch calculation with batch update:
               key: Component type name to be updated in batch.
