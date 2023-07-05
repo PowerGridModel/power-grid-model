d7bea1fdebaede3131ec51d4eb5eef4c8d14568a
Tony Xiang <tony.xiang@alliander.com>
2023-07-05 15:56:49 +0200

    documentation
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model_c/include/power_grid_model_c.h
+++ power_grid_model_c/power_grid_model_c/include/power_grid_model_c.h
@@ -5,7 +5,15 @@
 /**
  * @mainpage Power Grid Model C API Documentation
  * This gives an overview of C API.
- * All the exported functions are declared in power_grid_model_c.h
+ * All the exported functions are included in power_grid_model_c.h
+ * 
+ * Users can also include individual headers for individual functions
+ *     - basics.h: type and enum definition
+ *     - buffer.h: functions with buffer creation, release, set and get value
+ *     - handle.h: functions with error handling
+ *     - meta_data.h: functions with meta data
+ *     - model.h: functions with create, release, run calculation of model
+ *     - options.h: functions with setting the calculation options
  */
 
 #pragma once
