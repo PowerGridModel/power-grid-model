19af3b652ab6bb597f8b84bf57dba40c8a914712
Tony Xiang <tony.xiang@alliander.com>
2023-07-05 15:07:48 +0200

    start cpp file
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c/power_grid_model_c/include/handle.hpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_HANDLE_HPP
+#define POWER_GRID_MODEL_HANDLE_HPP
+
+#ifndef PGM_DLL_EXPORTS
+#define PGM_DLL_EXPORTS
+#endif
+
+#include "power_grid_model_c/handle.h"
+
+#include <power_grid_model/power_grid_mode.hpp>
+
+
+#endif
