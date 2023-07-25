7bf5746759b9151246ccfe27a5366402af21897d
Laurynas Jagutis <laurynas.jagutis@alliander.com>
2023-07-25 13:26:32 +0300

    fix one const code smell
    Signed-off-by: Laurynas Jagutis <laurynas.jagutis@alliander.com>

--- tests/cpp_unit_tests/test_sparse_mapping.cpp
+++ tests/cpp_unit_tests/test_sparse_mapping.cpp
@@ -9,7 +9,7 @@
 namespace power_grid_model {
 
 TEST_CASE("Test sparse mapping") {
-    IdxVector idx_B_in_A{3, 5, 2, 1, 1, 2};
+    IdxVector const idx_B_in_A{3, 5, 2, 1, 1, 2};
     SparseMapping mapping{{0, 0, 2, 4, 5, 5, 6, 6}, {3, 4, 2, 5, 0, 1}};
     SparseMapping mapping_2 = build_sparse_mapping(idx_B_in_A, 7);
     ;
