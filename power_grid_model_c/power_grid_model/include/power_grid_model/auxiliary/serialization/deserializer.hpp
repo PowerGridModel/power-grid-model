d1024cdb0d343afb8eb3cc391fe0824356f24694
Tony Xiang <tony.xiang@alliander.com>
2023-08-02 14:54:22 +0200

    add deserializer
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/deserializer.hpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_DESERIALIZER_HPP
+#define POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_DESERIALIZER_HPP
+
+#include "../../power_grid_model.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <msgpack.hpp>
+
+namespace power_grid_model::meta_data {
+
+class Deserializer {
+   public:
+};
+
+}  // namespace power_grid_model::meta_data
+
+#endif
