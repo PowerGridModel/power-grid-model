0abf23d4e12df698c85de923c355860ca080c2f4
Tony Xiang <tony.xiang@alliander.com>
2023-08-02 14:29:24 +0200

    start serializer
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/serializer.hpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_SERIALIZER_HPP
+#define POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_SERIALIZER_HPP
+
+#include "../../power_grid_model.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <msgpack.hpp>
+
+namespace power_grid_model::meta_data {
+
+class Serializer {};
+
+}  // namespace power_grid_model::meta_data
+
+#endif
---
+++ tests/cpp_unit_tests/test_serialization.cpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include <power_grid_model/auxiliary/serialization/serializer.hpp>
+
+#include <doctest/doctest.h>
+
+namespace power_grid_model {
+
+TEST_CASE("Serializer") {
+}
+
+}  // namespace power_grid_model
