8f93faee6d0bceb6d644c9cd3e5a0b17174d4800
Tony Xiang <tony.xiang@alliander.com>
2023-08-02 15:02:36 +0200

    start deserialize
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/deserializer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/deserializer.hpp
@@ -16,6 +16,21 @@ namespace power_grid_model::meta_data {
 
 class Deserializer {
    public:
+    void deserialize_from_json(char const* json_string) {
+        nlohmann::json const json_document = nlohmann::json::parse(json_string);
+        std::vector<char> msgpack_data;
+        json_document.to_msgpack(msgpack_data);
+        deserialize_from_msgpack(msgpack_data.data(), msgpack_data.size());
+    }
+
+    void deserialize_from_msgpack(char const* data, size_t length) {
+        handle_ = msgpack::unpack(data, length);
+        object_ = handle_.get();
+    }
+
+   private:
+    msgpack::object_handle handle_;
+    msgpack::object object_;
 };
 
 }  // namespace power_grid_model::meta_data
