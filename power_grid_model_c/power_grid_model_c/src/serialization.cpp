b6329a3799520c4264e692e473782b8cf45f29e7
Tony Xiang <tony.xiang@alliander.com>
2023-08-09 19:53:29 +0200

    add c-api
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c/power_grid_model_c/src/serialization.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#define PGM_DLL_EXPORTS
+#include "power_grid_model_c/basics.h"
+
+#include <power_grid_model/auxiliary/serialization/deserializer.hpp>
+#include <power_grid_model/auxiliary/serialization/serializer.hpp>
