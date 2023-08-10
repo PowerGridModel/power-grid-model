6c81937c27d3a9ffb80d1ae21245c8452cf4dd70
Tony Xiang <tony.xiang@alliander.com>
2023-08-10 12:33:38 +0200

    start serializer
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/serializer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/serializer.hpp
@@ -6,16 +6,43 @@
 #ifndef POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_SERIALIZER_HPP
 #define POWER_GRID_MODEL_AUXILIARY_SERIALIZTION_SERIALIZER_HPP
 
+#include "../../exception.hpp"
 #include "../../power_grid_model.hpp"
+#include "../meta_data.hpp"
+#include "../meta_data_gen.hpp"
 
 #include <nlohmann/json.hpp>
 
 #include <msgpack.hpp>
 
+#include <span>
+#include <string_view>
+
 namespace power_grid_model::meta_data {
 
 class Serializer {
   public:
+    // not copyable
+    Serializer(Serializer const&) = delete;
+    Serializer& operator=(Serializer const&) = delete;
+    // movable
+    Serializer(Serializer&&) = default;
+    Serializer& operator=(Serializer&&) = default;
+    // destructor
+    ~Serializer() = default;
+
+    Serializer(std::string_view dataset, bool is_batch, Idx batch_size, Idx n_components, char const** components,
+               Idx const* elements_per_scenario, Idx const** indptrs, void const** data)
+        : dataset_{&meta_data().get_dataset(dataset)},
+          is_batch_{is_batch},
+          batch_size_{batch_size},
+          n_components_{n_components} {}
+
+  private:
+    MetaDataset const* dataset_;
+    bool is_batch_;
+    Idx batch_size_;
+    Idx n_components_;
 };
 
 } // namespace power_grid_model::meta_data
