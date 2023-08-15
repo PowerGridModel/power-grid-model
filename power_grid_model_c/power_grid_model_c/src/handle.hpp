17fe06d6a1995b9c4324d86f385e0d5b7f9bf814
Tony Xiang <tony.xiang@alliander.com>
2023-08-15 15:31:41 +0200

    [skip ci] start error handler for model
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model_c/src/handle.hpp
+++ power_grid_model_c/power_grid_model_c/src/handle.hpp
@@ -29,13 +29,14 @@ struct PGM_Handle {
 template <class Exception = std::exception, class Functor>
 auto call_with_catch(PGM_Handle* handle, Functor func, PGM_Idx error_code, std::string_view extra_msg = {})
     -> std::invoke_result_t<Functor> {
-    static std::remove_cvref_t<std::invoke_result_t<Functor>> const empty{};
+    using ReturnValueType = std::remove_cvref_t<std::invoke_result_t<Functor>>;
+    static std::conditional_t<std::is_void_v<ReturnValueType>, int, ReturnValueType> const empty{};
     try {
         return func();
     } catch (Exception const& e) {
         handle->err_code = error_code;
         handle->err_msg = std::string(e.what()) + std::string(extra_msg);
-        return empty;
+        return static_cast<ReturnValueType>(empty);
     }
 }
 
