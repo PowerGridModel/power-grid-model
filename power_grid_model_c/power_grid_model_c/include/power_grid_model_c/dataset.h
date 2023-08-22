e062303b1417b43ef69e26b3152102352144d356
Tony Xiang <tony.xiang@alliander.com>
2023-08-22 15:03:26 +0200

    [skip ci] start c api
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ power_grid_model_c/power_grid_model_c/include/power_grid_model_c/dataset.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ * @brief header file which includes dataset handling functions
+ *
+ */
+
+#pragma once
+#ifndef POWER_GRID_MODEL_C_DATASET_H
+#define POWER_GRID_MODEL_C_DATASET_H
+
+#include "basics.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ power_grid_model_c/power_grid_model_c/src/dataset.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#define PGM_DLL_EXPORTS
+#include "forward_declaration.hpp"
+
+#include "power_grid_model_c/dataset.h"
+
+#include <power_grid_model/auxiliary/dataset_handler.hpp>
+#include <power_grid_model/auxiliary/meta_data.hpp>
