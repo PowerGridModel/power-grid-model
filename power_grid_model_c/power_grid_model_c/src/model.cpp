bd77d4202c66743bb5146c9c74d2bc8266df9c5b
Tony Xiang <tony.xiang@alliander.com>
2023-09-25 10:01:00 +0200

    refactor c api
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model_c/src/model.cpp
+++ power_grid_model_c/power_grid_model_c/src/model.cpp
@@ -10,6 +10,7 @@
 #include "handle.hpp"
 #include "options.hpp"
 
+#include <power_grid_model/auxiliary/dataset_handler.hpp>
 #include <power_grid_model/auxiliary/meta_data_gen.hpp>
 #include <power_grid_model/main_model.hpp>
 #include <power_grid_model/power_grid_model.hpp>
@@ -27,30 +28,23 @@ struct PGM_PowerGridModel : public MainModel {
 };
 
 // create model
-PGM_PowerGridModel* PGM_create_model(PGM_Handle* handle, double system_frequency, PGM_Idx n_components,
-                                     char const** components, PGM_Idx const* component_sizes,
-                                     RawDataConstPtr* input_data) {
-    ConstDataset dataset{};
-    for (Idx i = 0; i != n_components; ++i) {
-        dataset[components[i]] = ConstDataPointer{input_data[i], component_sizes[i]};
-    }
+PGM_PowerGridModel* PGM_create_model(PGM_Handle* handle, double system_frequency,
+                                     PGM_ConstDataset const* input_dataset) {
     return call_with_catch(
         handle,
-        [system_frequency, &dataset] {
-            return new PGM_PowerGridModel{system_frequency, dataset, 0};
+        [system_frequency, input_dataset] {
+            return new PGM_PowerGridModel{system_frequency, input_dataset->export_dataset<true>(), 0};
         },
         PGM_regular_error);
 }
 
 // update model
-void PGM_update_model(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_Idx n_components, char const** components,
-                      PGM_Idx const* component_sizes, RawDataConstPtr* update_data) {
-    ConstDataset dataset{};
-    for (Idx i = 0; i != n_components; ++i) {
-        dataset[components[i]] = ConstDataPointer{update_data[i], component_sizes[i]};
-    }
+void PGM_update_model(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_ConstDataset const* update_dataset) {
     call_with_catch(
-        handle, [model, &dataset] { model->update_component<MainModel::permanent_update_t>(dataset); },
+        handle,
+        [model, update_dataset] {
+            model->update_component<MainModel::permanent_update_t>(update_dataset->export_dataset<true>());
+        },
         PGM_regular_error);
 }
 
@@ -69,64 +63,51 @@ void PGM_get_indexer(PGM_Handle* handle, PGM_PowerGridModel const* model, char c
 }
 
 // run calculation
-void PGM_calculate(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_Options const* opt, PGM_Idx n_output_components,
-                   char const** output_components, RawDataPtr* output_data, PGM_Idx n_scenarios,
-                   PGM_Idx n_update_components, char const** update_components,
-                   PGM_Idx const* n_component_elements_per_scenario, PGM_Idx const** indptrs_per_component,
-                   RawDataConstPtr* update_data) {
+void PGM_calculate(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_Options const* opt,
+                   PGM_MutableDataset const* output_dataset, PGM_ConstDataset const* batch_dataset) {
     PGM_clear_error(handle);
-    std::map<std::string, Idx> const n_component = model->all_component_count();
-    // prepare output dataset
-    Dataset output_dataset{};
-    // set n_output_batch to one for single calculation
-    Idx const n_output_scenarios = std::max(Idx{1}, n_scenarios);
-    for (Idx i = 0; i != n_output_components; ++i) {
-        auto const found = n_component.find(output_components[i]);
-        if (found != n_component.cend()) {
-            output_dataset[output_components[i]] =
-                MutableDataPointer{output_data[i], n_output_scenarios, found->second};
-        }
+    // check dataset integrity
+    if (batch_dataset && (!batch_dataset->is_batch() || !output_dataset->is_batch())) {
+        handle->err_code = PGM_batch_error;
+        handle->err_msg = "If batch_dataset is provided. Both batch_dataset and output_dataset should be a batch!\n";
+        return;
     }
-    // prepare update dataset
-    ConstDataset update_dataset{};
-    for (Idx i = 0; i != n_update_components; ++i) {
-        if (n_component_elements_per_scenario[i] < 0) {
-            // use indptr as sparse batch
-            update_dataset[update_components[i]] =
-                ConstDataPointer(update_data[i], indptrs_per_component[i], n_scenarios);
-        } else {
-            // use dense batch
-            update_dataset[update_components[i]] =
-                ConstDataPointer(update_data[i], n_scenarios, n_component_elements_per_scenario[i]);
-        }
-    }
+
+    Dataset const exported_output_dataset = output_dataset->export_dataset<false>();
+    auto const exported_update_dataset = batch_dataset ? batch_dataset->export_dataset<true>() : ConstDataset{};
+
     // call calculation
     try {
         auto const calculation_method = static_cast<CalculationMethod>(opt->calculation_method);
         switch (opt->calculation_type) {
         case PGM_power_flow:
             if (opt->symmetric != 0) {
-                handle->batch_parameter = model->calculate_power_flow<true>(
-                    opt->err_tol, opt->max_iter, calculation_method, output_dataset, update_dataset, opt->threading);
+                handle->batch_parameter =
+                    model->calculate_power_flow<true>(opt->err_tol, opt->max_iter, calculation_method,
+                                                      exported_output_dataset, exported_update_dataset, opt->threading);
             } else {
                 handle->batch_parameter = model->calculate_power_flow<false>(
-                    opt->err_tol, opt->max_iter, calculation_method, output_dataset, update_dataset, opt->threading);
+                    opt->err_tol, opt->max_iter, calculation_method, exported_output_dataset, exported_update_dataset,
+                    opt->threading);
             }
             break;
         case PGM_state_estimation:
             if (opt->symmetric != 0) {
                 handle->batch_parameter = model->calculate_state_estimation<true>(
-                    opt->err_tol, opt->max_iter, calculation_method, output_dataset, update_dataset, opt->threading);
+                    opt->err_tol, opt->max_iter, calculation_method, exported_output_dataset, exported_update_dataset,
+                    opt->threading);
             } else {
                 handle->batch_parameter = model->calculate_state_estimation<false>(
-                    opt->err_tol, opt->max_iter, calculation_method, output_dataset, update_dataset, opt->threading);
+                    opt->err_tol, opt->max_iter, calculation_method, exported_output_dataset, exported_update_dataset,
+                    opt->threading);
             }
             break;
         case PGM_short_circuit: {
             auto const short_circuit_voltage_scaling =
                 static_cast<ShortCircuitVoltageScaling>(opt->short_circuit_voltage_scaling);
-            handle->batch_parameter = model->calculate_short_circuit(short_circuit_voltage_scaling, calculation_method,
-                                                                     output_dataset, update_dataset, opt->threading);
+            handle->batch_parameter =
+                model->calculate_short_circuit(short_circuit_voltage_scaling, calculation_method,
+                                               exported_output_dataset, exported_update_dataset, opt->threading);
             break;
         }
         default:
