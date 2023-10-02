903c3065f223220c1366ddb88811e2d56afc6d95
Nitish Bharambe <nitish.bharambe@alliander.com>
2023-10-02 17:10:08 +0200

    add dense vector
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/sparse_idx_vector.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/sparse_idx_vector.hpp
@@ -22,6 +22,9 @@ objects 6 is coupled to index 2
 
 Another intuitive way to look at this for python developers is like list of lists: [[0, 1, 2], [3, 4, 5], [6]].
 
+DenseIdxVector is a vector of element to group. ie. [0, 1, 1, 4] would denote that [[0], [1, 2], [], [], [3]]
+The input, ie. [0, 1, 3] should be strictly increasing
+
 */
 
 namespace power_grid_model::detail {
@@ -30,11 +33,46 @@ class SparseIdxVector {
   public:
     explicit SparseIdxVector(IdxVector indptr) : indptr_(indptr) {}
 
-    auto group(Idx group) { return boost::iterator_range<IdxCount>(indptr_[group], indptr_[group + 1]); }
+    auto get_element_range(Idx group) { return boost::iterator_range<IdxCount>(indptr_[group], indptr_[group + 1]); }
+
+    Idx get_group(Idx element) {
+        return static_cast<Idx>(std::upper_bound(indptr_.begin(), indptr_.end(), element) - indptr_.begin());
+    }
 
   private:
     IdxVector indptr_;
 };
+
+class DenseIdxVector {
+  public:
+    explicit DenseIdxVector(IdxVector dense_idx_vector) : dense_idx_vector_(dense_idx_vector) {}
+
+    auto get_group(Idx element) { return dense_idx_vector_[element]; }
+
+    auto get_element_range(Idx group) {
+        auto dense_begin = dense_idx_vector_.begin();
+        auto dense_end = dense_idx_vector_.end();
+        auto range_pair = std::equal_range(dense_begin, dense_end, group);
+        return boost::iterator_range<IdxCount>(range_pair.first - dense_begin, range_pair.second - dense_begin);
+    }
+
+  private:
+    IdxVector dense_idx_vector_;
+};
+
+// template<class ...T>
+// requires(same_as<T, SparseIdxVector>...)
+// auto iter_element_groups(T const& v1...)
+
+// for (auto cosnt [bus, group_1_range, group_2_range]: iter_element_groups(v1, v2)) {
+//   // do something with bus
+//   for (auto const source: group_1_range) {
+//     // sowething source
+//   }
+//   for (auto const load: group_2_range) {
+//     // something load
+//   }
+// }
 
 } // namespace power_grid_model::detail
 
--- tests/cpp_unit_tests/test_sparse_idx_vector.cpp
+++ tests/cpp_unit_tests/test_sparse_idx_vector.cpp
@@ -13,21 +13,46 @@ TEST_CASE("Sparse idx data strucuture for topology") {
     IdxVector expected_groups{1, 1, 1, 3, 3, 3, 4};
     IdxVector expected_keys{0, 1, 2, 3, 4, 5, 6};
 
-    SUBCASE("Group iterator values") {
+    IdxVector sample_dense_vector{1, 1, 1, 3, 3, 3, 4};
+    DenseIdxVector dense_idx_vector{sample_dense_vector};
+
+    SUBCASE("Sparse Idx vector") {
         // Check each group
         std::vector<IdxCount> actual_idx_counts{};
-        for (size_t i = 0; i < 6; i++) {
-            size_t range_size = sample_indptr[i + 1] - sample_indptr[i];
+        for (size_t group_number = 0; group_number < 6; group_number++) {
+            size_t range_size = sample_indptr[group_number + 1] - sample_indptr[group_number];
             actual_idx_counts.clear();
             actual_idx_counts.resize(range_size);
-            auto group_i = sparse_idx_vector.group(i);
+            auto group_i = sparse_idx_vector.get_element_range(group_number);
             std::copy(group_i.begin(), group_i.end(), actual_idx_counts.begin());
-            if (range_size != 0) {
-                CHECK(actual_idx_counts.front() == IdxCount{sample_indptr[i]});
-                CHECK(actual_idx_counts.back() == IdxCount{sample_indptr[i + 1] - 1});
+            if (range_size == 0)    {
+                CHECK(actual_idx_counts.empty());
+            } else {
+                CHECK(actual_idx_counts.front() == IdxCount{sample_indptr[group_number]});
+                CHECK(actual_idx_counts.back() == IdxCount{sample_indptr[group_number + 1] - 1});
             }
         }
     }
+
+    SUBCASE("Dense Idx vector") {
+        // Check each group
+        std::vector<IdxCount> actual_idx_counts{};
+        for (size_t group_number = 0; group_number < 6; group_number++) {
+            size_t range_size = sample_indptr[group_number + 1] - sample_indptr[group_number];
+            actual_idx_counts.clear();
+            actual_idx_counts.resize(range_size);
+            auto group_i = dense_idx_vector.get_element_range(group_number);
+            std::copy(group_i.begin(), group_i.end(), actual_idx_counts.begin());
+            if (range_size == 0)    {
+                CHECK(actual_idx_counts.empty());
+            } else {
+                CHECK(actual_idx_counts.front() == IdxCount{sample_indptr[group_number]});
+                CHECK(actual_idx_counts.back() == IdxCount{sample_indptr[group_number + 1] - 1});
+            }
+        }
+    }
+
+
 }
 
 } // namespace power_grid_model::detail
