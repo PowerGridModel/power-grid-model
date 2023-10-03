a3671223fb0ce025fe221bb8258d59a065bf296a
Peter Salemink <peter.salemink@alliander.com>
2023-10-03 13:14:47 +0200

    similar add_sources, linear PF, SC
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/linear_pf_solver.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/linear_pf_solver.hpp
@@ -109,30 +109,30 @@ template <bool sym> class LinearPFSolver {
         IdxVector const& load_gen_bus_idxptr = *load_gen_bus_indptr_;
         IdxVector const& source_bus_indptr = *source_bus_indptr_;
         for (Idx bus_number = 0; bus_number != n_bus_; ++bus_number) {
-            Idx const data_sequence = bus_entry[bus_number];
-            add_loads(load_gen_bus_idxptr, bus_number, input, data_sequence);
-            add_sources(source_bus_indptr, bus_number, y_bus, input, output, data_sequence);
+            Idx const diagonal_position = bus_entry[bus_number];
+            auto& diagonal_element = mat_data_[diagonal_position];
+            auto& u_bus = output.u[bus_number];
+            add_loads(load_gen_bus_idxptr, bus_number, input, diagonal_element);
+            add_sources(source_bus_indptr, bus_number, y_bus, input, diagonal_element, u_bus);
         }
     }
 
     void add_loads(IdxVector const& load_gen_bus_idxptr, Idx const& bus_number, PowerFlowInput<sym> const& input,
-                   Idx const& data_sequence) {
+                   ComplexTensor<sym>& diagonal_element) {
         for (Idx load_number = load_gen_bus_idxptr[bus_number]; load_number != load_gen_bus_idxptr[bus_number + 1];
              ++load_number) {
             // YBus_diag += -conj(S_base)
-            add_diag(mat_data_[data_sequence], -conj(input.s_injection[load_number]));
+            add_diag(diagonal_element, -conj(input.s_injection[load_number]));
         }
     }
 
     void add_sources(IdxVector const& source_bus_indptr, Idx const& bus_number, YBus<sym> const& y_bus,
-                     PowerFlowInput<sym> const& input, MathOutput<sym>& output, Idx const& data_sequence) {
+                     PowerFlowInput<sym> const& input, ComplexTensor<sym>& diagonal_element, ComplexValue<sym>& u_bus) {
         for (Idx source_number = source_bus_indptr[bus_number]; source_number != source_bus_indptr[bus_number + 1];
              ++source_number) {
-            // YBus_diag += Y_source
-            mat_data_[data_sequence] += y_bus.math_model_param().source_param[source_number];
-            // rhs += Y_source_j * U_ref_j
-            output.u[bus_number] += dot(y_bus.math_model_param().source_param[source_number],
-                                        ComplexValue<sym>{input.source[source_number]});
+            ComplexTensor<sym> const y_source = y_bus.math_model_param().source_param[source_number];
+            diagonal_element += y_source;                                           // YBus_diag += Y_source
+            u_bus += dot(y_source, ComplexValue<sym>{input.source[source_number]}); // rhs += Y_source * U_source
         }
     }
 
