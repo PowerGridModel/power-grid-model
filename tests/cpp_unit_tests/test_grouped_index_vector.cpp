d0aaf9d2a908e4f2f6d5e12bd83e064ea70a848a
Nitish Bharambe <nitish.bharambe@alliander.com>
2023-10-10 17:33:08 +0200

    add different contents
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- tests/cpp_unit_tests/test_grouped_index_vector.cpp
+++ tests/cpp_unit_tests/test_grouped_index_vector.cpp
@@ -38,28 +38,59 @@ TEST_CASE_TEMPLATE("Grouped idx data strucuture for topology", IdxVectorType, Sp
 
     auto const idx_vector = construct_from<IdxVectorType>(groups, num_groups);
 
-    // Test get_element_range
-    std::vector<IdxCount> actual_idx_counts{};
-    for (size_t group_number = 0; group_number < num_groups; group_number++) {
-        CHECK(idx_vector.get_element_range(group_number) == expected_ranges[group_number]);
-    }
-
-    // Test get_group
-    for (size_t element = 0; element < groups.size(); element++) {
-        CHECK(idx_vector.get_group(element) == groups[element]);
-    }
-
-    // Test Iteration
-    std::vector<IdxCount> actual_elements{};
-    std::vector<boost::iterator_range<IdxCount>> actual_ranges{};
-    for (auto element_range : idx_vector) {
-        actual_ranges.push_back(element_range);
-        for (auto& element : element_range) {
-            actual_elements.push_back(element);
+    SUBCASE("Grouped Idx vector functionalities") {
+        // Test get_element_range
+        std::vector<IdxCount> actual_idx_counts{};
+        for (size_t group_number = 0; group_number < num_groups; group_number++) {
+            CHECK(idx_vector.get_element_range(group_number) == expected_ranges[group_number]);
         }
+
+        // Test get_group
+        for (size_t element = 0; element < groups.size(); element++) {
+            CHECK(idx_vector.get_group(element) == groups[element]);
+        }
+
+        // Test Iteration
+        std::vector<IdxCount> actual_elements{};
+        std::vector<boost::iterator_range<IdxCount>> actual_ranges{};
+        for (auto element_range : idx_vector) {
+            actual_ranges.push_back(element_range);
+            for (auto& element : element_range) {
+                actual_elements.push_back(element);
+            }
+        }
+        CHECK(actual_elements == expected_elements);
+        CHECK(actual_ranges == expected_ranges);
     }
-    CHECK(actual_elements == expected_elements);
-    CHECK(actual_ranges == expected_ranges);
+
+    SUBCASE("Zipped 1 grouped idx vector") {
+        // Test single zipped iteration
+        std::vector<boost::iterator_range<IdxCount>> actual_ranges{};
+        for (auto [element_range] : zip_sequence(idx_vector)) {
+            actual_ranges.push_back(element_range);
+        }
+        CHECK(actual_ranges == expected_ranges);
+    }
+
+    SUBCASE("Zipped 3 same grouped idx vectors") {
+        // Create additional 2 grouped idx vectors
+        auto const idx_vector_2 = construct_from<IdxVectorType>(groups, num_groups);
+        auto const idx_vector_3 = construct_from<IdxVectorType>(groups, num_groups);
+
+        // Test 3 zipped iterations
+        std::vector<boost::iterator_range<IdxCount>> actual_ranges_1{};
+        std::vector<boost::iterator_range<IdxCount>> actual_ranges_2{};
+        std::vector<boost::iterator_range<IdxCount>> actual_ranges_3{};
+        for (auto [element_range_1, element_range_2, element_range_3] :
+             zip_sequence(idx_vector, idx_vector_2, idx_vector_3)) {
+            actual_ranges_1.push_back(element_range_1);
+            actual_ranges_2.push_back(element_range_2);
+            actual_ranges_3.push_back(element_range_3);
+        }
+        CHECK(actual_ranges_1 == expected_ranges);
+        CHECK(actual_ranges_2 == expected_ranges);
+        CHECK(actual_ranges_3 == expected_ranges);
+    }
 }
 
 template <typename first, typename second> struct TypePair {
@@ -67,8 +98,9 @@ template <typename first, typename second> struct TypePair {
     using B = second;
 };
 
-TEST_CASE_TEMPLATE("Zip iterator for grouped structure", IdxVectorTypes, TypePair<SparseIdxVector, SparseIdxVector>,
-                   TypePair<SparseIdxVector, DenseIdxVector>, TypePair<DenseIdxVector, DenseIdxVector>) {
+TEST_CASE_TEMPLATE("2 different grouped structures tests with zip iterator", IdxVectorTypes,
+                   TypePair<SparseIdxVector, SparseIdxVector>, TypePair<SparseIdxVector, DenseIdxVector>,
+                   TypePair<DenseIdxVector, SparseIdxVector>, TypePair<DenseIdxVector, DenseIdxVector>) {
     // First grouped idx vector and its expeceted elements and groups
     IdxVector const first_groups{1, 1, 1, 3, 3, 3, 4};
     Idx const first_num_groups{6};
@@ -76,10 +108,10 @@ TEST_CASE_TEMPLATE("Zip iterator for grouped structure", IdxVectorTypes, TypePai
     std::vector<IdxCount> const first_expected_elements{0, 1, 2, 3, 4, 5, 6};
 
     // Second grouped idx vector and its expeceted elements and groups
-    IdxVector const second_groups{1, 1, 1, 3, 3, 3, 4};
+    IdxVector const second_groups{1, 1, 3, 3, 4};
     Idx const second_num_groups{6};
-    std::vector<boost::iterator_range<IdxCount>> second_expected_ranges{{0, 0}, {0, 3}, {3, 3}, {3, 6}, {6, 7}, {7, 7}};
-    std::vector<IdxCount> const second_expected_elements{0, 1, 2, 3, 4, 5, 6};
+    std::vector<boost::iterator_range<IdxCount>> second_expected_ranges{{0, 0}, {0, 2}, {2, 2}, {2, 4}, {4, 5}, {5, 5}};
+    std::vector<IdxCount> const second_expected_elements{0, 1, 2, 3, 4};
 
     // Construct both grouped idx vectors
     using T1 = typename IdxVectorTypes::A;
@@ -92,6 +124,7 @@ TEST_CASE_TEMPLATE("Zip iterator for grouped structure", IdxVectorTypes, TypePai
     std::vector<IdxCount> second_actual_idx_counts{};
     std::vector<boost::iterator_range<IdxCount>> first_actual_ranges{};
     std::vector<boost::iterator_range<IdxCount>> second_actual_ranges{};
+
     for (auto const [first_group, second_group] : zip_sequence(first_idx_vector, second_idx_vector)) {
         for (auto& element : first_group) {
             first_actual_idx_counts.push_back(element);
@@ -102,6 +135,7 @@ TEST_CASE_TEMPLATE("Zip iterator for grouped structure", IdxVectorTypes, TypePai
         first_actual_ranges.push_back(first_group);
         second_actual_ranges.push_back(second_group);
     }
+
     CHECK(first_actual_idx_counts == first_expected_elements);
     CHECK(second_actual_idx_counts == second_expected_elements);
     CHECK(first_actual_ranges == first_expected_ranges);
