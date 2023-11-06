6d409f57c70929a60e73129b08cc95095a458d8a
Tony Xiang <tony.xiang@alliander.com>
2023-11-06 15:59:47 +0100

    try apple clang
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/deserializer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/serialization/deserializer.hpp
@@ -613,12 +613,11 @@ class Deserializer {
             // accumulate sum
             // TODO (TonyXiang8787) Apple Clang cannot compile transform_inclusive_scan correctly
             // So we disable the good code and write the loop manually
-            // std::transform_inclusive_scan(msg_data.cbegin(), msg_data.cend(),
-            //                               buffer.indptr.begin() + 1,
-            //                               std::plus{}, [](auto const& x) { return x.size; });
-            for (Idx batch_idx = 0; batch_idx != batch_size; ++batch_idx) {
-                buffer.indptr[batch_idx + 1] = buffer.indptr[batch_idx] + msg_data[batch_idx].size;
-            }
+            std::transform_inclusive_scan(msg_data.cbegin(), msg_data.cend(), buffer.indptr.begin() + 1, std::plus{},
+                                          [](auto const& x) { return x.size; });
+            // for (Idx batch_idx = 0; batch_idx != batch_size; ++batch_idx) {
+            //    buffer.indptr[batch_idx + 1] = buffer.indptr[batch_idx] + msg_data[batch_idx].size;
+            //}
         }
         // set nan
         info.component->set_nan(buffer.data, 0, info.total_elements);
