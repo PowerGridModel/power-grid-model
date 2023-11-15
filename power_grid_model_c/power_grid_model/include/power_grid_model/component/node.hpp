123acaca1349627c32a38e29df9868a0006937ae
Martijn Govers <Martijn.Govers@Alliander.com>
2023-11-15 15:56:49 +0100

    minor
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/component/node.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/component/node.hpp
@@ -29,7 +29,7 @@ class Node final : public Base {
 
     // update node, nothing happens here
     static constexpr UpdateChange update(BaseUpdate const& /* update_data */) { return {false, false}; }
-    static constexpr BaseUpdate inverse(BaseUpdate update_data) const { return update_data; }
+    static constexpr BaseUpdate inverse(BaseUpdate update_data) { return update_data; }
 
     // energized
     template <bool sym>
