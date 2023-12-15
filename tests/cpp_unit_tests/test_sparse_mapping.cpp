3f39f4375fce7684c9559baa6b625efdf1f9b15f
Laurynas Jagutis <laurynas.jagutis@alliander.com>
2023-12-15 13:16:05 +0100

    create a test for comparison sort
    Signed-off-by: Laurynas Jagutis <laurynas.jagutis@alliander.com>

--- tests/cpp_unit_tests/test_sparse_mapping.cpp
+++ tests/cpp_unit_tests/test_sparse_mapping.cpp
@@ -21,15 +21,37 @@ TEST_CASE("Test sparse mapping") {
     CHECK(mapping.reorder == mapping_2.reorder);
 }
 
-TEST_CASE("Test dense mapping") {
+TEST_CASE("Test dense mapping - n log n sort") {
     IdxVector const idx_B_in_A{3, 5, 2, 1, 1, 2};
     DenseMapping mapping{{1, 1, 2, 2, 3, 5}, {3, 4, 2, 5, 0, 1}};
     DenseMapping mapping_2 = build_dense_mapping(idx_B_in_A, 7);
 
     CHECK(mapping.indvector == mapping_2.indvector);
     CHECK(mapping.reorder == mapping_2.reorder);
-    CHECK(mapping_2.indvector.begin() == std::min_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
-    CHECK(mapping_2.indvector.end() - 1 == std::max_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
+    CHECK(*(mapping_2.indvector.begin()) == *std::min_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
+    CHECK(*(mapping_2.indvector.end() - 1) ==
+          *std::max_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
+}
+
+TEST_CASE("Test dense mapping - comparison sort") {
+
+    IdxVector idx_B_in_A(1000000);
+    std::mt19937 eng(16);
+    std::uniform_int_distribution<> distr(0, 1000000);
+
+    for (auto& val : idx_B_in_A) {
+        val = distr(eng);
+    }
+
+    IdxVector sorted_idx_B_in_A = idx_B_in_A;
+    std::ranges::sort(sorted_idx_B_in_A);
+
+    DenseMapping mapping_2 = build_dense_mapping(idx_B_in_A, 1000001);
+
+    CHECK(mapping_2.indvector == sorted_idx_B_in_A);
+    CHECK(*(mapping_2.indvector.begin()) == *std::min_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
+    CHECK(*(mapping_2.indvector.end() - 1) ==
+          *std::max_element(mapping_2.indvector.begin(), mapping_2.indvector.end()));
 }
 
 } // namespace power_grid_model
