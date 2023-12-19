ed9002c9bbaca8deede04f268edaff8c9d3b9328
Nitish Bharambe <nitish.bharambe@alliander.com>
2023-12-19 16:29:05 +0100

    namespace rename
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/newton_raphson_se_solver.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/newton_raphson_se_solver.hpp
@@ -20,7 +20,7 @@ Newton Raphson state estimation solver
 namespace power_grid_model {
 
 // hide implementation in inside namespace
-namespace math_model_impl::nrse_solver_impl {
+namespace math_model_impl::nrse {
 
 // block class for the unknown vector and/or right-hand side in state estimation equation
 template <bool sym> struct NRSEUnknown : public Block<double, sym, false, 4> {
@@ -200,9 +200,9 @@ template <bool sym> class NewtonRaphsonSESolver {
 template class NewtonRaphsonSESolver<true>;
 template class NewtonRaphsonSESolver<false>;
 
-} // namespace math_model_impl::nrse_solver_impl
+} // namespace math_model_impl::nrse
 
-template <bool sym> using NewtonRaphsonSESolver = math_model_impl::nrse_solver_impl::NewtonRaphsonSESolver<sym>;
+template <bool sym> using NewtonRaphsonSESolver = math_model_impl::nrse::NewtonRaphsonSESolver<sym>;
 
 } // namespace power_grid_model
 
