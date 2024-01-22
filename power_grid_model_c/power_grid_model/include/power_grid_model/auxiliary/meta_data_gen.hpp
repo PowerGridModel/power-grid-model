1ff9d20964c26e2e2d8794abc5996bb529b6899f
Tony Xiang <tony.xiang@alliander.com>
2024-01-22 13:24:39 +0100

    rename getter
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/meta_data_gen.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/meta_data_gen.hpp
@@ -27,29 +27,29 @@ namespace power_grid_model::meta_data {
 namespace meta_data_gen {
 
 // list of all dataset names
-template <class T> struct struct_getter_input {
+template <class T> struct input_getter_s {
     using type = typename T::InputType;
 };
-template <class T> struct struct_getter_update {
+template <class T> struct update_getter_s {
     using type = typename T::UpdateType;
 };
-template <class T> struct struct_getter_sym_output {
+template <class T> struct sym_output_getter_s {
     using type = typename T::template OutputType<true>;
 };
-template <class T> struct struct_getter_asym_output {
+template <class T> struct asym_output_getter_s {
     using type = typename T::template OutputType<false>;
 };
-template <class T> struct struct_getter_sc_output {
+template <class T> struct sc_output_getter_s {
     using type = typename T::ShortCircuitOutputType;
 };
 
 // generate meta data
 constexpr MetaData meta_data = get_meta_data<AllComponents, // all components list
-                                             dataset_mark<[] { return "input"; }, struct_getter_input>,
-                                             dataset_mark<[] { return "update"; }, struct_getter_update>,
-                                             dataset_mark<[] { return "sym_output"; }, struct_getter_sym_output>,
-                                             dataset_mark<[] { return "asym_output"; }, struct_getter_asym_output>,
-                                             dataset_mark<[] { return "sc_output"; }, struct_getter_sc_output>
+                                             dataset_mark<[] { return "input"; }, input_getter_s>,
+                                             dataset_mark<[] { return "update"; }, update_getter_s>,
+                                             dataset_mark<[] { return "sym_output"; }, sym_output_getter_s>,
+                                             dataset_mark<[] { return "asym_output"; }, asym_output_getter_s>,
+                                             dataset_mark<[] { return "sc_output"; }, sc_output_getter_s>
                                              // end list of all marks
                                              >::value;
 
