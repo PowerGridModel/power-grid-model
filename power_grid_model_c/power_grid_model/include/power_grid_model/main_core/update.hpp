04f8ed323f7e209afe454f1ecffee80e5d087223
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2024-01-23 16:42:53 +0100

    Fix and cleaned up the implementation. Edge case taken into consideration when `sequence_index_map` does not provide accurate reference.
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
    
    ---------------------------------------------------remendiation-commit---------------------------------------------------
    
    DCO Remediation Commit for Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
    
    I, Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>, hereby add my Signed-off-by to this commit: 04c779659e777036f4e99a50b0e2d5742ab42dfc
    
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/main_core/update.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/main_core/update.hpp
@@ -125,59 +125,66 @@ inline void update_y_bus(YBus<sym>& y_bus, std::shared_ptr<MathModelParam<sym> c
     assert(y_bus_param.shunt_param.size() == math_model_param->shunt_param.size());
     assert(y_bus_param.source_param.size() == math_model_param->source_param.size());
 
-    // when sequence_idx_map available: loop through all branches and shunts of math_model_param, check changes
-    if (seq_idx_map != nullptr) {
-        auto querry_param_in_seq_map = [&seq_idx_map](IdxVector const& param_idd_vec) {
-            std::vector<Idx2D> result;
-            for (auto const& param_idd : param_idd_vec) {
-                auto const& seq_idx = (*seq_idx_map)[param_idd];
-                result.insert(result.end(), seq_idx.begin(), seq_idx.end());
-            }
-            return result;
-        };
-        auto branch_params = querry_param_in_seq_map(y_bus.get_branch_param_idx());
-        auto shunt_params = querry_param_in_seq_map(y_bus.get_shunt_param_idx());
-
-        if (branch_params.size() == 0 && shunt_params.size() == 0) {
-            return;
-        }
-
-        //// [skipped] when sequence_idx_map not available: loop through all branches and shunts of math_model_param,
-        /// check changes
-        // auto branch_param_to_change_views =
-        //     boost::irange(y_bus_param.branch_param.size()) |
-        //     boost::adaptors::filtered([&math_model_param, &y_bus_param](Idx i) {
-        //         return cmplx_neq<sym>(math_model_param->branch_param[i].yff(), y_bus_param.branch_param[i].yff()) ||
-        //                cmplx_neq<sym>(math_model_param->branch_param[i].yft(), y_bus_param.branch_param[i].yft()) ||
-        //                cmplx_neq<sym>(math_model_param->branch_param[i].ytf(), y_bus_param.branch_param[i].ytf()) ||
-        //                cmplx_neq<sym>(math_model_param->branch_param[i].ytt(), y_bus_param.branch_param[i].ytt());
-        //     });
-        // auto shunt_param_to_change_views =
-        //     boost::irange(y_bus_param.shunt_param.size()) |
-        //     boost::adaptors::filtered([&math_model_param, &y_bus_param](Idx i) {
-        //         return cmplx_neq<sym>(math_model_param->shunt_param[i], y_bus_param.shunt_param[i]);
-        //     });
-        // MathModelParamIncrement<sym> math_model_param_incrmt;
-        // math_model_param_incrmt.branch_param_to_change = {branch_param_to_change_views.begin(),
-        //                                                   branch_param_to_change_views.end()};
-        // math_model_param_incrmt.shunt_param_to_change = {shunt_param_to_change_views.begin(),
-        //                                                  shunt_param_to_change_views.end()};
-
-        MathModelParamIncrement<sym> math_model_param_incrmt;
-        math_model_param_incrmt.branch_param_to_change.reserve(branch_params.size());
-        std::transform(branch_params.begin(), branch_params.end(),
-                       std::back_inserter(math_model_param_incrmt.branch_param_to_change),
-                       [](const Idx2D& val) { return val.pos; });
-        math_model_param_incrmt.shunt_param_to_change.reserve(shunt_params.size());
-        std::transform(shunt_params.begin(), shunt_params.end(),
-                       std::back_inserter(math_model_param_incrmt.shunt_param_to_change),
-                       [](const Idx2D& val) { return val.pos; });
-
-        auto param_incrmt_ptr = std::make_shared<MathModelParamIncrement<sym> const>(math_model_param_incrmt);
-
-        y_bus.update_admittance_increment(math_model_param, param_incrmt_ptr,
-                                          false); /* param, changed_param, is_decrement */
+    if (seq_idx_map == nullptr) {
+        return;
     }
+
+    // when sequence_idx_map available: loop through all branches and shunts of math_model_param, check changes
+    auto querry_param_in_seq_map = [&seq_idx_map](IdxVector const& param_idd_vec) {
+        std::vector<Idx2D> result;
+        for (auto const& param_idd : param_idd_vec) {
+            auto const& seq_idx = (*seq_idx_map)[param_idd];
+            result.insert(result.end(), seq_idx.begin(), seq_idx.end());
+        }
+        return result;
+    };
+
+    auto branch_params = querry_param_in_seq_map(y_bus.get_branch_param_idx());
+    auto shunt_params = querry_param_in_seq_map(y_bus.get_shunt_param_idx());
+
+    MathModelParamIncrement<sym> math_model_param_incrmt;
+    math_model_param_incrmt.branch_param_to_change.reserve(branch_params.size());
+    std::transform(branch_params.begin(), branch_params.end(),
+                   std::back_inserter(math_model_param_incrmt.branch_param_to_change),
+                   [](const Idx2D& val) { return val.pos; });
+    math_model_param_incrmt.shunt_param_to_change.reserve(shunt_params.size());
+    std::transform(shunt_params.begin(), shunt_params.end(),
+                   std::back_inserter(math_model_param_incrmt.shunt_param_to_change),
+                   [](const Idx2D& val) { return val.pos; });
+
+    // check changes in case sequence_idx_map is available but indicates empty
+    // this back-up check fixes the edge case where the sequence_idx_map is available but indicates empty
+    if (branch_params.size() == 0) {
+        auto branch_param_to_change_views =
+            boost::irange(y_bus_param.branch_param.size()) |
+            boost::adaptors::filtered([&math_model_param, &y_bus_param](Idx i) {
+                return cmplx_neq<sym>(math_model_param->branch_param[i].yff(), y_bus_param.branch_param[i].yff()) ||
+                       cmplx_neq<sym>(math_model_param->branch_param[i].yft(), y_bus_param.branch_param[i].yft()) ||
+                       cmplx_neq<sym>(math_model_param->branch_param[i].ytf(), y_bus_param.branch_param[i].ytf()) ||
+                       cmplx_neq<sym>(math_model_param->branch_param[i].ytt(), y_bus_param.branch_param[i].ytt());
+            });
+        math_model_param_incrmt.branch_param_to_change.reserve(
+            std::distance(branch_param_to_change_views.begin(), branch_param_to_change_views.end()));
+        math_model_param_incrmt.branch_param_to_change = {branch_param_to_change_views.begin(),
+                                                          branch_param_to_change_views.end()};
+    }
+
+    if (shunt_params.size() == 0) {
+        auto shunt_param_to_change_views =
+            boost::irange(y_bus_param.shunt_param.size()) |
+            boost::adaptors::filtered([&math_model_param, &y_bus_param](Idx i) {
+                return cmplx_neq<sym>(math_model_param->shunt_param[i], y_bus_param.shunt_param[i]);
+            });
+        math_model_param_incrmt.shunt_param_to_change.reserve(
+            std::distance(shunt_param_to_change_views.begin(), shunt_param_to_change_views.end()));
+        math_model_param_incrmt.shunt_param_to_change = {shunt_param_to_change_views.begin(),
+                                                         shunt_param_to_change_views.end()};
+    }
+
+    auto param_incrmt_ptr = std::make_shared<MathModelParamIncrement<sym> const>(math_model_param_incrmt);
+
+    y_bus.update_admittance_increment(math_model_param, param_incrmt_ptr,
+                                      false); /* param, changed_param, is_decrement */
 #else
     y_bus.update_admittance(math_model_param);
 #endif // INCREMENT_UPDATE_Y_BUS
@@ -215,14 +222,13 @@ template <bool sym>
 inline void update_y_bus(MathState& math_state, std::vector<MathModelParam<sym>> const& math_model_params,
                          Idx n_math_solvers, std::vector<std::vector<Idx2D>> const& seq_idx_map) {
     for (Idx i = 0; i != n_math_solvers; ++i) {
+        auto seq_idx_map_copy = seq_idx_map;
         if constexpr (sym) {
-            auto seq_idx_map_copy = seq_idx_map;
             update_y_bus(math_state.y_bus_vec_sym[i],
                          std::make_shared<MathModelParam<sym> const>(std::move(math_model_params[i])),
                          std::make_shared<std::vector<std::vector<Idx2D>> const>(std::move(seq_idx_map_copy)));
 
         } else {
-            auto seq_idx_map_copy = seq_idx_map;
             update_y_bus(math_state.y_bus_vec_asym[i],
                          std::make_shared<MathModelParam<sym> const>(std::move(math_model_params[i])),
                          std::make_shared<std::vector<std::vector<Idx2D>> const>(std::move(seq_idx_map_copy)));
