26ca1bf07eecd59fc2df752bcea0e40750fe512d
Martijn Govers <Martijn.Govers@Alliander.com>
2024-02-16 17:32:26 +0100

    remove test code
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- tests/cpp_validation_tests/test_validation.cpp
+++ tests/cpp_validation_tests/test_validation.cpp
@@ -608,15 +608,14 @@ TEST_CASE("Validation test single") {
 TEST_CASE("Validation test batch") {
     std::vector<CaseParam> const& all_cases = get_all_batch_cases();
     for (CaseParam const& param : all_cases) {
-        if (param.case_name ==
-            "power_flow/pandapower/networks/asymmetric/distribution-case-asym-newton_raphson_batch") {
-            SUBCASE(param.case_name.c_str()) {
-                try {
-                    validate_batch_case(param);
-                } catch (std::exception& e) {
-                    auto const msg = std::string("Unexpected exception with message: ") + e.what();
-                    FAIL_CHECK(msg);
-                }
+        SUBCASE(param.case_name.c_str()) {
+            try {
+                validate_batch_case(param);
+            } catch (std::exception& e) {
+                using namespace std::string_literals;
+
+                auto const msg = "Unexpected exception with message: "s + e.what();
+                FAIL_CHECK(msg);
             }
         }
     }
