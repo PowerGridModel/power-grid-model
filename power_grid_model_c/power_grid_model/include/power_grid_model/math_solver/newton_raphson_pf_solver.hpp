afa2bf8537a447bb48a24d090e30dac07e50b3b0
Nitish Bharambe <nitish.bharambe@alliander.com>
2024-03-05 15:24:39 +0100

    modify real to complex calculation
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/newton_raphson_pf_solver.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/newton_raphson_pf_solver.hpp
@@ -276,26 +276,15 @@ template <symmetry_tag sym> class NewtonRaphsonPFSolver : public IterativePFSolv
     static PFJacBlock<sym> calculate_hnml(ComplexTensor<sym> const& yij, ComplexValue<sym> const& ui,
                                           ComplexValue<sym> const& uj) {
         PFJacBlock<sym> block{};
-        // real and imag of addmittance
-        RealTensor<sym> const gij = real(yij);
-        RealTensor<sym> const bij = imag(yij);
-        // diag(Vi) * cos(theta_ij) * diag(Vj)
-        // Ui_r @* Uj_r + Ui_i @* Uj_i
-        // = cij
-        RealTensor<sym> const c_ij =
-            vector_outer_product(real(ui), real(uj)) + vector_outer_product(imag(ui), imag(uj));
-        // diag(Vi) * sin(theta_ij) * diag(Vj)
-        // = Ui_i @* Uj_r - Ui_r @* Uj_i
-        // = sij
-        RealTensor<sym> const s_ij =
-            vector_outer_product(imag(ui), real(uj)) - vector_outer_product(real(ui), imag(uj));
+        // Calculate power flow from i to j ie. (ui @* uj)  .* conj(yij)
+        ComplexTensor<sym> power_flow_ij = vector_outer_product(ui, conj(uj)) * conj(yij);
         // calculate H, N, M, L
         // Hij = diag(Vi) * ( Gij .* sin(theta_ij) - Bij .* cos(theta_ij) ) * diag(Vj)
-        // = Gij .* sij - Bij .* cij
-        block.h() = gij * s_ij - bij * c_ij;
+        // = imag(power_flow_ij)
+        block.h() = imag(power_flow_ij);
         // Nij = diag(Vi) * ( Gij .* cos(Theta_ij) + Bij .* sin(Theta_ij) ) * diag(Vj)
-        // = Gij .* cij + Bij .* sij
-        block.n() = gij * c_ij + bij * s_ij;
+        // = real(power_flow_ij)
+        block.n() = real(power_flow_ij);
         // Mij = - Nij
         block.m() = -block.n();
         // Lij = Hij
