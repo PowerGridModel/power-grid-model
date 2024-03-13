0115a90120de8429d68564332e1db7d101b3ef53
Martijn Govers <Martijn.Govers@Alliander.com>
2024-03-13 10:53:41 +0100

    remove hardware destructive inference size
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/common/calculation_info.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/common/calculation_info.hpp
@@ -15,16 +15,6 @@
 
 namespace power_grid_model {
 
-// calculation info
-#ifdef POWER_GRID_MODEL_CPP_BENCHMARK
-#if defined(__cpp_lib_hardware_interference_size)
-constexpr size_t cache_line_size = std::hardware_destructive_interference_size;
-#else
-constexpr size_t cache_line_size = 64;
-#endif
-class alignas(cache_line_size) CalculationInfo : public std::map<std::string, double, std::less<>> {};
-#else
 using CalculationInfo = std::map<std::string, double, std::less<>>;
-#endif
 
 } // namespace power_grid_model
