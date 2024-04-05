6e1c96bed537d165b61f49f904aee97b4d1f1be1
Martijn Govers <Martijn.Govers@Alliander.com>
2024-04-05 08:20:42 +0200

    more refactor output.hpp
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/main_core/output.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/main_core/output.hpp
@@ -98,7 +98,7 @@ constexpr ResIt produce_output(MainModelState<ComponentContainer> const& state, 
 } // namespace detail
 
 // output node
-template <std::same_as<Node> Component, steady_state_math_output_type MathOutputType>
+template <std::derived_from<Node> Component, steady_state_math_output_type MathOutputType>
 constexpr auto output_result(Node const& node, std::vector<MathOutputType> const& math_output, Idx2D math_id) {
     using sym = typename MathOutputType::sym;
 
@@ -108,20 +108,20 @@ constexpr auto output_result(Node const& node, std::vector<MathOutputType> const
     return node.get_output<sym>(math_output[math_id.group].u[math_id.pos],
                                 math_output[math_id.group].bus_injection[math_id.pos]);
 }
-template <std::same_as<Node> Component, short_circuit_math_output_type MathOutputType>
-inline auto output_result(Node const& node, std::vector<MathOutputType> const& math_output, Idx2D math_id) {
+template <std::derived_from<Node> Component, short_circuit_math_output_type MathOutputType>
+inline auto output_result(Component const& node, std::vector<MathOutputType> const& math_output, Idx2D math_id) {
     if (math_id.group == -1) {
         return node.get_null_sc_output();
     }
     return node.get_sc_output(math_output[math_id.group].u_bus[math_id.pos]);
 }
-template <std::same_as<Node> Component, class ComponentContainer, math_output_type MathOutputType,
+template <std::derived_from<Node> Component, class ComponentContainer, math_output_type MathOutputType,
           std::forward_iterator ResIt>
     requires model_component_state_c<MainModelState, ComponentContainer, Component>
 constexpr ResIt output_result(MainModelState<ComponentContainer> const& state,
                               std::vector<MathOutputType> const& math_output, ResIt res_it) {
-    return detail::produce_output<Node, Idx2D>(state, res_it, [&math_output](Node const& node, Idx2D math_id) {
-        return output_result<Node>(node, math_output, math_id);
+    return detail::produce_output<Node, Idx2D>(state, res_it, [&math_output](Component const& node, Idx2D math_id) {
+        return output_result<Component>(node, math_output, math_id);
     });
 }
 
@@ -315,101 +315,102 @@ constexpr ResIt output_result(MainModelState<ComponentContainer> const& state,
                               std::vector<MathOutputType> const& math_output, ResIt res_it) {
     return detail::produce_output<Component, Idx>(
         state, res_it, [&state, &math_output](Component const& voltage_sensor, Idx const node_seq) {
-            return output_result(voltage_sensor, state, math_output, node_seq);
+            return output_result<Component, ComponentContainer>(voltage_sensor, state, math_output, node_seq);
         });
 }
 
 // output power sensor
 template <std::derived_from<GenericPowerSensor> Component, class ComponentContainer,
-          steady_state_math_output_type MathOutputType, std::forward_iterator ResIt>
+          steady_state_math_output_type MathOutputType>
     requires model_component_state_c<MainModelState, ComponentContainer, Component>
-constexpr ResIt output_result(MainModelState<ComponentContainer> const& state,
-                              std::vector<MathOutputType> const& math_output, ResIt res_it) {
-    return detail::produce_output<
-        Component, Idx>(state, res_it, [&state, &math_output](Component const& power_sensor, Idx const obj_seq) {
-        using sym = typename MathOutputType::sym;
+constexpr auto output_result(Component const& power_sensor, MainModelState<ComponentContainer> const& state,
+                             std::vector<MathOutputType> const& math_output, Idx const obj_seq) {
+    using sym = typename MathOutputType::sym;
 
-        auto const terminal_type = power_sensor.get_terminal_type();
-        Idx2D const obj_math_id = [&]() {
-            switch (terminal_type) {
-                using enum MeasuredTerminalType;
-
-            case branch_from:
-            case branch_to:
-                return state.topo_comp_coup->branch[obj_seq];
-            case source:
-                return state.topo_comp_coup->source[obj_seq];
-            case shunt:
-                return state.topo_comp_coup->shunt[obj_seq];
-            case load:
-            case generator:
-                return state.topo_comp_coup->load_gen[obj_seq];
-            // from branch3, get relevant math object branch based on the measured side
-            case branch3_1:
-                return Idx2D{state.topo_comp_coup->branch3[obj_seq].group,
-                             state.topo_comp_coup->branch3[obj_seq].pos[0]};
-            case branch3_2:
-                return Idx2D{state.topo_comp_coup->branch3[obj_seq].group,
-                             state.topo_comp_coup->branch3[obj_seq].pos[1]};
-            case branch3_3:
-                return Idx2D{state.topo_comp_coup->branch3[obj_seq].group,
-                             state.topo_comp_coup->branch3[obj_seq].pos[2]};
-            case node:
-                return state.topo_comp_coup->node[obj_seq];
-            default:
-                throw MissingCaseForEnumError(std::string(GenericPowerSensor::name) + " output_result()",
-                                              terminal_type);
-            }
-        }();
-
-        if (obj_math_id.group == -1) {
-            return power_sensor.template get_null_output<sym>();
-        }
-
+    auto const terminal_type = power_sensor.get_terminal_type();
+    Idx2D const obj_math_id = [&]() {
         switch (terminal_type) {
             using enum MeasuredTerminalType;
 
         case branch_from:
-        // all power sensors in branch3 are at from side in the mathematical model
-        case branch3_1:
-        case branch3_2:
-        case branch3_3:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].branch[obj_math_id.pos].s_f);
         case branch_to:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].branch[obj_math_id.pos].s_t);
+            return state.topo_comp_coup->branch[obj_seq];
         case source:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].source[obj_math_id.pos].s);
+            return state.topo_comp_coup->source[obj_seq];
         case shunt:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].shunt[obj_math_id.pos].s);
+            return state.topo_comp_coup->shunt[obj_seq];
         case load:
         case generator:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].load_gen[obj_math_id.pos].s);
+            return state.topo_comp_coup->load_gen[obj_seq];
+        // from branch3, get relevant math object branch based on the measured side
+        case branch3_1:
+            return Idx2D{state.topo_comp_coup->branch3[obj_seq].group, state.topo_comp_coup->branch3[obj_seq].pos[0]};
+        case branch3_2:
+            return Idx2D{state.topo_comp_coup->branch3[obj_seq].group, state.topo_comp_coup->branch3[obj_seq].pos[1]};
+        case branch3_3:
+            return Idx2D{state.topo_comp_coup->branch3[obj_seq].group, state.topo_comp_coup->branch3[obj_seq].pos[2]};
         case node:
-            return power_sensor.template get_output<sym>(math_output[obj_math_id.group].bus_injection[obj_math_id.pos]);
+            return state.topo_comp_coup->node[obj_seq];
         default:
             throw MissingCaseForEnumError(std::string(GenericPowerSensor::name) + " output_result()", terminal_type);
         }
-    });
+    }();
+
+    if (obj_math_id.group == -1) {
+        return power_sensor.template get_null_output<sym>();
+    }
+
+    switch (terminal_type) {
+        using enum MeasuredTerminalType;
+
+    case branch_from:
+    // all power sensors in branch3 are at from side in the mathematical model
+    case branch3_1:
+    case branch3_2:
+    case branch3_3:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].branch[obj_math_id.pos].s_f);
+    case branch_to:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].branch[obj_math_id.pos].s_t);
+    case source:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].source[obj_math_id.pos].s);
+    case shunt:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].shunt[obj_math_id.pos].s);
+    case load:
+    case generator:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].load_gen[obj_math_id.pos].s);
+    case node:
+        return power_sensor.template get_output<sym>(math_output[obj_math_id.group].bus_injection[obj_math_id.pos]);
+    default:
+        throw MissingCaseForEnumError(std::string(GenericPowerSensor::name) + " output_result()", terminal_type);
+    }
 }
 template <std::derived_from<GenericPowerSensor> Component, class ComponentContainer,
-          short_circuit_math_output_type MathOutputType, std::forward_iterator ResIt>
+          short_circuit_math_output_type MathOutputType>
+    requires model_component_state_c<MainModelState, ComponentContainer, Component>
+constexpr auto output_result(Component const& power_sensor, MainModelState<ComponentContainer> const& state,
+                             std::vector<MathOutputType> const& /* math_output */, Idx const /* obj_seq */) {
+    return power_sensor.get_null_sc_output();
+}
+template <std::derived_from<GenericPowerSensor> Component, class ComponentContainer, math_output_type MathOutputType,
+          std::forward_iterator ResIt>
     requires model_component_state_c<MainModelState, ComponentContainer, Component>
 constexpr ResIt output_result(MainModelState<ComponentContainer> const& state,
-                              std::vector<MathOutputType> const& /* math_output */, ResIt res_it) {
+                              std::vector<MathOutputType> const& math_output, ResIt res_it) {
     return detail::produce_output<Component, Idx>(
-        state, res_it,
-        [](Component const& power_sensor, Idx const /* node_seq */) { return power_sensor.get_null_sc_output(); });
+        state, res_it, [&state, &math_output](Component const& power_sensor, Idx const obj_seq) {
+            return output_result<Component, ComponentContainer>(power_sensor, state, math_output, obj_seq);
+        });
 }
 
 // output fault
-template <std::same_as<Fault> Component, class ComponentContainer, steady_state_math_output_type MathOutputType>
+template <std::derived_from<Fault> Component, class ComponentContainer, steady_state_math_output_type MathOutputType>
     requires model_component_state_c<MainModelState, ComponentContainer, Component> &&
              model_component_state_c<MainModelState, ComponentContainer, Node>
 constexpr auto output_result(Component const& fault, MainModelState<ComponentContainer> const& state,
                              std::vector<MathOutputType> const& math_output, Idx2D /* math_id */) {
     return fault.get_output();
 }
-template <std::same_as<Fault> Component, class ComponentContainer, short_circuit_math_output_type MathOutputType>
+template <std::derived_from<Fault> Component, class ComponentContainer, short_circuit_math_output_type MathOutputType>
     requires model_component_state_c<MainModelState, ComponentContainer, Component> &&
              model_component_state_c<MainModelState, ComponentContainer, Node>
 inline auto output_result(Component const& fault, MainModelState<ComponentContainer> const& state,
@@ -421,7 +422,7 @@ inline auto output_result(Component const& fault, MainModelState<ComponentContai
     auto const u_rated = state.components.template get_item<Node>(fault.get_fault_object()).u_rated();
     return fault.get_sc_output(math_output[math_id.group].fault[math_id.pos], u_rated);
 }
-template <std::same_as<Fault> Component, class ComponentContainer, math_output_type MathOutputType,
+template <std::derived_from<Fault> Component, class ComponentContainer, math_output_type MathOutputType,
           std::forward_iterator ResIt>
     requires model_component_state_c<MainModelState, ComponentContainer, Component> &&
              model_component_state_c<MainModelState, ComponentContainer, Node>
