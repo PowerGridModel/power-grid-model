e2aa2f2b1f9f6407d710ce6a18ebee9aef447966
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2024-04-11 14:43:37 +0200

    Test graph for build_transformer_graph
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- tests/cpp_unit_tests/test_tap_position_optimizer.cpp
+++ tests/cpp_unit_tests/test_tap_position_optimizer.cpp
@@ -2,11 +2,13 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+#include <power_grid_model/main_core/input.hpp>
 #include <power_grid_model/optimizer/tap_position_optimizer.hpp>
 
 #include <doctest/doctest.h>
 
 namespace pgm_tap = power_grid_model::optimizer::tap_position_optimizer;
+namespace main_core = power_grid_model::main_core;
 
 namespace power_grid_model {
 
@@ -16,38 +18,28 @@ using TestState = main_core::MainModelState<TestComponentContainer>;
 } // namespace
 
 TEST_CASE("Test Transformer ranking") {
-    // ToDo: The grid from OntNote page
-    // TestState state{50.0}; TODO figure out
-    state.add_component<Node>({{0, 150e3},
-                               {1, 10e3},
-                               {2, 10e3},
-                               {3, 10e3},
-                               {4, 10e3},
-                               {5, 50e3},
-                               {6, 10e3},
-                               {7, 10e3},
-                               {8, 10e3},
-                               {9, 10e3}});
-    state.add_component<Source>({{10, 0, 1, 1.0, nan, nan, nan, nan}});
-    state.add_component<Transformer>({{11,  0,   1,   1,   1,   150e3, 10e3, nan, nan, nan, nan, nan, nan, nan,
-                                       nan, nan, nan, nan, nan, nan,   nan,  nan, nan, nan, nan, nan, nan},
-                                      {12,  0,   1,   1,   1,   150e3, 10e3, nan, nan, nan, nan, nan, nan, nan,
-                                       nan, nan, nan, nan, nan, nan,   nan,  nan, nan, nan, nan, nan, nan},
-                                      {13,  5,   7,   1,   1,   50e3, 10e3, nan, nan, nan, nan, nan, nan, nan,
-                                       nan, nan, nan, nan, nan, nan,  nan,  nan, nan, nan, nan, nan, nan},
-                                      {14,  2,   3,   1,   1,   10e3, 10e3, nan, nan, nan, nan, nan, nan, nan,
-                                       nan, nan, nan, nan, nan, nan,  nan,  nan, nan, nan, nan, nan, nan},
-                                      {15,  8,   9,   1,   1,   10e3, 10e3, nan, nan, nan, nan, nan, nan, nan,
-                                       nan, nan, nan, nan, nan, nan,  nan,  nan, nan, nan, nan, nan, nan}});
-    state.add_component<Line>({{16, 1, 2, 1, 1, nan, nan, nan, nan, nan, nan, nan, nan, nan},
-                               {17, 4, 6, 1, 1, nan, nan, nan, nan, nan, nan, nan, nan, nan},
-                               {18, 7, 8, 1, 1, nan, nan, nan, nan, nan, nan, nan, nan, nan},
-                               {19, 3, 6, 1, 1, nan, nan, nan, nan, nan, nan, nan, nan, nan}});
-    state.add_component<Link>({{20, 2, 1, 1, 1}, {21, 6, 4, 1, 1}, {22, 8, 7, 1, 1}, {23, 9, 3, 1, 1}});
+    // The grid from OntNote page
+    TestState state;
+    std::vector<NodeInput> nodes{{0, 150e3}, {1, 10e3}, {2, 10e3}, {3, 10e3}, {4, 10e3},
+                                 {5, 50e3},  {6, 10e3}, {7, 10e3}, {8, 10e3}, {9, 10e3}};
+    main_core::add_component<Node>(state, nodes.begin(), nodes.end(), 50.0);
+
+    std::vector<TransformerInput> transformers{{11, 0, 1, 1, 1, 150e3, 10e3},
+                                               {12, 0, 1, 1, 1, 150e3, 10e3},
+                                               {13, 5, 7, 1, 1, 50e3, 10e3},
+                                               {14, 2, 3, 1, 1, 10e3, 10e3},
+                                               {15, 8, 9, 1, 1, 10e3, 10e3}};
+    main_core::add_component<Transformer>(state, transformers.begin(), transformers.end(), 50.0);
+
+    std::vector<LineInput> lines{{16, 1, 2, 1, 1}, {17, 4, 6, 1, 1}, {18, 7, 8, 1, 1}, {19, 3, 6, 1, 1}};
+    main_core::add_component<Line>(state, lines.begin(), lines.end(), 50.0);
+
+    std::vector<LinkInput> links{{20, 2, 1, 1, 1}, {21, 6, 4, 1, 1}, {22, 8, 7, 1, 1}, {23, 9, 3, 1, 1}};
+    main_core::add_component<Link>(state, links.begin(), links.end(), 50.0);
 
     // Subcases
     SUBCASE("Building the graph") {
-        // ToDo: graph creation
+        // reference graph creation
         std::vector<std::pair<Idx, Idx>> edge_array = {{0, 1}, {0, 2}, {2, 3}};
 
         std::vector<pgm_tap::TrafoGraphEdge> edge_prop{{{0, 1}, 1}, {{0, 1}, 1}, {{0, 1}, 1}};
