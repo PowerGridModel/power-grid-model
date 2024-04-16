a61b6d65cb8acdff29b7fa020bed1044cf321d38
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2024-04-16 11:19:27 +0200

    Quick fix to the `size`call
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- tests/cpp_unit_tests/test_tap_position_optimizer.cpp
+++ tests/cpp_unit_tests/test_tap_position_optimizer.cpp
@@ -13,7 +13,7 @@ namespace main_core = power_grid_model::main_core;
 namespace power_grid_model {
 
 namespace {
-using TestComponentContainer = Container<Line, Link, Node, Transformer, Source>;
+using TestComponentContainer = Container<Line, Link, Node, Transformer, TransformerTapRegulator, Source>;
 using TestState = main_core::MainModelState<TestComponentContainer>;
 
 TransformerInput get_transformer(ID id, ID from, ID to, double u1, double u2) {
