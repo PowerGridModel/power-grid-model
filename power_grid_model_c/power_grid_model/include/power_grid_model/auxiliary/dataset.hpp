c048b8ce11c4ffa1220c06a230c067fd5338a0b5
Tony Xiang <tony.xiang@alliander.com>
2024-05-03 21:05:53 +0200

    refactor dataset
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/dataset.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/auxiliary/dataset.hpp
@@ -1,85 +1,223 @@
 // SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
 //
 // SPDX-License-Identifier: MPL-2.0
-/*
+
 #pragma once
 
-// define dataset classes with void pointers
-
-#include "dataset_fwd.hpp"
+// handle dataset and buffer related stuff
 
 #include "../common/common.hpp"
+#include "../common/exception.hpp"
+#include "dataset.hpp"
+#include "meta_data.hpp"
+#include "meta_data_gen.hpp"
 
-#include <cassert>
-#include <map>
+#include <span>
+#include <string_view>
 
 namespace power_grid_model {
 
-// a dataset wrapper using void pointers
-// the void pointers can be cast to relevant types
-// the dataset is either one set of data
-//     the indptr is a nullptr
-//     the batch_size is one
-// the dataset can also be a batch of sets with different length per batch
-//     the indptr is an integer array, for i-th sets,
-//          the set data is in the range [ indptr[i], indptr[i + 1] )
-// the dataset can also be a batch of sets with the same length per batch
-//     the indptr is a nullptr, for i-th sets,
-//          the set data is in the range [ i * elements_per_scenario, (i + 1) * elements_per_scenario )
+namespace meta_data {
 
-template <dataset_type_tag T> constexpr bool is_const_dataset_v = std::same_as<T, const_dataset_t>;
+template <dataset_type_tag T>
+constexpr bool is_data_mutable_v = std::same_as<T, mutable_dataset_t> || std::same_as<T, writable_dataset_t>;
+template <dataset_type_tag T> constexpr bool is_indptr_mutable_v = std::same_as<T, writable_dataset_t>;
 
 static_assert(dataset_type_tag<const_dataset_t>);
 static_assert(dataset_type_tag<mutable_dataset_t>);
-static_assert(is_const_dataset_v<const_dataset_t>);
-static_assert(!is_const_dataset_v<mutable_dataset_t>);
+static_assert(dataset_type_tag<writable_dataset_t>);
+static_assert(!is_data_mutable_v<const_dataset_t>);
+static_assert(is_data_mutable_v<mutable_dataset_t>);
+static_assert(is_data_mutable_v<writable_dataset_t>);
+static_assert(!is_indptr_mutable_v<const_dataset_t>);
+static_assert(!is_indptr_mutable_v<mutable_dataset_t>);
+static_assert(is_indptr_mutable_v<writable_dataset_t>);
 
-template <dataset_type_tag dataset_type_> class DataPointer {
+struct ComponentInfo {
+    MetaComponent const* component;
+    // for non-uniform component, this is -1, we use indptr to describe the elements per scenario
+    Idx elements_per_scenario;
+    Idx total_elements;
+};
+
+struct DatasetInfo {
+    bool is_batch;
+    Idx batch_size; // for single dataset, the batch size is one
+    MetaDataset const* dataset;
+    std::vector<ComponentInfo> component_info;
+};
+
+template <dataset_type_tag dataset_type_> class Dataset {
+    struct immutable_t {};
+    struct mutable_t {};
+
   public:
     using dataset_type = dataset_type_;
+    using Data = std::conditional_t<is_data_mutable_v<dataset_type>, void, void const>;
+    using Indptr = std::conditional_t<is_indptr_mutable_v<dataset_type>, Idx, Idx const>;
 
-    template <class T> std::pair<ptr_t<T>, ptr_t<T>> get_iterators(Idx pos) const {
-        assert(pos < batch_size_);
-        auto* const ptr = reinterpret_cast<ptr_t<T>>(ptr_);
-        if (indptr_) {
-            if (pos < 0) {
-                return std::make_pair(ptr, ptr + indptr_[batch_size_]);
+    template <class StructType>
+    using DataStruct = std::conditional_t<is_data_mutable_v<dataset_type>, StructType, StructType const>;
+
+    struct Buffer {
+        Data* data;
+        // for uniform buffer, indptr is empty
+        std::span<Indptr> indptr;
+    };
+
+    Dataset(bool is_batch, Idx batch_size, std::string_view dataset)
+        : dataset_info_{.is_batch = is_batch,
+                        .batch_size = batch_size,
+                        .dataset = &meta_data.get_dataset(dataset),
+                        .component_info = {}} {
+        if (!dataset_info_.is_batch && (dataset_info_.batch_size != 1)) {
+            throw DatasetError{"For non-batch dataset, batch size should be one!\n"};
+        }
+    }
+
+    // implicit conversion constructor to const
+    template <dataset_type_tag other_dataset_type>
+        requires(is_data_mutable_v<other_dataset_type> && !is_data_mutable_v<dataset_handler_type>)
+    Dataset(Dataset<other_dataset_type> const& other) : dataset_info_{other.get_description()} {
+        for (Idx i{}; i != other.n_components(); ++i) {
+            auto const& buffer = other.get_buffer(i);
+            buffers_.push_back(Buffer{.data = buffer.data, .indptr = buffer.indptr});
+        }
+    }
+
+    bool is_batch() const { return dataset_info_.is_batch; }
+    Idx batch_size() const { return dataset_info_.batch_size; }
+    MetaDataset const& dataset() const { return *dataset_info_.dataset; }
+    Idx n_components() const { return static_cast<Idx>(buffers_.size()); }
+    DatasetInfo const& get_description() const { return dataset_info_; }
+    ComponentInfo const& get_component_info(Idx i) const { return dataset_info_.component_info[i]; }
+    Buffer const& get_buffer(std::string_view component) const { return get_buffer(find_component(component, true)); }
+    Buffer const& get_buffer(Idx i) const { return buffers_[i]; }
+
+    Idx find_component(std::string_view component, bool required = false) const {
+        auto const found = std::ranges::find_if(dataset_info_.component_info, [component](ComponentInfo const& x) {
+            return x.component->name == component;
+        });
+        if (found == dataset_info_.component_info.cend()) {
+            if (required) {
+                using namespace std::string_literals;
+                throw DatasetError{"Cannot find component '"s + std::string{component} + "'!\n"s};
             }
-            return std::make_pair(ptr + indptr_[pos], ptr + indptr_[pos + 1]);
+            return -1;
         }
-        if (pos < 0) {
-            return std::make_pair(ptr, ptr + elements_per_scenario_ * batch_size_);
-        }
-        return std::make_pair(ptr + elements_per_scenario_ * pos, ptr + elements_per_scenario_ * (pos + 1));
+        return std::distance(dataset_info_.component_info.cbegin(), found);
     }
 
-
-    Idx elements_per_scenario(Idx pos) const {
-        assert(pos >= 0);
-        assert(pos < batch_size_);
-        if (indptr_ != nullptr) {
-            return indptr_[pos + 1] - indptr_[pos];
-        }
-        return elements_per_scenario_;
+    ComponentInfo const& get_component_info(std::string_view component) const {
+        return dataset_info_.component_info[find_component(component, true)];
     }
 
-    ptr_t<void> raw_ptr() const { return ptr_; }
+    void add_component_info(std::string_view component, Idx elements_per_scenario, Idx total_elements)
+        requires is_indptr_mutable_v<dataset_handler_type>
+    {
+        add_component_info_impl(component, elements_per_scenario, total_elements);
+    }
 
-    // check if the dataset is one empty batch
-    // the length of data should be zero
-    bool is_empty() const {
-        if (indptr_ != nullptr) {
-            return (indptr_[batch_size_] == 0);
+    void add_buffer(std::string_view component, Idx elements_per_scenario, Idx total_elements, Indptr* indptr,
+                    Data* data)
+        requires(!is_indptr_mutable_v<dataset_handler_type>)
+    {
+        check_non_uniform_integrity<immutable_t>(elements_per_scenario, total_elements, indptr);
+        add_component_info_impl(component, elements_per_scenario, total_elements);
+        buffers_.back().data = data;
+        if (indptr) {
+            buffers_.back().indptr = {indptr, static_cast<size_t>(batch_size() + 1)};
+        } else {
+            buffers_.back().indptr = {};
         }
-        return batch_size_ == 0 || elements_per_scenario_ == 0;
+    }
+
+    void set_buffer(std::string_view component, Indptr* indptr, Data* data)
+        requires is_indptr_mutable_v<dataset_handler_type>
+    {
+        Idx const idx = find_component(component, true);
+        ComponentInfo const& info = dataset_info_.component_info[idx];
+        check_non_uniform_integrity<mutable_t>(info.elements_per_scenario, info.total_elements, indptr);
+        buffers_[idx].data = data;
+        if (indptr) {
+            buffers_[idx].indptr = {indptr, static_cast<size_t>(batch_size() + 1)};
+        } else {
+            buffers_[idx].indptr = {};
+        }
+    }
+
+    // get buffer by component type
+    template <template <class> class type_getter, class ComponentType,
+              class StructType = DataStruct<typename type_getter<ComponentType>::type>>
+    std::span<StructType> get_buffer_span(Idx scenario = -1) const {
+        if (!is_batch() && scenario > 0) {
+            throw DatasetError{"Cannot export a single dataset for scenario > 1\n"};
+        }
+        Idx const idx = find_component(ComponentType::name, false);
+        // return empty span if the component does not exist
+        if (idx < 0) {
+            return {};
+        }
+        // return span based on uniform or non-uniform buffer
+        ComponentInfo const& info = dataset_info_.component_info[idx];
+        Buffer const& buffer = buffers_[idx];
+        StructType* ptr = reinterpret_cast<StructType*>(buffer.data);
+        if (scenario < 0) {
+            return std::span<StructType>{ptr, ptr + info.total_elements};
+        }
+        if (info.elements_per_scenario < 0) {
+            return std::span<StructType>{ptr + buffer.indptr[scenario], ptr + buffer.indptr[scenario + 1]};
+        }
+        return std::span<StructType>{ptr + info.elements_per_scenario * scenario,
+                                     ptr + info.elements_per_scenario * (scenario + 1)};
     }
 
   private:
-    ptr_t<void> ptr_;
-    Idx const* indptr_;
-    Idx batch_size_;            // number of batches
-    Idx elements_per_scenario_; // number of data points per batch, -1 for variable batches
+    DatasetInfo dataset_info_;
+    std::vector<Buffer> buffers_;
+
+    void check_uniform_integrity(Idx elements_per_scenario, Idx total_elements) {
+        if ((elements_per_scenario >= 0) && (elements_per_scenario * batch_size() != total_elements)) {
+            throw DatasetError{
+                "For a uniform buffer, total_elements should be equal to elements_per_scenario * batch_size !\n"};
+        }
+    }
+
+    template <typename check_indptr_content>
+        requires std::same_as<check_indptr_content, mutable_t> || std::same_as<check_indptr_content, immutable_t>
+    void check_non_uniform_integrity(Idx elements_per_scenario, Idx total_elements, Indptr* indptr) {
+        if (elements_per_scenario < 0) {
+            if (!indptr) {
+                throw DatasetError{"For a non-uniform buffer, indptr should be supplied !\n"};
+            }
+            if constexpr (std::same_as<check_indptr_content, immutable_t>) {
+                if (indptr[0] != 0 || indptr[batch_size()] != total_elements) {
+                    throw DatasetError{
+                        "For a non-uniform buffer, indptr should begin with 0 and end with total_elements !\n"};
+                }
+            }
+        } else {
+            if (indptr) {
+                throw DatasetError{"For a uniform buffer, indptr should be nullptr !\n"};
+            }
+        }
+    }
+
+    void add_component_info_impl(std::string_view component, Idx elements_per_scenario, Idx total_elements) {
+        if (find_component(component) >= 0) {
+            throw DatasetError{"Cannot have duplicated components!\n"};
+        }
+        check_uniform_integrity(elements_per_scenario, total_elements);
+        dataset_info_.component_info.push_back(
+            {&dataset_info_.dataset->get_component(component), elements_per_scenario, total_elements});
+        buffers_.push_back(Buffer{});
+    }
 };
 
+} // namespace meta_data
+
+using ConstDataset = meta_data::Dataset<const_dataset_t>;
+using MutableDataset = meta_data::Dataset<mutable_dataset_t>;
+using WritableDataset = meta_data::Dataset<writable_dataset_t>;
+
 } // namespace power_grid_model
-*/
