67bcf9cea14e008e9d51169e85e4130a1f1118bb
Martijn Govers <Martijn.Govers@Alliander.com>
2024-05-07 09:33:12 +0200

    add retry logic when tap optimizer diverges
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/optimizer/tap_position_optimizer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/optimizer/tap_position_optimizer.hpp
@@ -632,12 +632,27 @@ class TapPositionOptimizerImpl<std::tuple<TransformerTypes...>, StateCalculator,
                   CalculationMethod method) const -> ResultType {
         initialize(regulator_order);
 
-        if (auto result = iterate(state, regulator_order, method); strategy_ == OptimizerStrategy::any) {
+        if (auto result = iterate_with_fallback(state, regulator_order, method); strategy_ == OptimizerStrategy::any) {
             return result;
         }
 
         // refine solution
         exploit_neighborhood(regulator_order);
+        return iterate_with_fallback(state, regulator_order, method);
+    }
+
+    auto iterate_with_fallback(State const& state,
+                               std::vector<std::vector<RegulatedTransformer>> const& regulator_order,
+                               CalculationMethod method) const -> ResultType {
+        try {
+            return iterate(state, regulator_order, method);
+        } catch (IterationDiverge const& /* ex */) {
+            // fallback
+        } catch (SparseMatrixError const& /* ex */) {
+            // fallback
+        }
+
+        std::ignore = iterate(state, regulator_order, CalculationMethod::linear);
         return iterate(state, regulator_order, method);
     }
 
