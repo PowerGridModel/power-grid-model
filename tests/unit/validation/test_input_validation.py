e623ff7cccd0e5752c88314e9bda7e52f27e9008
Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>
2024-06-05 10:34:25 +0200

    Update tests/unit/validation/test_input_validation.py
    Co-authored-by: Jerry Guo <6221579+Jerry-Jinfeng-Guo@users.noreply.github.com>
    Signed-off-by: Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>

--- tests/unit/validation/test_input_validation.py
+++ tests/unit/validation/test_input_validation.py
@@ -578,10 +578,10 @@ def test_validate_input_data_transformer_tap_regulator(input_data):
     )
     # TODO (nbharambe) Add control side error after it is included
     # assert InvalidControlSideError("transformer_tap_regulator", "control_side", [52], BranchSide) in validation_errors
-    assert NotGreaterOrEqualError("transformer_tap_regulator", "u_set", [52], 0) in validation_errors
-    assert NotGreaterThanError("transformer_tap_regulator", "u_band", [52, 1], 0) in validation_errors
-    assert NotGreaterOrEqualError("transformer_tap_regulator", "line_drop_compensation_r", [52], 0) in validation_errors
-    assert NotGreaterOrEqualError("transformer_tap_regulator", "line_drop_compensation_x", [1], 0) in validation_errors
+    assert NotGreaterOrEqualError("transformer_tap_regulator", "u_set", [52], 0.0) in validation_errors
+    assert NotGreaterThanError("transformer_tap_regulator", "u_band", [52, 1], 0.0) in validation_errors
+    assert NotGreaterOrEqualError("transformer_tap_regulator", "line_drop_compensation_r", [52], 0.0) in validation_errors
+    assert NotGreaterOrEqualError("transformer_tap_regulator", "line_drop_compensation_x", [1], 0.0) in validation_errors
 
 
 def test_fault(input_data):
