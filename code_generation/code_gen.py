9c40f587e3e07dc1fb0dde7e5fad429236c5b237
Santiago Figueroa <santiago.figueroa.manrique@alliander.com>
2024-07-02 12:32:37 +0200

    Run and passing: all up to date
    Signed-off-by: Santiago Figueroa <santiago.figueroa.manrique@alliander.com>

--- code_generation/code_gen.py
+++ code_generation/code_gen.py
@@ -104,7 +104,7 @@ class CodeGenerator:
         all_map = {}
         for dataset in dataset_meta_data:
             if dataset.is_template:
-                prefixes = ["sym_"]
+                prefixes = ["sym_", "asym_"]
             else:
                 prefixes = [""]
             for prefix in prefixes:
