c603bb672b6582366207194526a32ccf4dc2bf2e
Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>
2024-08-09 16:06:44 +0200

    [skip ci] Added Meta class for consistency
    Signed-off-by: Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>

--- power_grid_model_c/power_grid_model_cpp/include/power_grid_model_cpp/meta_data.hpp
+++ power_grid_model_c/power_grid_model_cpp/include/power_grid_model_cpp/meta_data.hpp
@@ -11,80 +11,81 @@
 #include "meta_data.h"
 
 namespace power_grid_model_cpp {
-    
-PGM_Idx meta_n_datasets(PGM_Handle* handle) {
-    return PGM_meta_n_datasets(handle);
-}
-    
-PGM_MetaDataset const* meta_get_dataset_by_idx(PGM_Handle* handle, PGM_Idx idx) {
-    return PGM_meta_get_dataset_by_idx(handle, idx);
-}
 
-PGM_MetaDataset const* meta_get_dataset_by_name(PGM_Handle* handle, char const* dataset) {
-    return PGM_meta_get_dataset_by_name(handle, dataset);
-}
+class Meta {
+public:
+    static PGM_Idx n_datasets(PGM_Handle* handle) {
+        return PGM_meta_n_datasets(handle);
+    }
+        
+    static PGM_MetaDataset const* get_dataset_by_idx(PGM_Handle* handle, PGM_Idx idx) {
+        return PGM_meta_get_dataset_by_idx(handle, idx);
+    }
 
-char const* meta_dataset_name(PGM_Handle* handle, PGM_MetaDataset const* dataset) {
-    return PGM_meta_dataset_name(handle, dataset);
-}
+    static PGM_MetaDataset const* get_dataset_by_name(PGM_Handle* handle, char const* dataset) {
+        return PGM_meta_get_dataset_by_name(handle, dataset);
+    }
 
-PGM_Idx meta_n_components(PGM_Handle* handle, PGM_MetaDataset const* dataset) {
-    return PGM_meta_n_components(handle, dataset);
-}
+    static char const* dataset_name(PGM_Handle* handle, PGM_MetaDataset const* dataset) {
+        return PGM_meta_dataset_name(handle, dataset);
+    }
 
-PGM_MetaComponent const* meta_get_component_by_idx(PGM_Handle* handle, PGM_MetaDataset const* dataset,
-                                                               PGM_Idx idx) {
-    return PGM_meta_get_component_by_idx(handle, dataset, idx);
-}
+    static PGM_Idx n_components(PGM_Handle* handle, PGM_MetaDataset const* dataset) {
+        return PGM_meta_n_components(handle, dataset);
+    }
 
-PGM_MetaComponent const* meta_get_component_by_name(PGM_Handle* handle, char const* dataset,
-                                                                char const* component) {
-    return PGM_meta_get_component_by_name(handle, dataset, component);
-}
+    static PGM_MetaComponent const* get_component_by_idx(PGM_Handle* handle, PGM_MetaDataset const* dataset,
+                                                                PGM_Idx idx) {
+        return PGM_meta_get_component_by_idx(handle, dataset, idx);
+    }
 
-char const* meta_component_name(PGM_Handle* handle, PGM_MetaComponent const* component) {
-    return PGM_meta_component_name(handle, component);
-}
+    static PGM_MetaComponent const* get_component_by_name(PGM_Handle* handle, char const* dataset,
+                                                                    char const* component) {
+        return PGM_meta_get_component_by_name(handle, dataset, component);
+    }
 
-size_t meta_component_size(PGM_Handle* handle, PGM_MetaComponent const* component) {
-    return PGM_meta_component_size(handle, component);
-}
+    static char const* component_name(PGM_Handle* handle, PGM_MetaComponent const* component) {
+        return PGM_meta_component_name(handle, component);
+    }
 
-size_t meta_component_alignment(PGM_Handle* handle, PGM_MetaComponent const* component) {
-    return PGM_meta_component_alignment(handle, component);
-}
+    static size_t component_size(PGM_Handle* handle, PGM_MetaComponent const* component) {
+        return PGM_meta_component_size(handle, component);
+    }
 
-PGM_Idx meta_n_attributes(PGM_Handle* handle, PGM_MetaComponent const* component) {
-    return PGM_meta_n_attributes(handle, component);
-}
+    static size_t component_alignment(PGM_Handle* handle, PGM_MetaComponent const* component) {
+        return PGM_meta_component_alignment(handle, component);
+    }
 
-PGM_MetaAttribute const* meta_get_attribute_by_idx(PGM_Handle* handle, PGM_MetaComponent const* component,
-                                                               PGM_Idx idx) {
-    return PGM_meta_get_attribute_by_idx(handle, component, idx);
-}
+    static PGM_Idx n_attributes(PGM_Handle* handle, PGM_MetaComponent const* component) {
+        return PGM_meta_n_attributes(handle, component);
+    }
 
-PGM_MetaAttribute const* meta_get_attribute_by_name(PGM_Handle* handle, char const* dataset,
-                                                                char const* component, char const* attribute) {
-    return PGM_meta_get_attribute_by_name(handle, dataset, component, attribute);
-}
+    static PGM_MetaAttribute const* get_attribute_by_idx(PGM_Handle* handle, PGM_MetaComponent const* component,
+                                                                PGM_Idx idx) {
+        return PGM_meta_get_attribute_by_idx(handle, component, idx);
+    }
 
-char const* meta_attribute_name(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
-    return PGM_meta_attribute_name(handle, attribute);
-}
+    static PGM_MetaAttribute const* get_attribute_by_name(PGM_Handle* handle, char const* dataset,
+                                                                    char const* component, char const* attribute) {
+        return PGM_meta_get_attribute_by_name(handle, dataset, component, attribute);
+    }
 
-PGM_Idx meta_attribute_ctype(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
-    return PGM_meta_attribute_ctype(handle, attribute);
-}
+    static char const* attribute_name(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
+        return PGM_meta_attribute_name(handle, attribute);
+    }
 
-size_t meta_attribute_offset(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
-    return PGM_meta_attribute_offset(handle, attribute);
-}
+    static PGM_Idx attribute_ctype(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
+        return PGM_meta_attribute_ctype(handle, attribute);
+    }
 
-int is_little_endian(PGM_Handle* handle) {
-    return PGM_is_little_endian(handle);
-}
+    static size_t attribute_offset(PGM_Handle* handle, PGM_MetaAttribute const* attribute) {
+        return PGM_meta_attribute_offset(handle, attribute);
+    }
 
-
+    static int is_little_endian(PGM_Handle* handle) {
+        return PGM_is_little_endian(handle);
+    }
+};
 } // namespace power_grid_model_cpp
 
 #endif // POWER_GRID_MODEL_CPP_META_DATA_HPP
