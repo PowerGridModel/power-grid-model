90fc9ab6936b21db85c28a8932cf4b08ab68fe60
Tony Xiang <tony.xiang@alliander.com>
2024-09-29 10:37:35 +0200

    remove static in options
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- power_grid_model_c/power_grid_model_cpp/include/power_grid_model_cpp/options.hpp
+++ power_grid_model_c/power_grid_model_cpp/include/power_grid_model_cpp/options.hpp
@@ -18,55 +18,28 @@ class Options {
 
     RawOptions* get() const { return options_.get(); }
 
-    static void set_calculation_type(Options const& options, Idx type) {
-        options.handle_.call_with(PGM_set_calculation_type, options.get(), type);
-    }
-    void set_calculation_type(Idx type) const { set_calculation_type(*this, type); }
+    void set_calculation_type(Idx type) const { handle_.call_with(PGM_set_calculation_type, get(), type); }
 
-    static void set_calculation_method(Options const& options, Idx method) {
-        options.handle_.call_with(PGM_set_calculation_method, options.get(), method);
-    }
-    void set_calculation_method(Idx method) const { set_calculation_method(*this, method); }
+    void set_calculation_method(Idx method) const { handle_.call_with(PGM_set_calculation_method, get(), method); }
 
-    static void set_symmetric(Options const& options, Idx sym) {
-        options.handle_.call_with(PGM_set_symmetric, options.get(), sym);
-    }
-    void set_symmetric(Idx sym) const { set_symmetric(*this, sym); }
+    void set_symmetric(Idx sym) const { handle_.call_with(PGM_set_symmetric, get(), sym); }
 
-    static void set_err_tol(Options const& options, double err_tol) {
-        options.handle_.call_with(PGM_set_err_tol, options.get(), err_tol);
-    }
-    void set_err_tol(double err_tol) const { set_err_tol(*this, err_tol); }
+    void set_err_tol(double err_tol) const { handle_.call_with(PGM_set_err_tol, get(), err_tol); }
 
-    static void set_max_iter(Options const& options, Idx max_iter) {
-        options.handle_.call_with(PGM_set_max_iter, options.get(), max_iter);
-    }
-    void set_max_iter(Idx max_iter) const { set_max_iter(*this, max_iter); }
+    void set_max_iter(Idx max_iter) const { handle_.call_with(PGM_set_max_iter, get(), max_iter); }
 
-    static void set_threading(Options const& options, Idx threading) {
-        options.handle_.call_with(PGM_set_threading, options.get(), threading);
-    }
-    void set_threading(Idx threading) const { set_threading(*this, threading); }
+    void set_threading(Idx threading) const { handle_.call_with(PGM_set_threading, get(), threading); }
 
-    static void set_short_circuit_voltage_scaling(Options const& options, Idx short_circuit_voltage_scaling) {
-        options.handle_.call_with(PGM_set_short_circuit_voltage_scaling, options.get(), short_circuit_voltage_scaling);
-    }
     void set_short_circuit_voltage_scaling(Idx short_circuit_voltage_scaling) const {
-        set_short_circuit_voltage_scaling(*this, short_circuit_voltage_scaling);
+        handle_.call_with(PGM_set_short_circuit_voltage_scaling, get(), short_circuit_voltage_scaling);
     }
 
-    static void set_tap_changing_strategy(Options const& options, Idx tap_changing_strategy) {
-        options.handle_.call_with(PGM_set_tap_changing_strategy, options.get(), tap_changing_strategy);
-    }
     void set_tap_changing_strategy(Idx tap_changing_strategy) const {
-        set_tap_changing_strategy(*this, tap_changing_strategy);
+        handle_.call_with(PGM_set_tap_changing_strategy, get(), tap_changing_strategy);
     }
 
-    static void set_experimental_features(Options const& options, Idx experimental_features) {
-        options.handle_.call_with(PGM_set_experimental_features, options.get(), experimental_features);
-    }
     void set_experimental_features(Idx experimental_features) const {
-        set_experimental_features(*this, experimental_features);
+        handle_.call_with(PGM_set_experimental_features, get(), experimental_features);
     }
 
   private:
