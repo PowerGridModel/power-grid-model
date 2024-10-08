a6260e012ac034ebdc71f9b29b11e987e105ecb0
Martijn Govers <Martijn.Govers@Alliander.com>
2024-10-08 10:40:19 +0200

    fix
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- build.sh
+++ build.sh
@@ -71,8 +71,9 @@ if [[ "${COVERAGE}" ]];  then
     -d ${BUILD_DIR}/tests/cpp_unit_tests/CMakeFiles/power_grid_model_unit_tests.dir \
     -d ${BUILD_DIR}/tests/cpp_integration_tests/CMakeFiles/power_grid_model_integration_tests.dir \
     -d ${BUILD_DIR}/tests/cpp_validation_tests/CMakeFiles/power_grid_model_validation_tests.dir \
-    -d ${BUILD_DIR}/tests/c_api_tests/CMakeFiles/power_grid_model_c_api_tests.dir \
+    -d ${BUILD_DIR}/tests/native_api_tests/CMakeFiles/power_grid_model_api_tests.dir \
     -d ${BUILD_DIR}/power_grid_model_c/power_grid_model_c/CMakeFiles/power_grid_model_c.dir \
+    -d ${BUILD_DIR}/power_grid_model_c_example/CMakeFiles/power_grid_model_c_example.dir \
     -b . \
     --no-external \
     --output-file cpp_coverage.info \
