a6995b801fcc3ccfd5ed3b8b42247c60d241c3d9
Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>
2024-10-18 15:01:41 +0200

    removed unnecesary stuff
    Signed-off-by: Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>

--- tests/cpp_validation_tests/test_entry_point.cpp
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
-//
-// SPDX-License-Identifier: MPL-2.0
-
-// main cpp file
-
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
-#include <doctest/doctest.h>
