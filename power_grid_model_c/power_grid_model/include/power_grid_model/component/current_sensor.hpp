8dc43749daab5023ff328edeefc934baf26a5b9b
petersalemink95 <peter.salemink95@gmail.com>
2024-12-24 15:19:56 +0100

    current sensor file with license
    Signed-off-by: petersalemink95 <peter.salemink95@gmail.com>

---
+++ power_grid_model_c/power_grid_model/include/power_grid_model/component/current_sensor.hpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include "sensor.hpp"
+
+namespace power_grid_model {} // namespace power_grid_model
