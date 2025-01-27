f8c88bf879ab4b839a180e3c0f217abfb0cc69be
petersalemink95 <peter.salemink95@gmail.com>
2025-01-27 17:26:07 +0100

    clean-up
    Signed-off-by: petersalemink95 <peter.salemink95@gmail.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/optimizer/tap_position_optimizer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/optimizer/tap_position_optimizer.hpp
@@ -899,12 +899,11 @@ class TapPositionOptimizerImpl<std::tuple<TransformerTypes...>, StateCalculator,
                         adjust_transformer(regulator, state, result, update_data, search, options) || tap_changed;
                 }
                 if (tap_changed) {
-                    iterations_per_rank[rank_index + 1] = 0; // NOSONAR
-                    iterations_per_rank[rank_index] += 1;    // NOSONAR
+                    iterations_per_rank[rank_index + 1] = 0;
+                    iterations_per_rank[rank_index] += 1;
                     break;
                 }
                 ++rank_index;
-                // iterations_per_rank[++rank_index] = 0; // NOSONAR
             }
             if (tap_changed) {
                 if (static_cast<uint64_t>(iterations_per_rank[rank_index]) > 2 * max_tap_ranges_per_rank[rank_index]) {
@@ -913,9 +912,6 @@ class TapPositionOptimizerImpl<std::tuple<TransformerTypes...>, StateCalculator,
                 update_state(update_data);
                 result = calculate_(state, method);
                 ++total_iterations;
-                if (total_iterations > 50) { // TODO: remove - only for debug purpose
-                    throw MaxIterationReached{" 15 TapPositionOptimizer::iterate"};
-                }
             }
         }
 
