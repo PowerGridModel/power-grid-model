df407e2b998f4e43ab8a614bf3d6984191c07ad7
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2025-03-19 11:54:10 +0100

    Address case when user has a specific target
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ if platform.system() == "Windows":
     if_win = True
 elif platform.system() in ["Linux", "Darwin"]:
     if_win = False
-    if platform.system() == "Darwin":
+    if platform.system() == "Darwin" and (
+        "MACOSX_DEPLOYMENT_TARGET" not in os.environ or float(os.environ["MACOSX_DEPLOYMENT_TARGET"]) < 13.3
+    ):
         os.environ["MACOSX_DEPLOYMENT_TARGET"] = "13.3"
 else:
     raise SystemError("Only Windows, Linux, or MacOS is supported!")
