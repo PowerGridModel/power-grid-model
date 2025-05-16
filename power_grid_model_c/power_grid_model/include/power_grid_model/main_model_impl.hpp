ca56983ff4f7305fc46459a2a179450543ab5b69
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2025-05-16 16:10:58 +0200

    remove experimental flag
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/main_model_impl.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/main_model_impl.hpp
@@ -801,12 +801,7 @@ class MainModelImpl<ExtraRetrievableTypes<ExtraRetrievableType...>, ComponentLis
 
     CalculationInfo calculation_info() const { return calculation_info_; }
 
-    void check_no_experimental_features_used(Options const& options) const {
-        if (options.calculation_type == CalculationType::state_estimation &&
-            state_.components.template size<GenericCurrentSensor>() > 0) {
-            throw ExperimentalFeature{"State estimation", "current sensors"};
-        }
-    }
+    void check_no_experimental_features_used(Options const& options) const {}
 
   private:
     template <typename Component, typename MathOutputType, typename ResIt>
