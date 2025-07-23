cfc1e9377b50e76c9ef21c9c0a13e05c56c07bca
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2025-07-23 17:31:27 +0200

    [skip ci] resolved some more comments
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/job_dispatch_adapter.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/job_dispatch_adapter.hpp
@@ -40,14 +40,12 @@ class JobDispatchAdapter : public JobDispatchInterface<JobDispatchAdapter<MainMo
     std::reference_wrapper<MainModel> model_;
 
     main_core::utils::SequenceIdx<ComponentType...> current_scenario_sequence_cache_{};
-    std::shared_ptr<const power_grid_model::main_core::utils::ComponentFlags<ComponentType...>> components_to_update_;
-    std::shared_ptr<const power_grid_model::main_core::update::independence::UpdateIndependence<ComponentType...>>
-        update_independence_;
-    std::shared_ptr<const main_core::utils::SequenceIdx<ComponentType...>> all_scenarios_sequence_;
+    power_grid_model::main_core::utils::ComponentFlags<ComponentType...> components_to_update_{};
+    power_grid_model::main_core::update::independence::UpdateIndependence<ComponentType...> update_independence_{};
+    main_core::utils::SequenceIdx<ComponentType...> all_scenarios_sequence_{};
     main_core::utils::ComponentFlags<ComponentType...> independence_flags_{};
 
     // TODO(figueroa1395): Keep calculation_fn at the adapter level only
-
     template <typename Calculate>
         requires std::invocable<std::remove_cvref_t<Calculate>, MainModel&, MutableDataset const&, Idx>
     void calculate_impl(Calculate&& calculation_fn, MutableDataset const& result_data, Idx scenario_idx) const {
@@ -77,28 +75,19 @@ class JobDispatchAdapter : public JobDispatchInterface<JobDispatchAdapter<MainMo
     void prepare_job_impl(ConstDataset const& update_data) {
         // cache component update order where possible.
         // the order for a cacheable (independent) component by definition is the same across all scenarios
-        components_to_update_ =
-            std::make_shared<const power_grid_model::main_core::utils::ComponentFlags<ComponentType...>>(
-                model_.get().get_components_to_update(update_data));
-        update_independence_ = std::make_shared<
-            const power_grid_model::main_core::update::independence::UpdateIndependence<ComponentType...>>(
-            main_core::update::independence::check_update_independence<ComponentType...>(model_.get().state(),
-                                                                                         update_data));
-        assert(components_to_update_ != nullptr);
-        assert(update_independence_ != nullptr);
-        all_scenarios_sequence_ = std::make_shared<const main_core::utils::SequenceIdx<ComponentType...>>(
-            main_core::update::get_all_sequence_idx_map<ComponentType...>(
-                model_.get().state(), update_data, 0, *components_to_update_, *update_independence_, false));
+        components_to_update_ = model_.get().get_components_to_update(update_data);
+        update_independence_ = main_core::update::independence::check_update_independence<ComponentType...>(
+            model_.get().state(), update_data);
+        all_scenarios_sequence_ = main_core::update::get_all_sequence_idx_map<ComponentType...>(
+            model_.get().state(), update_data, 0, components_to_update_, update_independence_, false);
 
-        std::ranges::transform(*update_independence_, independence_flags_.begin(),
+        std::ranges::transform(update_independence_, independence_flags_.begin(),
                                [](auto const& comp) { return comp.is_independent(); });
     }
 
     void setup_impl(ConstDataset const& update_data, Idx scenario_idx) {
-        assert(components_to_update_ != nullptr);
-        assert(update_independence_ != nullptr);
         current_scenario_sequence_cache_ = main_core::update::get_all_sequence_idx_map<ComponentType...>(
-            model_.get().state(), update_data, scenario_idx, *components_to_update_, *update_independence_, true);
+            model_.get().state(), update_data, scenario_idx, components_to_update_, update_independence_, true);
         model_.get().template update_components<cached_update_t>(update_data, scenario_idx,
                                                                  get_current_scenario_sequence_view_());
     }
@@ -118,7 +107,7 @@ class JobDispatchAdapter : public JobDispatchInterface<JobDispatchAdapter<MainMo
         return main_core::utils::run_functor_with_all_types_return_array<ComponentType...>([this]<typename CT>() {
             constexpr auto comp_idx = main_core::utils::index_of_component<CT, ComponentType...>;
             if (std::get<comp_idx>(independence_flags_)) {
-                return std::span<Idx2D const>{std::get<comp_idx>(*all_scenarios_sequence_)};
+                return std::span<Idx2D const>{std::get<comp_idx>(all_scenarios_sequence_)};
             }
             return std::span<Idx2D const>{std::get<comp_idx>(current_scenario_sequence_cache_)};
         });
