1880d89f677dec95e765d023ae77cf713c51a4df
Jerry Guo <6221579+Jerry-Jinfeng-Guo@users.noreply.github.com>
2025-07-29 15:48:26 +0200

    Update power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/observability.hpp
    Signed-off-by: Jerry Guo <6221579+Jerry-Jinfeng-Guo@users.noreply.github.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/observability.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/observability.hpp
@@ -150,7 +150,7 @@ inline bool necessary_observability_condition(ObservabilitySensorsResult const& 
         throw NotObservableError{
             "The total number of independent power sensors is not enough to make the grid observable."};
     }
-    // If there are any voltage phasor sensor, one will not be used:
+    // If there are any voltage phasor sensor, one will be reserved as reference and not be used:
     //      n_flow_sensors + n_voltage_phasor_sensors - 1 < n_bus - 1
     if (n_voltage_phasor_sensors > 0 && n_flow_sensors + n_voltage_phasor_sensors < n_bus) {
         throw NotObservableError{"The total number of independent power sensors and voltage phasor sensors is not "
