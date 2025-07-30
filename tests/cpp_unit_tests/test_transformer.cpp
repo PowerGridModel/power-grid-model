0554d69f2e6f58e0f67d7af31b1c94e3accfbc06
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2025-07-30 11:25:15 +0200

    const plus notebook reset
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- tests/cpp_unit_tests/test_transformer.cpp
+++ tests/cpp_unit_tests/test_transformer.cpp
@@ -163,18 +163,18 @@ TEST_CASE("Test transformer") {
 
     SUBCASE("periodic clock input") {
         input.clock = 24;
-        Transformer trafo_24(input, 150.0e3, 10.0e3);
+        Transformer const trafo_24(input, 150.0e3, 10.0e3);
         input.clock = 36;
-        Transformer trafo_36(input, 150.0e3, 10.0e3);
+        Transformer const trafo_36(input, 150.0e3, 10.0e3);
         input.clock = -2;
-        Transformer trafo_m2(input, 150.0e3, 10.0e3);
+        Transformer const trafo_m2(input, 150.0e3, 10.0e3);
         CHECK(trafo_24.clock() == 0);
         CHECK(trafo_36.clock() == 0);
         CHECK(trafo_m2.clock() == 10);
 
         input.winding_to = WindingType::delta;
         input.clock = 25;
-        Transformer trafo_25(input, 150.0e3, 10.0e3);
+        Transformer const trafo_25(input, 150.0e3, 10.0e3);
         CHECK(trafo_25.clock() == 1);
     }
 
