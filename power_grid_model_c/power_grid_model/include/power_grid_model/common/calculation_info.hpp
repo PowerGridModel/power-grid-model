eff593f4702e0b923cbbc117e22cfb7854f21056
Martijn Govers <Martijn.Govers@Alliander.com>
2025-07-31 09:49:50 +0200

    revert reintroducing alignas
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/common/calculation_info.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/common/calculation_info.hpp
@@ -11,12 +11,6 @@
 
 namespace power_grid_model {
 
-// We would prefer to use std::hardware_destructive_interference_size, but as explained in
-// https://discourse.llvm.org/t/rfc-c-17-hardware-constructive-destructive-interference-size/48674/7
-// and https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Winterference-size, there is no
-// good way to determine the size of the cache line in a portable way at compile-time.
-constexpr size_t cache_line_size = 64;
-
-class alignas(cache_line_size) CalculationInfo : public std::map<std::string, double, std::less<>> {};
+using CalculationInfo = std::map<std::string, double, std::less<>>;
 
 } // namespace power_grid_model
