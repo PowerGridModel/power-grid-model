6a9354a461947b9812bc97e88b1e0abe0b94d6bd
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2025-08-08 16:20:40 +0200

    silence sonar 2
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/job_dispatch_interface.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/job_dispatch_interface.hpp
@@ -19,59 +19,59 @@ template <typename Adapter> class JobDispatchInterface {
     // the multiple  NOSONARs are used to avoid the complaints about the unnamed concepts
     template <typename ResultDataset>
     void calculate(ResultDataset const& result_data, Idx pos = 0)
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.calculate_impl(result_data, pos) } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         return static_cast<Adapter*>(this)->calculate_impl(result_data, pos);
     }
 
     void cache_calculate()
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.cache_calculate_impl() } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         return static_cast<Adapter*>(this)->cache_calculate_impl();
     }
 
     template <typename UpdateDataset>
     void prepare_job_dispatch(UpdateDataset const& update_data)
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.prepare_job_dispatch_impl(update_data) } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         return static_cast<Adapter*>(this)->prepare_job_dispatch_impl(update_data);
     }
 
     template <typename UpdateDataset>
     void setup(UpdateDataset const& update_data, Idx scenario_idx)
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.setup_impl(update_data, scenario_idx) } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         return static_cast<Adapter*>(this)->setup_impl(update_data, scenario_idx);
     }
 
     void winddown()
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.winddown_impl() } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         return static_cast<Adapter*>(this)->winddown_impl();
     }
 
     CalculationInfo get_calculation_info() const
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.get_calculation_info_impl() } -> std::same_as<CalculationInfo>;
-        } // NOSONAR
+        }
     {
         return static_cast<const Adapter*>(this)->get_calculation_info_impl();
     }
 
     void thread_safe_add_calculation_info(CalculationInfo const& info)
-        requires requires(Adapter& adapter) {
+        requires requires(Adapter& adapter) { // NOSONAR
             { adapter.thread_safe_add_calculation_info_impl(info) } -> std::same_as<void>;
-        } // NOSONAR
+        }
     {
         static_cast<Adapter*>(this)->thread_safe_add_calculation_info_impl(info);
     }
