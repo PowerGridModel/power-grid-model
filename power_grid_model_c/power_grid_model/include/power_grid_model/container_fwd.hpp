79c1da4d5fe81baf2f3a3c6c36266b3db9078ce7
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-08-26 10:31:43 +0200

    workaround compiler bug and address end()
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/container_fwd.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/container_fwd.hpp
@@ -10,15 +10,22 @@
 
 namespace power_grid_model::common {
 
-template <typename ContainerType, typename... RetrievableType>
-concept component_container_c = (requires(ContainerType const& c, ID id, Idx2D idx2d) {
+namespace detail {
+template <typename ContainerType, typename RetrievableType>
+concept single_component_container_c = requires(ContainerType const& c, ID id, Idx2D idx2d) {
     { c.template citer<RetrievableType>().begin() } -> std::forward_iterator;
     { c.template citer<RetrievableType>().end() } -> std::forward_iterator;
     { *(c.template citer<RetrievableType>().begin()) } -> std::same_as<RetrievableType const&>;
-    { *(c.template citer<RetrievableType>().end()) } -> std::same_as<RetrievableType const&>;
+    {
+        c.template citer<RetrievableType>().end()
+    } -> std::same_as<decltype(c.template citer<RetrievableType>().begin())>;
     { c.template get_item<RetrievableType>(id) } -> std::convertible_to<RetrievableType const&>;
     { c.template size<RetrievableType>() } -> std::same_as<Idx>;
     { c.template get_seq<RetrievableType>(idx2d) } -> std::same_as<Idx>;
-} && ...);
+};
+} // namespace detail
+
+template <typename ContainerType, typename... RetrievableType>
+concept component_container_c = (detail::single_component_container_c<ContainerType, RetrievableType> && ...);
 
 } // namespace power_grid_model::common
