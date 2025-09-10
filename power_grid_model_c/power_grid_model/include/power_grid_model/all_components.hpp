18849861a4367ba0ab7eaaf248ec9e974a8bdf6c
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-09-10 15:51:56 +0200

    Revert "add checks at all components"
    This reverts commit d8a7bfa348194ca65ba399d948b3989af3951481.
    
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/all_components.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/all_components.hpp
@@ -27,56 +27,9 @@
 #include "component/voltage_sensor.hpp"
 
 namespace power_grid_model {
-namespace detail {
-template <typename List, typename T, typename... NeedsTypes>
-concept dependent_type_check = !IsInList<T, List>::value || (IsInList<NeedsTypes, List>::value && ...);
-
-template <typename CompList>
-concept validate_component_types_c =
-    dependent_type_check<CompList, Source, Node> &&                  //
-    dependent_type_check<CompList, Line, Node> &&                    //
-    dependent_type_check<CompList, Link, Node> &&                    //
-    dependent_type_check<CompList, Transformer, Node> &&             //
-    dependent_type_check<CompList, GenericBranch, Node> &&           //
-    dependent_type_check<CompList, AsymLine, Node> &&                //
-    dependent_type_check<CompList, ThreeWindingTransformer, Node> && //
-    dependent_type_check<CompList, Shunt, Node> &&                   //
-    dependent_type_check<CompList, SymGenerator, Node> &&            //
-    dependent_type_check<CompList, AsymGenerator, Node> &&           //
-    dependent_type_check<CompList, SymLoad, Node> &&                 //
-    dependent_type_check<CompList, AsymLoad, Node> &&                //
-    dependent_type_check<CompList, SymVoltageSensor, Node> &&        //
-    dependent_type_check<CompList, AsymVoltageSensor, Node> &&       //
-    dependent_type_check<CompList, SymPowerSensor, Node, Line, AsymLine, Link, GenericBranch, Transformer,
-                         ThreeWindingTransformer, SymGenerator, AsymGenerator, SymLoad, AsymLoad> && //
-    dependent_type_check<CompList, AsymPowerSensor, Node, Line, AsymLine, Link, GenericBranch, Transformer,
-                         ThreeWindingTransformer, SymGenerator, AsymGenerator, SymLoad, AsymLoad> && //
-    dependent_type_check<CompList, SymCurrentSensor, Line, AsymLine, Link, GenericBranch, Transformer,
-                         ThreeWindingTransformer> && //
-    dependent_type_check<CompList, AsymCurrentSensor, Line, AsymLine, Link, GenericBranch, Transformer,
-                         ThreeWindingTransformer> &&                                                       //
-    dependent_type_check<CompList, TransformerTapRegulator, Node, Transformer, ThreeWindingTransformer> && //
-    dependent_type_check<CompList, Fault, Node>;
-
-} // namespace detail
 
 using AllComponents =
     ComponentList<Node, Line, AsymLine, Link, GenericBranch, Transformer, ThreeWindingTransformer, Shunt, Source,
                   SymGenerator, AsymGenerator, SymLoad, AsymLoad, SymPowerSensor, AsymPowerSensor, SymVoltageSensor,
                   AsymVoltageSensor, SymCurrentSensor, AsymCurrentSensor, Fault, TransformerTapRegulator>;
-
-static_assert(detail::validate_component_types_c<AllComponents>);
-static_assert(detail::validate_component_types_c<ComponentList<Node, Source>>);
-static_assert(detail::validate_component_types_c<ComponentList<Source, Node>>);
-static_assert(detail::validate_component_types_c<ComponentList<Node, Line>>);
-static_assert(!detail::validate_component_types_c<ComponentList<Line>>);
-static_assert(!detail::validate_component_types_c<ComponentList<Source, Line>>);
-
-// TODO We can also collect all Abstract types here
-// using AllAbstractTypes =
-//     ExtraRetrievableTypes<Base, Node, Branch, Branch3, Appliance, GenericLoadGen, GenericLoad, GenericGenerator,
-//                   GenericPowerSensor, GenericVoltageSensor, GenericCurrentSensor, Regulator>;
-
-// TODO Shall we make similar rules for Abstract types?
-
 } // namespace power_grid_model
