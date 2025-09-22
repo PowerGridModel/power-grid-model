806bf71350a1d0020de482bc1a3a6e1d0354d6bf
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-09-22 14:01:09 +0200

    remove constructible check
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- tests/cpp_unit_tests/test_main_model_type.cpp
+++ tests/cpp_unit_tests/test_main_model_type.cpp
@@ -18,9 +18,6 @@ struct AComponent {
 };
 } // namespace
 
-template <typename T>
-concept Constructible = requires { T{}; };
-
 static_assert(detail::validate_component_types_c<AllComponents>);
 
 static_assert(detail::validate_component_types_c<ComponentList<Node, Source>>);
@@ -30,11 +27,6 @@ static_assert(detail::validate_component_types_c<ComponentList<Source, Node>>);
 
 static_assert(!detail::validate_component_types_c<ComponentList<Line>>);
 static_assert(!detail::validate_component_types_c<ComponentList<Source, Line>>);
-
-static_assert(!Constructible<MainModelType<ExtraRetrievableTypes<Base, Branch>, ComponentList<Line>>>);
-static_assert(
-    !Constructible<MainModelType<ExtraRetrievableTypes<Base, Branch, Appliance>, ComponentList<Line, Source>>>);
-static_assert(Constructible<MainModelType<ExtraRetrievableTypes<Base, Branch>, ComponentList<Node, Line>>>);
 
 TEST_CASE("MainModelType") {
 
