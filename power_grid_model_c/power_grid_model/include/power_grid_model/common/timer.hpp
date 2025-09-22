996390033c8f0f2f4547b96c0d8d955371e9eba2
Martijn Govers <martijn.govers@alliander.com>
2025-09-22 14:37:44 +0200

    Update power_grid_model_c/power_grid_model/include/power_grid_model/common/timer.hpp
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>
    
    Co-authored-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>
    Signed-off-by: Martijn Govers <martygovers@hotmail.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/common/timer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/common/timer.hpp
@@ -26,7 +26,7 @@ class Timer {
     Timer(Logger& log, LogEvent code) : log_{&log}, code_{code}, start_{Clock::now()} {}
 
     Timer(Timer const&) = delete;
-    Timer(Timer&& other) noexcept : log_{other.log_}, code_{other.code_}, start_{other.start_} { other.log_ = nullptr; }
+    Timer(Timer&& other) noexcept : log_{std::exchange(other.log_, nullptr)}, code_{other.code_}, start_{other.start_} {}
     Timer& operator=(Timer const&) = delete;
 
     Timer& operator=(Timer&& timer) noexcept {
