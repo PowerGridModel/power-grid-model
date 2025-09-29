16348445d0bd75deac556b7b6d04b0d3b5fa409a
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-09-29 09:04:07 +0200

    address comments
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/job_interface.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/job_interface.hpp
@@ -18,67 +18,75 @@ class JobInterface {
   public:
     // the multiple  NOSONARs are used to avoid the complaints about the unnamed concepts
     template <typename Self, typename ResultDataset>
-    void calculate(this Self const& self, ResultDataset const& result_data, Idx pos = 0)
+    void calculate(this Self&& self, ResultDataset const& result_data, Idx pos = 0)
         requires requires { // NOSONAR
-            { self.calculate_impl(result_data, pos) } -> std::same_as<void>;
+            { std::forward<Self>(self).calculate_impl(result_data, pos) } -> std::same_as<void>;
         }
     {
-        return self.calculate_impl(result_data, pos);
+        return std::forward<Self>(self).calculate_impl(result_data, pos);
     }
 
     template <typename Self>
-    void cache_calculate(this Self const& self)
+    void cache_calculate(this Self&& self)
         requires requires { // NOSONAR
-            { self.cache_calculate_impl() } -> std::same_as<void>;
+            { std::forward<Self>(self).cache_calculate_impl() } -> std::same_as<void>;
         }
     {
-        return self.cache_calculate_impl();
+        return std::forward<Self>(self).cache_calculate_impl();
     }
 
     template <typename Self, typename UpdateDataset>
-    void prepare_job_dispatch(this Self& self, UpdateDataset const& update_data)
+    void prepare_job_dispatch(this Self&& self, UpdateDataset const& update_data)
         requires requires { // NOSONAR
-            { self.prepare_job_dispatch_impl(update_data) } -> std::same_as<void>;
+            { std::forward<Self>(self).prepare_job_dispatch_impl(update_data) } -> std::same_as<void>;
         }
     {
-        return self.prepare_job_dispatch_impl(update_data);
+        return std::forward<Self>(self).prepare_job_dispatch_impl(update_data);
     }
 
     template <typename Self, typename UpdateDataset>
-    void setup(this Self& self, UpdateDataset const& update_data, Idx scenario_idx)
+    void setup(this Self&& self, UpdateDataset const& update_data, Idx scenario_idx)
         requires requires { // NOSONAR
-            { self.setup_impl(update_data, scenario_idx) } -> std::same_as<void>;
+            { std::forward<Self>(self).setup_impl(update_data, scenario_idx) } -> std::same_as<void>;
         }
     {
-        return self.setup_impl(update_data, scenario_idx);
+        return std::forward<Self>(self).setup_impl(update_data, scenario_idx);
     }
 
     template <typename Self>
-    void winddown(this Self& self)
+    void winddown(this Self&& self)
         requires requires { // NOSONAR
-            { self.winddown_impl() } -> std::same_as<void>;
+            { std::forward<Self>(self).winddown_impl() } -> std::same_as<void>;
         }
     {
-        return self.winddown_impl();
+        return std::forward<Self>(self).winddown_impl();
     }
 
     template <typename Self>
-    void reset_logger(this Self& self)
+    void reset_logger(this Self&& self)
         requires requires { // NOSONAR
-            { self.reset_logger_impl() } -> std::same_as<void>;
+            { std::forward<Self>(self).reset_logger_impl() } -> std::same_as<void>;
         }
     {
-        self.reset_logger_impl();
+        std::forward<Self>(self).reset_logger_impl();
     }
 
     template <typename Self>
-    void set_logger(this Self& self, Logger& log)
+    void set_logger(this Self&& self, Logger& log)
         requires requires { // NOSONAR
-            { self.set_logger_impl(log) } -> std::same_as<void>;
+            { std::forward<Self>(self).set_logger_impl(log) } -> std::same_as<void>;
         }
     {
-        self.set_logger_impl(log);
+        std::forward<Self>(self).set_logger_impl(log);
     }
+
+  protected:
+    JobInterface() = default;
+    JobInterface(const JobInterface& /*other*/) = default;
+    JobInterface& operator=(const JobInterface& /*other*/) = default;
+    JobInterface(JobInterface&& /*other*/) noexcept = default;
+    JobInterface& operator=(JobInterface&& /*other*/) noexcept = default;
+    ~JobInterface() = default;
 };
 
 } // namespace power_grid_model
