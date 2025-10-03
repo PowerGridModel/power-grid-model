0b2a24747e0a95fb5acbcd973899f02edc6ee13d
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2025-10-03 09:05:14 +0200

    no nosonar
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/y_bus.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/y_bus.hpp
@@ -52,7 +52,7 @@ inline void counting_sort_element(std::vector<YBusElementMap>& vec, Idx n_bus) {
         counter[i] += counter[i - 1];
     }
     for (auto it_element = vec.rbegin(); it_element != vec.rend(); ++it_element) {
-        temp_vec[--counter[it_element->pos.second]] = std::move(*it_element); // NOSONAR
+        temp_vec[--counter[it_element->pos.second]] = std::move(*it_element);
     }
 
     // swap vectors to avoid copying
@@ -67,7 +67,7 @@ inline void counting_sort_element(std::vector<YBusElementMap>& vec, Idx n_bus) {
         counter[i] += counter[i - 1];
     }
     for (auto it_element = vec.rbegin(); it_element != vec.rend(); ++it_element) {
-        temp_vec[--counter[it_element->pos.first]] = std::move(*it_element); // NOSONAR
+        temp_vec[--counter[it_element->pos.first]] = std::move(*it_element);
     }
 
     // final swap to get result back in vec
