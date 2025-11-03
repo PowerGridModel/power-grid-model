c9eb00137cc1f3fbc9ecbd53f50730c2ddf9fbff
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-11-03 22:34:16 +0100

    revert and add documentation
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/component/transformer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/component/transformer.hpp
@@ -245,14 +245,15 @@ class Transformer : public Branch {
             param0.ytt() = (y0_series + y_shunt);
         }
         // ZN*
+        // Zero sequence impedance of zigzag winding is approximately 10% of positive sequence impedance
         if (winding_from_ == WindingType::zigzag_n && from_status()) {
-            DoubleComplex const z0_series = (1.0 / y_series) * 2.0 + 3.0 * z_grounding_from_ / k / k;
+            DoubleComplex const z0_series = (1.0 / y_series) * 0.1 + 3.0 * z_grounding_from_ / k / k;
             DoubleComplex const y0_series = 1.0 / z0_series;
             param0.yff() = y0_series / k / k;
         }
         // *zn
         if (winding_to_ == WindingType::zigzag_n && to_status()) {
-            DoubleComplex const z0_series = (1.0 / y_series) * 2.0 + 3.0 * z_grounding_to_;
+            DoubleComplex const z0_series = (1.0 / y_series) * 0.1 + 3.0 * z_grounding_to_;
             DoubleComplex const y0_series = 1.0 / z0_series;
             param0.ytt() = y0_series;
         }
