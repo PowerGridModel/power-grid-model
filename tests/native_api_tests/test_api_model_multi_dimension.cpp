d88db976d243213836d1ddb8bb90a82c610ea8bb
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-11-10 15:46:29 +0100

    start test
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

---
+++ tests/native_api_tests/test_api_model_multi_dimension.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include "power_grid_model_cpp.hpp"
+
+#include <power_grid_model_c/dataset_definitions.h>
+
+#include <doctest/doctest.h>
+
+TEST_CASE("API Model Multi-Dimension") {}
