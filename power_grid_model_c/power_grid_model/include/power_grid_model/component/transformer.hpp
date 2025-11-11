19ed1c26eac50288df51ca35689acdf873bae068
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-11-11 10:54:41 +0100

    approach 2
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/component/transformer.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/component/transformer.hpp
@@ -258,6 +258,22 @@ class Transformer : public Branch {
             param0.ytt() = y0_series;
         }
 
+        auto const low_from_admittance = 0.5 * 1e-6;
+        auto const low_to_admittance = 0.5 * 1e-6;
+        bool const zero_seq_available_from =
+            (winding_from_ == WindingType::wye_n && winding_to_ == WindingType::wye_n) ||
+            (winding_from_ == WindingType::wye_n && winding_to_ == WindingType::delta) ||
+            winding_from_ == WindingType::zigzag_n;
+        bool const zero_seq_available_to = (winding_to_ == WindingType::wye_n && winding_from_ == WindingType::wye_n) ||
+                                           (winding_to_ == WindingType::wye_n && winding_from_ == WindingType::delta) ||
+                                           winding_to_ == WindingType::zigzag_n;
+        if (!zero_seq_available_from && from_status()) {
+            param0.yff() += DoubleComplex{0.0, -low_from_admittance};
+        }
+        if (!zero_seq_available_to && to_status()) {
+            param0.ytt() += DoubleComplex{0.0, -low_to_admittance};
+        }
+
         // for the rest param0 is zero
         // calculate yabc
         ComplexTensor<asymmetric_t> const sym_matrix = get_sym_matrix();
@@ -270,18 +286,16 @@ class Transformer : public Branch {
             param.value[i] = dot(sym_matrix, y012, sym_matrix_inv);
         }
 
-        auto const low_from_admittance = 0.5 * 1e-8;
-        auto const low_to_admittance = 0.5 * 1e-8;
-        for (size_t phase = 0; phase < 3; ++phase) {
+        // for (size_t phase = 0; phase < 3; ++phase) {
 
-            if (from_status()) {
-                param.yff()(phase, phase) += DoubleComplex{0.0, -low_from_admittance};
-            }
-            if (to_status()) {
-                param.ytt()(phase, phase) += DoubleComplex{0.0, -low_to_admittance};
-            }
-        }
-        return param;
+        //     if (from_status()) {
+        //         param.yff()(phase, phase) += DoubleComplex{0.0, -low_from_admittance};
+        //     }
+        //     if (to_status()) {
+        //         param.ytt()(phase, phase) += DoubleComplex{0.0, -low_to_admittance};
+        //     }
+        // }
+        // return param;
     }
 };
 
