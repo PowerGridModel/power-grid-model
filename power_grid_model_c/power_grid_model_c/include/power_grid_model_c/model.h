30ec19ce679194834161da32beccffdf7f35cf4b
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-12-15 09:11:11 +0100

    Update power_grid_model_c/power_grid_model_c/include/power_grid_model_c/model.h
    Co-authored-by: Martijn Govers <martijn.govers@alliander.com>
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- power_grid_model_c/power_grid_model_c/include/power_grid_model_c/model.h
+++ power_grid_model_c/power_grid_model_c/include/power_grid_model_c/model.h
@@ -91,9 +91,9 @@ PGM_API void PGM_get_indexer(PGM_Handle* handle, PGM_PowerGridModel const* model
  * If batch_dataset == NULL, it is a one-time calculation.
  * If batch_dataset != NULL, it is a batch calculation with batch update in the batch_dataset.
  *
- * The user can use the function set_next_cartesian_product_dimension() to chain multiple batch datasets
- * to create a multi-dimension batch calculation. The calculation core will interpret the chained datasets
- * as a cartesian product of all the scenarios.
+ * The user can use the function set_next_cartesian_product_dimension() to combine multiple batch datasets
+ * to create a multi-dimension batch calculation using a linked list pattern. The calculation core will
+ * interpret the combined dataset as a cartesian product on a linked list of all the scenarios.
  * Each batch dataset in the chain represents one dimension of the cartesian product.
  *
  * You need to pre-allocate all output buffer.
