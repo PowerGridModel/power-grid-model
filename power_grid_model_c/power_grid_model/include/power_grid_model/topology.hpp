147a479e7ecd35db29b23a4f14557c3dd8dfc6b1
Martijn Govers <Martijn.Govers@Alliander.com>
2025-12-17 12:54:48 +0100

    add ubsan + hardening for gcc
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/topology.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/topology.hpp
@@ -353,8 +353,9 @@ class Topology {
         };
         // k as branch number for 2-way branch
         for (auto const& [idx, branch_node_idx, branch_connected] :
-             std::views::zip(std::views::iota(0), comp_topo_.branch_node_idx, comp_conn_.branch_connected)) {
-            assert(std::ssize(branch_connected) == 2); // NOSONAR(R354)
+             std::views::zip(std::views::iota(0), std::as_const(comp_topo_.branch_node_idx),
+                             std::as_const(comp_conn_.branch_connected))) {
+            assert(std::ssize(branch_connected) == 2);
 
             auto const [i, j] = branch_node_idx;
             IntS const i_status = branch_connected[0];
@@ -387,8 +388,9 @@ class Topology {
         }
         // k as branch number for 3-way branch
         for (auto const& [idx, i, i_status, j_math] :
-             std::views::zip(std::views::iota(0), comp_topo_.branch3_node_idx, comp_conn_.branch3_connected,
-                             std::views::drop(comp_coup_.node, comp_topo_.n_node))) {
+             std::views::zip(std::views::iota(0), std::as_const(comp_topo_.branch3_node_idx),
+                             std::as_const(comp_conn_.branch3_connected),
+                             std::views::drop(std::as_const(comp_coup_.node), comp_topo_.n_node))) {
             std::array<Idx2D, 3> const i_math{
                 comp_coup_.node[i[0]],
                 comp_coup_.node[i[1]],
@@ -553,7 +555,8 @@ class Topology {
         std::ranges::for_each(math_topology_,
                               [](MathModelTopology& topo) { topo.load_gen_type.resize(topo.n_load_gen()); });
         // assign load type
-        for (auto const& [idx_math, load_gen_type] : std::views::zip(comp_coup_.load_gen, comp_topo_.load_gen_type)) {
+        for (auto const& [idx_math, load_gen_type] :
+             std::views::zip(std::as_const(comp_coup_.load_gen), std::as_const(comp_topo_.load_gen_type))) {
             if (idx_math.group == -1) {
                 continue;
             }
