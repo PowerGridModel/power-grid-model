f1a1de8db82e48fc8bff649c5edfb77f6a243b59
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-12-29 14:51:48 +0100

    produce seg fault more frequently
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- tests/unit/test_python_threading.py
+++ tests/unit/test_python_threading.py
@@ -22,22 +22,21 @@ CORRUPTED_JSON_DATA = JSON_DATA[:-1]  # Remove the last character to corrupt the
 
 def json_deserialize_valid_json():
     """Test json_deserialize with valid JSON data."""
-    for _ in range(1000):
-        result = json_deserialize(JSON_DATA)
-        assert result == {}
+    result = json_deserialize(JSON_DATA)
+    assert result == {}
 
 
 def json_deserialize_invalid_json():
     """Test json_deserialize with invalid JSON data."""
-    for _ in range(1000):
-        with pytest.raises(PowerGridSerializationError):
-            json_deserialize(CORRUPTED_JSON_DATA)
+    with pytest.raises(PowerGridSerializationError):
+        json_deserialize(CORRUPTED_JSON_DATA)
 
 
 def test_python_threading_json_deserialization():
-    thread_1 = threading.Thread(target=json_deserialize_valid_json)
-    thread_2 = threading.Thread(target=json_deserialize_invalid_json)
-    thread_1.start()
-    thread_2.start()
-    thread_1.join()
-    thread_2.join()
+    for _ in range(1000):
+        thread_1 = threading.Thread(target=json_deserialize_valid_json)
+        thread_2 = threading.Thread(target=json_deserialize_invalid_json)
+        thread_1.start()
+        thread_2.start()
+        thread_1.join()
+        thread_2.join()
