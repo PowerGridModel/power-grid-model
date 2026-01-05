9854acea4bf36ea134073abaa7162811afc36609
Steffan Wullems <129064935+SteffanWullems@users.noreply.github.com>
2026-01-05 13:01:59 +0100

    Update src/power_grid_model/utils.py
    Co-authored-by: Martijn Govers <martygovers@hotmail.com>
    Signed-off-by: Steffan Wullems <129064935+SteffanWullems@users.noreply.github.com>

--- src/power_grid_model/utils.py
+++ src/power_grid_model/utils.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2025 Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
 # SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
 #
 # SPDX-License-Identifier: MPL-2.0
