072ea2033c4b152b6ceaba6c68b7644f6f8654ef
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2026-01-29 14:58:22 +0100

    test version
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- tests/cpp_cli_tests/test_cli.cpp
+++ tests/cpp_cli_tests/test_cli.cpp
@@ -21,7 +21,12 @@ constexpr std::string_view cli_executable = POWER_GRID_MODEL_CLI_EXECUTABLE;
 fs::path get_cli_tmp_path() {
     // Get the system temp directory
     fs::path const tmpdir = fs::temp_directory_path();
-    fs::path const cli_test_dir = tmpdir / "pgm_cli_test";
+    // Return the path
+    return tmpdir / "pgm_cli_test";
+}
+
+void clear_and_create_cli_tmp_path() {
+    fs::path const cli_test_dir = get_cli_tmp_path();
 
     // Remove the dir if it exists (including contents)
     if (fs::exists(cli_test_dir)) {
@@ -32,17 +37,15 @@ fs::path get_cli_tmp_path() {
     if (!fs::create_directory(cli_test_dir)) {
         throw std::runtime_error("Failed to create cli_test temp directory");
     }
-
-    // Return the path
-    return cli_test_dir;
 }
 
 TEST_CASE("Test CLI version") {
-    std::string const command = std::string{cli_executable} + " --version" + " > version.txt";
+    clear_and_create_cli_tmp_path();
+    fs::path const version_file = get_cli_tmp_path() / "version.txt";
+    std::string const command = std::string{cli_executable} + " --version" + " > " + version_file.string();
     int ret = std::system(command.c_str());
     REQUIRE(ret == 0);
-    fs::path const version_file = get_cli_tmp_path() / "version.txt";
-    std::ifstream version_ifs("version.txt");
+    std::ifstream version_ifs(version_file);
     REQUIRE(version_ifs);
     std::string version_line;
     std::getline(version_ifs, version_line);
