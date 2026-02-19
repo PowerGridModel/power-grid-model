d9b608321b2cf4fc1529235110355f1c06739746
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2026-02-19 10:13:15 +0100

    fix cmake
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- cmake/pgm_version.cmake
+++ cmake/pgm_version.cmake
@@ -13,8 +13,7 @@ string(
     "${_PGM_VERSION_STRIPPED}"
 )
 string(
-    REGEX REPLACE
-    "^([0-9]+)\\.[0-9]+(\\.[0-9]+)?.*"
+    REGEX REPLACE "^([0-9]+)\\.[0-9]+(\\.[0-9]+)?.*"
     "\\1"
     PGM_VERSION_MAJOR
     "${PGM_VERSION}"
