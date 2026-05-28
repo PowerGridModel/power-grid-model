3b9ad8873a30b3f71da002fb17bf5af0745f7b5a
Martijn Govers <Martijn.Govers@Alliander.com>
2026-05-28 07:42:13 +0200

    clang-format
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- tests/cpp_unit_tests/test_supernodes.cpp
+++ tests/cpp_unit_tests/test_supernodes.cpp
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-#include "power_grid_model/common/common.hpp"
 #include "power_grid_model/calculation_parameters.hpp"
+#include "power_grid_model/common/common.hpp"
 #include "power_grid_model/supernodes.hpp"
 
 #include <doctest/doctest.h>
