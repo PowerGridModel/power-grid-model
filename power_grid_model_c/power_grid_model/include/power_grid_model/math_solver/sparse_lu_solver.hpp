fa024f9e78b54a04bc70f849e063d27328deab8e
Martijn Govers <martijn.govers@alliander.com>
2026-06-02 09:17:56 +0200

    Update power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/sparse_lu_solver.hpp
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>
    
    Signed-off-by: Martijn Govers <martygovers@hotmail.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/sparse_lu_solver.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/math_solver/sparse_lu_solver.hpp
@@ -247,7 +247,7 @@ template <class Tensor, class RHSVector, class XVector> class SparseLUSolver {
     void inplace_selective_inverse_with_prefactorized_matrix(
         std::vector<Tensor>& data, // pre-factorized data, will be in-place modified to store selective inverse
         BlockPermArray const& block_perm_array // pre-calculated permutation, const ref
-    ) {
+    ) const {
         capturing::into_the_void(data, block_perm_array); // prevent compiler from complaining about unused variables
         if (has_pivot_perturbation_) {
             throw SparseMatrixError{};
