ae6cc2ba4da1a7d90a2d3f7d8f8f26904850e861
Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>
2026-09-07 17:55:22 +0200

    bugfix
    Signed-off-by: Santiago Figueroa Manrique <santiago.figueroa.manrique@alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/main_core/topological_node_output.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/main_core/topological_node_output.hpp
@@ -96,10 +96,10 @@ inline Idx get_node_sequence_idx(main_model_state_c auto const& state, Idx compo
     }
 }
 
-template <std::derived_from<Branch> ComponentType>
+template <std::derived_from<Edge> ComponentType>
 inline BranchIdx const& get_branch_sequence_idx(main_model_state_c auto const& state, Idx component_idx) {
     return state.comp_topo
-        ->branch_node_idx[get_component_sequence_offset<Branch, ComponentType>(state.components) + component_idx];
+        ->branch_node_idx[get_component_sequence_offset<Edge, ComponentType>(state.components) + component_idx];
 }
 
 template <std::derived_from<Branch3> ComponentType>
