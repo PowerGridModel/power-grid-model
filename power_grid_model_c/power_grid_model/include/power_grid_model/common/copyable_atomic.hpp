c9c9960dd4b6701cede5f15651c22f4bedf291d6
Martijn Govers <Martijn.Govers@Alliander.com>
2026-09-17 16:18:55 +0200

    sonar
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model/include/power_grid_model/common/copyable_atomic.hpp
+++ power_grid_model_c/power_grid_model/include/power_grid_model/common/copyable_atomic.hpp
@@ -12,7 +12,7 @@ namespace power_grid_model::common::atomic {
 template <typename T>
     requires(std::is_trivially_copyable_v<T> && std::copy_constructible<T> && std::move_constructible<T> &&
              std::is_copy_assignable_v<T> && std::is_move_assignable_v<T> && std::same_as<T, std::remove_cv_t<T>>)
-class CopyableAtomic : public std::atomic<T> { // NOSONAR(S3642)
+class CopyableAtomic : public std::atomic<T> { // NOSONAR(S3624)
   private:
     static constexpr std::memory_order copy_order = std::memory_order::seq_cst;
 
@@ -21,7 +21,7 @@ class CopyableAtomic : public std::atomic<T> { // NOSONAR(S3642)
     template <typename... Args>
         requires(std::constructible_from<T, Args...> && !std::same_as<std::remove_cvref_t<T>, CopyableAtomic<T>>)
     CopyableAtomic(Args&&... args) noexcept : std::atomic<T>{std::forward<Args>(args)...} {}
-    CopyableAtomic(CopyableAtomic const& other) noexcept : std::atomic<T>{other.load(copy_order)} {} // NOSONAR(S3642)
+    CopyableAtomic(CopyableAtomic const& other) noexcept : std::atomic<T>{other.load(copy_order)} {} // NOSONAR(S3624)
     CopyableAtomic(CopyableAtomic&& other) noexcept = default;
     CopyableAtomic& operator=(CopyableAtomic const& other) noexcept {
         if (this != &other) {
