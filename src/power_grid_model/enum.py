e30d0900a09089cb212168a6f0a9a172615ee951
Nitish Bharambe <nitish.bharambe@alliander.com>
2026-09-18 16:46:18 +0200

    input from poc
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- src/power_grid_model/enum.py
+++ src/power_grid_model/enum.py
@@ -19,6 +19,7 @@ from power_grid_model._core.enum import (
     FaultPhase,
     FaultType,
     LoadGenType,
+    LoggerType,
     MeasuredTerminalType,
     ShortCircuitVoltageScaling,
     TapChangingStrategy,
