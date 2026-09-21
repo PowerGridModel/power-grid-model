6155c047bdbe339ec983b46c0d7fcdedf7f24cba
Nitish Bharambe <nitish.bharambe@alliander.com>
2026-09-21 11:17:01 +0200

    fix move of NOLINT
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model_c/src/logger.hpp
+++ power_grid_model_c/power_grid_model_c/src/logger.hpp
@@ -49,8 +49,8 @@ inline PGM_Logger* make_logger(PGM_Idx type) {
 template <typename Callback, typename UserData>
 inline void logger_get_output(PGM_Logger const& pgm_logger, Callback callback, UserData user_data) {
     pgm_logger.logger->get_output([callback, user_data](std::string_view sv) {
-        callback(sv.data(), safe_cast<PGM_Idx>(sv.size()),
-                 user_data); // NOLINT(bugprone-suspicious-stringview-data-usage)
+        callback(sv.data(), safe_cast<PGM_Idx>(sv.size()), // NOLINT(bugprone-suspicious-stringview-data-usage)
+                 user_data);
     });
 }
 
