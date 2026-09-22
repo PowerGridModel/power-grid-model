252a9f81b032a43de75e10de54b2fbf5f9c95182
Nitish Bharambe <nitish.bharambe@alliander.com>
2026-09-22 11:30:37 +0200

    add scoping and constructor
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- power_grid_model_c/power_grid_model_c/src/logger.hpp
+++ power_grid_model_c/power_grid_model_c/src/logger.hpp
@@ -6,8 +6,38 @@
 
 #include "logger_fwd.hpp"
 
+#include <power_grid_model/common/exception.hpp>
+
 #include <memory>
 
 namespace power_grid_model_c {
 [[nodiscard]] HandleLogger make_handle_logger();
+
+template <typename Module, typename Logger>
+concept logger_scoped_module_c = requires(Module& module, Logger& logger) {
+    module.set_logger(logger);
+    { module.reset_logger() } noexcept;
+    { module.logger_empty() } noexcept;
+};
+
+template <typename Module, typename Logger>
+    requires logger_scoped_module_c<Module, Logger>
+class ScopedModuleLogger {
+  public:
+    ScopedModuleLogger(Module& module, Logger& logger) : module_{module}, logger_{logger} {
+        if (!module_.logger_empty()) {
+            throw power_grid_model::UnreachableHit{"ScopedModuleLogger", "module has no logger set"};
+        }
+        module_.set_logger(logger_);
+    }
+    ScopedModuleLogger(ScopedModuleLogger const&) = delete;
+    ScopedModuleLogger& operator=(ScopedModuleLogger const&) = delete;
+    ScopedModuleLogger(ScopedModuleLogger&&) = delete;
+    ScopedModuleLogger& operator=(ScopedModuleLogger&&) = delete;
+    ~ScopedModuleLogger() noexcept { module_.reset_logger(); }
+
+  private:
+    Module& module_;
+    Logger& logger_;
+};
 } // namespace power_grid_model_c
