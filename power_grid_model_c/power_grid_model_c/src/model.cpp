e1aed0ee10d3bd224eed578e411a999c21f7836e
Martijn Govers <Martijn.Govers@Alliander.com>
2026-09-22 11:32:20 +0200

    minor cleanup
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- power_grid_model_c/power_grid_model_c/src/model.cpp
+++ power_grid_model_c/power_grid_model_c/src/model.cpp
@@ -7,6 +7,7 @@
 #include "handle.hpp"
 #include "input_sanitization.hpp"
 #include "logger.hpp"
+#include "logger_fwd.hpp"
 #include "math_solver.hpp"
 #include "options.hpp" // NOLINT(misc-include-cleaner)
 #include "safe_memory_handling.hpp"
@@ -38,6 +39,7 @@ using power_grid_model_c::cast_to_c;
 using power_grid_model_c::cast_to_cpp;
 using power_grid_model_c::create;
 using power_grid_model_c::destroy;
+using power_grid_model_c::get_logger;
 using power_grid_model_c::get_math_solver_dispatcher;
 using power_grid_model_c::safe_enum;
 using power_grid_model_c::safe_ptr;
@@ -45,15 +47,13 @@ using power_grid_model_c::safe_ptr_get;
 using power_grid_model_c::safe_ptr_maybe_nullptr;
 using power_grid_model_c::safe_str_view;
 using power_grid_model_c::ScopedModuleLogger;
-
-auto& get_logger(PGM_Handle const& handle) { return safe_ptr_get(handle.logger.get()); }
 } // namespace
 
 // create model
 PGM_PowerGridModel* PGM_create_model(PGM_Handle* handle, double system_frequency,
                                      PGM_ConstDataset const* input_dataset) noexcept {
     return call_with_catch(handle, [system_frequency, input_dataset, handle] {
-        auto& logger = get_logger(safe_ptr_get(handle));
+        auto& logger = get_logger(safe_ptr_get(handle).logger);
         auto model = std::unique_ptr<MainModel>{create<MainModel>(
             system_frequency, safe_ptr_get(cast_to_cpp(input_dataset)), get_math_solver_dispatcher(), 0, logger)};
         model->reset_logger();
@@ -72,7 +72,7 @@ void PGM_update_model(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_ConstDa
 // copy model
 PGM_PowerGridModel* PGM_copy_model(PGM_Handle* handle, PGM_PowerGridModel const* model) noexcept {
     return call_with_catch(handle, [model, handle] {
-        auto& logger = get_logger(safe_ptr_get(handle));
+        auto& logger = get_logger(safe_ptr_get(handle).logger);
         auto copied_model = std::unique_ptr<MainModel>{create<MainModel>(safe_ptr_get(cast_to_cpp(model)), logger)};
         copied_model->reset_logger();
         return cast_to_c(copied_model.release());
@@ -366,7 +366,7 @@ void PGM_calculate(PGM_Handle* handle, PGM_PowerGridModel* model, PGM_Options co
         [handle, model, opt, output_dataset, batch_dataset] {
             auto& cpp_model = safe_ptr_get(cast_to_cpp(model));
             // Log to the handle passed to this call, not the handle the model was created with.
-            ScopedModuleLogger logger_guard{cpp_model, get_logger(safe_ptr_get(handle))};
+            ScopedModuleLogger logger_guard{cpp_model, get_logger(safe_ptr_get(handle).logger)};
             calculate_impl(cpp_model, safe_ptr_get(opt), safe_ptr_get(cast_to_cpp(output_dataset)),
                            safe_ptr_maybe_nullptr(cast_to_cpp(batch_dataset)));
         },
